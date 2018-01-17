5c6b7bbdefe18b84ce88e9c9ccfd3ff6597916ad
Daniel Griesser <daniel.griesser.86@gmail.com>
2018-01-17 14:58:26 +0100

    feat: Add common dsn

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,7 +1,10 @@
 //! Common functionality for the sentry agent.
 extern crate failure;
+extern crate url;
 #[macro_use] extern crate failure_derive;
 
 mod auth;
+mod dsn;
 
 pub use auth::*;
+pub use dsn::*;
