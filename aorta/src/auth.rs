ad9f00316a15e67bda194cc74853f154549405ea
Armin Ronacher <armin.ronacher@active-4.com>
2018-01-18 10:59:17 +0100

    feat: Added support for putting strings in keys

--- aorta/src/auth.rs
+++ aorta/src/auth.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::str::FromStr;
 use std::sync::{Once, ONCE_INIT};
 
 use base64;
@@ -15,15 +17,24 @@ fn with_sodiumoxide<T, F: FnOnce() -> T>(cb: F) -> T {
     cb()
 }
 
+/// Raised if a key could not be parsed.
+#[derive(Debug, Fail, PartialEq, Eq, Hash)]
+pub enum KeyParseError {
+    /// Invalid key encoding
+    #[fail(display="bad key encoding")]
+    BadEncoding,
+    /// Invalid key data
+    #[fail(display="bad key data")]
+    BadKey,
+}
+
 
 /// Represents the public key of an agent.
-#[derive(Debug)]
 pub struct PublicKey {
     inner: sign_backend::PublicKey,
 }
 
 /// Represents the secret key of an agent.
-#[derive(Debug)]
 pub struct SecretKey {
     inner: sign_backend::SecretKey,
 }
@@ -33,6 +44,41 @@ impl SecretKey {
     pub fn sign(&self, data: &[u8]) -> String {
         let sig = sign_backend::sign_detached(data, &self.inner);
         base64::encode_config(&sig.0[..], base64::URL_SAFE_NO_PAD)
+    }
+}
+
+impl PartialEq for SecretKey {
+    fn eq(&self, other: &SecretKey) -> bool {
+        &self.inner.0[..] == &other.inner.0[..]
+    }
+}
+
+impl Eq for SecretKey {}
+
+impl FromStr for SecretKey {
+    type Err = KeyParseError;
+
+    fn from_str(s: &str) -> Result<SecretKey, KeyParseError> {
+        let bytes = match base64::decode_config(s, base64::URL_SAFE_NO_PAD) {
+            Ok(bytes) => bytes,
+            _ => return Err(KeyParseError::BadEncoding)
+        };
+        Ok(SecretKey {
+            inner: sign_backend::SecretKey::from_slice(&bytes)
+                .ok_or(KeyParseError::BadKey)?
+        })
+    }
+}
+
+impl fmt::Display for SecretKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", base64::encode_config(&self.inner.0[..], base64::URL_SAFE_NO_PAD))
+    }
+}
+
+impl fmt::Debug for SecretKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "SecretKey(\"{}\")", self)
     }
 }
 
@@ -53,6 +99,41 @@ impl PublicKey {
     }
 }
 
+impl PartialEq for PublicKey {
+    fn eq(&self, other: &PublicKey) -> bool {
+        &self.inner.0[..] == &other.inner.0[..]
+    }
+}
+
+impl Eq for PublicKey {}
+
+impl FromStr for PublicKey {
+    type Err = KeyParseError;
+
+    fn from_str(s: &str) -> Result<PublicKey, KeyParseError> {
+        let bytes = match base64::decode_config(s, base64::URL_SAFE_NO_PAD) {
+            Ok(bytes) => bytes,
+            _ => return Err(KeyParseError::BadEncoding)
+        };
+        Ok(PublicKey {
+            inner: sign_backend::PublicKey::from_slice(&bytes)
+                .ok_or(KeyParseError::BadKey)?
+        })
+    }
+}
+
+impl fmt::Display for PublicKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", base64::encode_config(&self.inner.0[..], base64::URL_SAFE_NO_PAD))
+    }
+}
+
+impl fmt::Debug for PublicKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PublicKey(\"{}\")", self)
+    }
+}
+
 /// Generates a secret + public key pair.
 pub fn generate_key_pair() -> (SecretKey, PublicKey) {
     with_sodiumoxide(|| {
@@ -62,11 +143,26 @@ pub fn generate_key_pair() -> (SecretKey, PublicKey) {
 }
 
 #[test]
+fn test_keys() {
+    let sk: SecretKey = "OvXFVm1tIUi8xDTuyHX1SSqdMc8nCt2qU9IUaH5p7oUk5pHZsdnfXNiMWiMLtSE86J3N9Peo5CBP1YQHDUkApQ".parse().unwrap();
+    let pk: PublicKey = "JOaR2bHZ31zYjFojC7UhPOidzfT3qOQgT9WEBw1JAKU".parse().unwrap();
+
+    assert_eq!(sk.to_string(), "OvXFVm1tIUi8xDTuyHX1SSqdMc8nCt2qU9IUaH5p7oUk5pHZsdnfXNiMWiMLtSE86J3N9Peo5CBP1YQHDUkApQ");
+    assert_eq!(pk.to_string(), "JOaR2bHZ31zYjFojC7UhPOidzfT3qOQgT9WEBw1JAKU");
+
+    assert_eq!("bad data".parse::<SecretKey>(), Err(KeyParseError::BadEncoding));
+    assert_eq!("OvXF".parse::<SecretKey>(), Err(KeyParseError::BadKey));
+
+    assert_eq!("bad data".parse::<PublicKey>(), Err(KeyParseError::BadEncoding));
+    assert_eq!("OvXF".parse::<PublicKey>(), Err(KeyParseError::BadKey));
+}
+
+#[test]
 fn test_signatures() {
     let (sk, pk) = generate_key_pair();
     let data = b"Hello World!";
 
-    let mut sig = sk.sign(data);
+    let sig = sk.sign(data);
     assert_eq!(pk.verify(data, &sig), true);
 
     let bad_sig = "jgubwSf2wb2wuiRpgt2H9_bdDSMr88hXLp5zVuhbr65EGkSxOfT5ILIWr623twLgLd0bDgHg6xzOaUCX7XvUCw";
