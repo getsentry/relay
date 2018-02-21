0816bc674bb990b4ea06daab668f99a9d874e579
Armin Ronacher <armin.ronacher@active-4.com>
2018-02-21 22:03:48 +0100

    feat(cabi): Added basic aorta auth code to cabi

---
+++ cabi/src/aorta.rs
@@ -0,0 +1,79 @@
+use smith_aorta::{PublicKey, SecretKey};
+
+use core::{SmithBuf, SmithStr};
+
+pub struct SmithAgentId;
+
+/// Represents a public key in smith.
+pub struct SmithPublicKey;
+
+/// Represents a secret key in smith.
+pub struct SmithSecretKey;
+
+ffi_fn! {
+    /// Parses a public key from a string.
+    unsafe fn smith_publickey_parse(s: &SmithStr) -> Result<*mut SmithPublicKey> {
+        let public_key: PublicKey = s.as_str().parse()?;
+        Ok(Box::into_raw(Box::new(public_key)) as *mut SmithPublicKey)
+    }
+}
+
+ffi_fn! {
+    /// Frees a public key.
+    unsafe fn smith_publickey_free(spk: *mut SmithPublicKey) {
+        if !spk.is_null() {
+            let pk = spk as *mut PublicKey;
+            Box::from_raw(pk);
+        }
+    }
+}
+
+ffi_fn! {
+    /// Converts a public key into a string.
+    unsafe fn smith_publickey_to_string(spk: *const SmithPublicKey) -> Result<SmithStr> {
+        let pk = spk as *const PublicKey;
+        Ok(SmithStr::from_string((*pk).to_string()))
+    }
+}
+
+ffi_fn! {
+    /// Verifies a signature
+    unsafe fn smith_publickey_verify(spk: *const SmithPublicKey, data: *const SmithBuf, sig: *const SmithStr) -> Result<bool> {
+        let pk = spk as *const PublicKey;
+        Ok((*pk).verify((*data).as_bytes(), (*sig).as_str()))
+    }
+}
+
+ffi_fn! {
+    /// Parses a secret key from a string.
+    unsafe fn smith_secretkey_parse(s: &SmithStr) -> Result<*mut SmithSecretKey> {
+        let secret_key: SecretKey = s.as_str().parse()?;
+        Ok(Box::into_raw(Box::new(secret_key)) as *mut SmithSecretKey)
+    }
+}
+
+ffi_fn! {
+    /// Frees a secret key.
+    unsafe fn smith_secretkey_free(spk: *mut SmithSecretKey) {
+        if !spk.is_null() {
+            let pk = spk as *mut SecretKey;
+            Box::from_raw(pk);
+        }
+    }
+}
+
+ffi_fn! {
+    /// Converts a secret key into a string.
+    unsafe fn smith_secretkey_to_string(spk: *const SmithSecretKey) -> Result<SmithStr> {
+        let pk = spk as *const SecretKey;
+        Ok(SmithStr::from_string((*pk).to_string()))
+    }
+}
+
+ffi_fn! {
+    /// Verifies a signature
+    unsafe fn smith_secretkey_sign(spk: *const SmithSecretKey, data: *const SmithBuf) -> Result<SmithStr> {
+        let pk = spk as *const SecretKey;
+        Ok(SmithStr::from_string((*pk).sign((*data).as_bytes())))
+    }
+}
--- cabi/src/lib.rs
+++ cabi/src/lib.rs
@@ -4,5 +4,7 @@ extern crate smith_aorta;
 #[macro_use] mod utils;
 
 mod core;
+mod aorta;
 
 pub use core::*;
+pub use aorta::*;
