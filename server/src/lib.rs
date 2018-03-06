a6516a597c0b021f3f7263cbbaad6ac1fdd31ece
Armin Ronacher <armin.ronacher@active-4.com>
2018-03-06 10:58:12 +0100

    feat: initial work on proxy handling

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -10,7 +10,11 @@ extern crate log;
 extern crate parking_lot;
 extern crate smith_aorta;
 extern crate smith_config;
+extern crate smith_common;
 extern crate smith_trove;
+extern crate regex;
+#[macro_use]
+extern crate lazy_static;
 
 mod service;
 mod errors;
--- server/src/service.rs
+++ server/src/service.rs
@@ -1,20 +1,50 @@
 use parking_lot::Mutex;
 
-use futures::future::Future;
+use futures::future::{self, Future};
 use futures::sync::oneshot;
-
-use hyper::Body;
+use hyper::{Body, Error as HyperError};
 use hyper::header::{ContentLength, ContentType};
-use hyper::server::{const_service, service_fn, Http, Response};
-
-use errors::{Error, ErrorKind};
+use hyper::server::{Http, Request, Response, Service};
+use regex::Regex;
 use failure::ResultExt;
 use ctrlc;
 
+use errors::{Error, ErrorKind};
+
 use smith_config::Config;
 use smith_trove::Trove;
+use smith_common::ProjectId;
 
 static TEXT: &'static str = "Doing absolutely nothing so far!";
+
+struct ProxyService;
+
+
+impl Service for ProxyService {
+    type Request = Request;
+    type Response = Response;
+    type Error = HyperError;
+    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        lazy_static! {
+            static ref SUBMIT_URL: Regex = Regex::new(r"^/api/(\d+)/store/$").unwrap();
+        }
+
+        if let Some(m) = SUBMIT_URL.captures(req.path()) {
+            if let Ok(project_id) = m[1].parse::<ProjectId>() {
+                println!("{}", project_id);
+            }
+        }
+
+        Box::new(future::ok(
+            Response::new()
+                .with_header(ContentLength(TEXT.len() as u64))
+                .with_header(ContentType::plaintext())
+                .with_body(TEXT)
+        ))
+    }
+}
 
 fn dump_spawn_infos(config: &Config) {
     info!(
@@ -32,15 +62,8 @@ pub fn run(config: &Config) -> Result<(), Error> {
     let trove = Trove::new(config.make_aorta_config());
     trove.govern().context(ErrorKind::TroveGovernSpawnFailed)?;
 
-    let hello = const_service(service_fn(move |_req| {
-        Ok(Response::<Body>::new()
-            .with_header(ContentLength(TEXT.len() as u64))
-            .with_header(ContentType::plaintext())
-            .with_body(TEXT))
-    }));
-
     let server = Http::new()
-        .bind(&config.listen_addr(), hello)
+        .bind(&config.listen_addr(), || Ok(ProxyService))
         .context(ErrorKind::BindFailed)?;
 
     let (shutdown_tx, shutdown_rx) = oneshot::channel();
