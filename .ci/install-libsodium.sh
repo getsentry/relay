24486e8ca509e33744e0a3c6b63d51c3d2328279
Jan Michael Auer <account@jauer.org>
2018-04-30 12:52:33 +0200

    build: Remove libsodium build dependency (#8)
    * build: Remove libsodium build dependency
    
    * ci: Cache venv and execute pytest separately
    
    * ci: Build cargo locked

--- .ci/install-libsodium.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  brew update
-  brew install libsodium
-else
-  sudo add-apt-repository ppa:chris-lea/libsodium -y
-  sudo apt-get update && sudo apt-get install libsodium-dev -y
-fi
