c267b730f63a240afada0c36fae769fb4d837ec3
Jan Michael Auer <jan.auer@sentry.io>
2018-05-11 10:20:48 +0200

    build: Simplify the docker build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,11 +3,10 @@ set -ex
 
 BUILD_DIR="/work"
 TARGET="${BUILD_ARCH}-unknown-linux-musl"
-CC_NAME="CC_${TARGET//\-/_}"
 
 docker run \
         -e 'CC=' \
-        -e "${CC_NAME}=${TARGET}-gcc" \
+        -e "TARGET_CC=${TARGET}-gcc" \
         -w ${BUILD_DIR} \
         -v `pwd`:${BUILD_DIR} \
         -v $HOME/.cargo/registry:/root/.cargo/registry \
