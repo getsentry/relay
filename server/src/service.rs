f9e22f293eac59b0cbb4d3f1ff3373010cede13d
Jan Michael Auer <jan.auer@sentry.io>
2018-08-20 10:32:23 +0200

    fix: Silence warnings by re-enabling all endpoints

--- server/src/service.rs
+++ server/src/service.rs
@@ -69,8 +69,8 @@ fn make_app(state: ServiceState) -> ServiceApp {
         .middleware(AddCommonHeaders)
         .middleware(ErrorHandlers);
 
-    // app = endpoints::healthcheck::configure_app(app);
-    // app = endpoints::store::configure_app(app);
+    app = endpoints::healthcheck::configure_app(app);
+    app = endpoints::store::configure_app(app);
     app = endpoints::forward::configure_app(app);
     app = endpoints::queries::configure_app(app);
 
