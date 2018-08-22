97e82d2ee99b4a549e1d37a4f6c273a7e9fcdbef
Jan Michael Auer <jan.auer@sentry.io>
2018-08-22 11:45:32 +0200

    ref: Move project extractor to store endpoint

--- server/src/endpoints/store.rs
+++ server/src/endpoints/store.rs
@@ -1,12 +1,13 @@
 //! Handles event store requests.
 use actix_web::middleware::cors::Cors;
-use actix_web::{http::Method, FromRequest, HttpResponse, Json, ResponseError};
+use actix_web::{http::Method, HttpRequest, HttpResponse, Json, ResponseError};
+use sentry;
 use uuid::Uuid;
 
-use extractors::{IncomingEvent, ProjectRequest};
 use service::{ServiceApp, ServiceState};
 
-use semaphore_aorta::{ApiErrorResponse, StoreChangeset};
+use semaphore_aorta::ApiErrorResponse;
+use semaphore_common::{Auth, AuthParseError, ProjectId, ProjectIdParseError};
 
 #[derive(Serialize)]
 struct StoreResponse {
@@ -14,30 +15,63 @@ struct StoreResponse {
 }
 
 #[derive(Fail, Debug)]
-#[fail(display = "event submission rejected")]
-struct StoreRejected;
+enum BadStoreRequest {
+    #[fail(display = "invalid project path parameter")]
+    BadProject(#[cause] ProjectIdParseError),
+    #[fail(display = "bad x-sentry-auth header")]
+    BadAuth(#[cause] AuthParseError),
+}
 
-impl ResponseError for StoreRejected {
+impl ResponseError for BadStoreRequest {
     fn error_response(&self) -> HttpResponse {
-        HttpResponse::Forbidden().json(&ApiErrorResponse::from_fail(self))
+        HttpResponse::BadRequest().json(&ApiErrorResponse::from_fail(self))
     }
 }
 
-fn store_event<I: FromRequest<ServiceState> + Into<StoreChangeset>>(
-    mut request: ProjectRequest<I>,
-) -> Result<Json<StoreResponse>, StoreRejected> {
-    let changeset = request.take_payload().into();
-    let event_id = changeset.event.id();
+fn get_auth_from_request<S>(req: &HttpRequest<S>) -> Result<Auth, BadStoreRequest> {
+    // try auth from header
+    let auth = req
+        .headers()
+        .get("x-sentry-auth")
+        .and_then(|x| x.to_str().ok());
 
-    metric!(counter(&format!("event.protocol.v{}", request.auth().version())) += 1);
-
-    if unimplemented!() {
-        metric!(counter("event.accepted") += 1);
-        Ok(Json(StoreResponse { id: event_id }))
-    } else {
-        metric!(counter("event.rejected") += 1);
-        Err(StoreRejected)
+    if let Some(auth) = auth {
+        return auth.parse::<Auth>().map_err(BadStoreRequest::BadAuth);
     }
+
+    Auth::from_querystring(req.query_string().as_bytes()).map_err(BadStoreRequest::BadAuth)
+}
+
+fn store_event(request: HttpRequest<ServiceState>) -> Result<Json<StoreResponse>, BadStoreRequest> {
+    let auth = get_auth_from_request(&request)?;
+    let project_id = request
+        .match_info()
+        .get("project")
+        .unwrap_or_default()
+        .parse::<ProjectId>()
+        .map_err(BadStoreRequest::BadProject)?;
+
+    sentry::configure_scope(|scope| {
+        scope.set_user(Some(sentry::User {
+            id: Some(project_id.to_string()),
+            ..Default::default()
+        }));
+    });
+
+    unimplemented!()
+
+    // let changeset: StoreChangeset = request.take_payload().into();
+    // let event_id = changeset.event.id();
+
+    // metric!(counter(&format!("event.protocol.v{}", request.auth().version())) += 1);
+
+    // if unimplemented!() {
+    //     metric!(counter("event.accepted") += 1);
+    //     Ok(Json(StoreResponse { id: event_id }))
+    // } else {
+    //     metric!(counter("event.rejected") += 1);
+    //     Err(StoreRejected)
+    // }
 }
 
 pub fn configure_app(app: ServiceApp) -> ServiceApp {
@@ -53,7 +87,7 @@ pub fn configure_app(app: ServiceApp) -> ServiceApp {
         ])
         .max_age(3600)
         .resource(r"/api/{project:\d+}/store/", |r| {
-            r.method(Method::POST).with(store_event::<IncomingEvent>);
+            r.method(Method::POST).with(store_event);
         })
         .register()
 }
--- server/src/extractors/mod.rs
+++ server/src/extractors/mod.rs
@@ -3,11 +3,9 @@ use actix_web::State;
 use service::ServiceState;
 
 mod event;
-mod project;
 mod signed;
 
 pub use self::event::*;
-pub use self::project::*;
 pub use self::signed::*;
 
 /// An extractor for the entire service state.
