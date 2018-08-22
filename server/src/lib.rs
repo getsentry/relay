da2632e0ad908b161c35a9d5baf5480fdfd5c697
Jan Michael Auer <jan.auer@sentry.io>
2018-08-22 23:26:59 +0200

    build: Fix dependencies in server crate

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -5,18 +5,18 @@ extern crate actix;
 extern crate actix_web;
 extern crate base64;
 extern crate bytes;
+extern crate chrono;
 extern crate failure;
 #[macro_use]
 extern crate failure_derive;
 extern crate flate2;
 extern crate futures;
-extern crate http;
-extern crate itertools;
+extern crate listenfd;
 #[macro_use]
 extern crate log;
-extern crate mime;
 #[cfg(feature = "with_ssl")]
 extern crate openssl;
+extern crate parking_lot;
 extern crate semaphore_aorta;
 #[macro_use]
 extern crate semaphore_common;
@@ -26,10 +26,6 @@ extern crate sentry_actix;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate chrono;
-extern crate listenfd;
-extern crate parking_lot;
-extern crate serde_json;
 extern crate url;
 extern crate uuid;
 
