417bc318a99de2e9069d032c1a04442d36d1ef91
Jan Michael Auer <jan.auer@sentry.io>
2018-08-23 00:07:03 +0200

    ref: Move store body extractor to extractors

--- server/src/extractors/mod.rs
+++ server/src/extractors/mod.rs
@@ -2,9 +2,11 @@ use actix_web::State;
 
 use service::ServiceState;
 
-mod signed;
+mod signed_json;
+mod store_body;
 
-pub use self::signed::*;
+pub use self::signed_json::*;
+pub use self::store_body::*;
 
 /// An extractor for the entire service state.
 pub type CurrentServiceState = State<ServiceState>;
