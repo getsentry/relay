eaf4b5893e46b16605eab59cd9ac6bbd47f1b0b9
Jan Michael Auer <jan.auer@sentry.io>
2018-08-27 09:41:40 +0200

    test(integration): Disable Sentry in integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -202,7 +202,7 @@ def relay(tmpdir, mini_sentry, request, random_port, background_process, config_
                         "tls_private_key": None,
                         "tls_cert": None,
                     },
-                    "sentry": {"dsn": mini_sentry.dsn},
+                    "sentry": {"enabled": False},
                 }
             )
         )
