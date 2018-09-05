e4bb2d075de288551909300c8d9a053ca27aba8e
Jan Michael Auer <jan.auer@sentry.io>
2018-09-05 19:28:17 +0200

    test: Switch to trace logging in tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -286,7 +286,7 @@ def relay(tmpdir, mini_sentry, request, random_port, background_process, config_
             "sentry": {"dsn": mini_sentry.internal_error_dsn},
             "limits": {"max_api_file_upload_size": "1MiB"},
             "cache": {"batch_interval": 0},
-            "logging": {"level": "debug"},
+            "logging": {"level": "trace"},
             "http": {"timeout": 2},
         }
 
