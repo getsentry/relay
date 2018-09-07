a2df5de0310747c23d436e2557005a8a08ead432
Anton Ovchinnikov <anton@tonyo.info>
2018-09-07 18:02:12 +0200

    feat(docker): Create a dedicated user/group with fixed ids

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# Fix permissions
-chmod 755 /bin/semaphore
-chown -R semaphore:semaphore /etc/semaphore /work
-
-exec gosu semaphore /bin/semaphore "$@"
+if [ "$(id -u)" == "0" ]; then
+  exec gosu semaphore /bin/semaphore "$@"
+else
+  exec /bin/semaphore "$@"
+fi
