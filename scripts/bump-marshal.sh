60549b874794fc1037dd5dba9c83e64977b6c394
Anton Ovchinnikov <anton@tonyo.info>
2018-10-31 19:12:54 +0100

    feat: Add a script for bumping "marshal" version

---
+++ scripts/bump-marshal.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Bump "marshal" version in common/
+#
+# Usage:
+#     bump-marshal.sh MARSHAL_GIT_SHA [--no-commit]
+#
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR/..
+
+COMMIT="1"
+
+while [[ $# -gt 0 ]]; do
+    case "${1}" in
+    -n|--no-commit)
+        COMMIT=""
+        shift
+        ;;
+    *)
+        MARSHAL_SHA="${1}"
+        shift
+        ;;
+    esac
+done
+
+sed -i '' -e "s|^\(marshal.*github.com/getsentry/marshal\", rev = \)\"[a-z0-9]*\"|\1\"${MARSHAL_SHA}\"|" \
+    common/Cargo.toml
+
+cargo update -p semaphore
+
+cd cabi
+cargo update -p semaphore-common
+
+if [ -n "${COMMIT}" ]; then
+    # Commit changes
+    git commit --all -n -m "getsentry/marshal@{sha}"
+fi
