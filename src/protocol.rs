8dca02b07bc45b6f0fa85845892d29ac9470cfe7
Armin Ronacher <armin.ronacher@active-4.com>
2018-11-03 14:13:57 +0100

    feat: Added non string key support for array based tags

--- src/protocol.rs
+++ src/protocol.rs
@@ -483,15 +483,16 @@ pub struct Tags(pub Array<(Annotated<String>, Annotated<String>)>);
 
 impl FromValue for Tags {
     fn from_value(value: Annotated<Value>) -> Annotated<Self> {
-        type TagTuple = (Annotated<String>, Annotated<LenientString>);
+        type TagTuple = (Annotated<LenientString>, Annotated<LenientString>);
         match value {
             Annotated(Some(Value::Array(items)), meta) => {
                 let mut rv = Vec::new();
                 for item in items.into_iter() {
                     rv.push(TagTuple::from_value(item).map_value(|tuple| {
-                        let key = tuple.0.map_value(|key| key.trim().replace(" ", "-"));
-                        let value = tuple.1.map_value(|v| v.0);
-                        (key, value)
+                        (
+                            tuple.0.map_value(|key| key.0.trim().replace(" ", "-")),
+                            tuple.1.map_value(|v| v.0),
+                        )
                     }));
                 }
                 Annotated(Some(Tags(rv)), meta)
@@ -505,12 +506,14 @@ impl FromValue for Tags {
                     ));
                 }
                 rv.sort_unstable_by(|a, b| a.0.cmp(&b.0));
-                Annotated(Some(Tags(rv.into_iter().map(|(k, v)| {
-                    Annotated::new((
-                        Annotated::new(k),
-                        v.map_value(|x| x.0)
-                    ))
-                }).collect())), meta)
+                Annotated(
+                    Some(Tags(
+                        rv.into_iter()
+                            .map(|(k, v)| Annotated::new((Annotated::new(k), v.map_value(|x| x.0))))
+                            .collect(),
+                    )),
+                    meta,
+                )
             }
             other => FromValue::from_value(other).map_value(Tags),
         }
@@ -3168,6 +3171,37 @@ fn test_tags_from_object() {
     arr.push(Annotated::new((
         Annotated::new("non-string".to_string()),
         Annotated::new("42".to_string()),
+    )));
+
+    let tags = Annotated::new(Tags(arr));
+    assert_eq_dbg!(tags, Annotated::from_json(json).unwrap());
+}
+
+#[test]
+fn test_tags_from_array() {
+    let json = r#"[
+  ["bool", true],
+  ["foo bar", "baz"],
+  [23, 42],
+  ["blah", "blub"]
+]"#;
+
+    let mut arr = Array::new();
+    arr.push(Annotated::new((
+        Annotated::new("bool".to_string()),
+        Annotated::new("True".to_string()),
+    )));
+    arr.push(Annotated::new((
+        Annotated::new("foo-bar".to_string()),
+        Annotated::new("baz".to_string()),
+    )));
+    arr.push(Annotated::new((
+        Annotated::new("23".to_string()),
+        Annotated::new("42".to_string()),
+    )));
+    arr.push(Annotated::new((
+        Annotated::new("blah".to_string()),
+        Annotated::new("blub".to_string()),
     )));
 
     let tags = Annotated::new(Tags(arr));
