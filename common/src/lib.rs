4edca4c8f9c86c109eae706b631e3b5a233fe3e4
Armin Ronacher <armin.ronacher@active-4.com>
2018-11-12 12:14:54 +0100

    ref: Codestyle

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -57,7 +57,7 @@ pub use sentry_types::{
 };
 
 // expose the general module entirely
-pub use general::{processor as processor};
+pub use general::processor;
 /// The v8 version of the protocol.
 pub mod protocol {
     pub use general::protocol::*;
