f6a869b1161600625d13a19a0cde67101f665579
Armin Ronacher <armin.ronacher@active-4.com>
2018-11-12 21:38:30 +0100

    feat: Add python interface for normalization

--- py/semaphore/processing.py
+++ py/semaphore/processing.py
@@ -1,11 +1,12 @@
 import json
-import six
 
-from semaphore._lowlevel import lib
-from semaphore.utils import encode_str, decode_str, rustcall
+from semaphore._compat import string_types, iteritems, text_type
+from semaphore._lowlevel import lib, ffi
+from semaphore.utils import encode_str, decode_str, rustcall, RustObject, attached_refs
 
 
-__all__ = ['split_chunks', 'meta_with_chunks']
+__all__ = ['split_chunks', 'meta_with_chunks', 'StoreNormalizer', 'GeoIoLookup']
+
 
 def split_chunks(string, remarks):
     return json.loads(decode_str(rustcall(
@@ -14,15 +15,16 @@ def split_chunks(string, remarks):
         encode_str(json.dumps(remarks))
     )))
 
+
 def meta_with_chunks(data, meta):
     if not isinstance(meta, dict):
         return meta
 
     result = {}
-    for key, item in six.iteritems(meta):
+    for key, item in iteritems(meta):
         if key == '' and isinstance(item, dict):
             result[''] = item.copy()
-            if item.get('rem') and isinstance(data, six.string_types):
+            if item.get('rem') and isinstance(data, string_types):
                 result['']['chunks'] = split_chunks(data, item['rem'])
         elif isinstance(data, dict):
             result[key] = meta_with_chunks(data.get(key), item)
@@ -32,3 +34,32 @@ def meta_with_chunks(data, meta):
             result[key] = item
 
     return result
+
+
+class GeoIoLookup(RustObject):
+    __dealloc_func__ = lib.semaphore_geoip_lookup_free
+
+    @classmethod
+    def from_path(cls, path):
+        if isinstance(path, text_type):
+            path = path.encode('utf-8')
+        return cls._from_objptr(rustcall(lib.semaphore_geoip_lookup_new, path))
+
+
+class StoreNormalizer(RustObject):
+    __dealloc_func__ = lib.semaphore_store_normalizer_free
+    __init__ = object.__init__
+
+    def __new__(cls, geoip_lookup=None, **config):
+        config = json.dumps(config)
+        geoptr = geoip_lookup._get_objptr() if geoip_lookup is not None else ffi.NULL
+        rv = cls._from_objptr(rustcall(
+            lib.semaphore_store_normalizer_new, encode_str(config), geoptr))
+        if geoip_lookup is not None:
+            attached_refs[rv] = geoip_lookup
+        return rv
+
+    def normalize_event(self, event):
+        event = encode_str(json.dumps(event))
+        rv = self._methodcall(lib.semaphore_store_normalizer_normalize_event, event)
+        return json.loads(decode_str(rv))
