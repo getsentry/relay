245a030101e2eb2eb0d53ff29762d74868a59da1
Jan Michael Auer <jan.auer@sentry.io>
2018-12-17 16:04:16 +0100

    test(general): Allow more cases in testutils

--- general/src/testutils.rs
+++ general/src/testutils.rs
@@ -1,35 +1,33 @@
 macro_rules! assert_eq_str {
-    ($left:expr, $right:expr) => {{
-        let left = &($left);
-        let right = &$right;
-
-        assert!(
-            left == right,
-            "`left == right` in line {}:\n{}\n{}",
-            line!(),
-            difference::Changeset::new("- left", "+ right", "\n"),
-            difference::Changeset::new(&left, &right, "\n")
-        )
-    }};
     ($left:expr, $right:expr) => {
+        match (&$left, &$right) {
+            (left, right) => assert!(
+                left == right,
+                "`left == right` in line {}:\n{}\n{}",
+                line!(),
+                difference::Changeset::new("- left", "+ right", "\n"),
+                difference::Changeset::new(&left, &right, "\n")
+            ),
+        }
+    };
+    ($left:expr, $right:expr,) => {
         assert_eq_str($left, $right)
     };
 }
 
 macro_rules! assert_eq_dbg {
-    ($left:expr, $right:expr) => {{
-        let left = &$left;
-        let right = &$right;
-
-        assert!(
-            left == right,
-            "`left == right` in line {}:\n{}\n{}",
-            line!(),
-            difference::Changeset::new("- left", "+ right", "\n"),
-            difference::Changeset::new(&format!("{:#?}", left), &format!("{:#?}", right), "\n")
-        )
-    }};
     ($left:expr, $right:expr) => {
+        match (&$left, &$right) {
+            (left, right) => assert!(
+                left == right,
+                "`left == right` in line {}:\n{}\n{}",
+                line!(),
+                difference::Changeset::new("- left", "+ right", "\n"),
+                difference::Changeset::new(&format!("{:#?}", left), &format!("{:#?}", right), "\n")
+            ),
+        }
+    };
+    ($left:expr, $right:expr,) => {
         assert_eq_dbg($left, $right)
     };
 }
