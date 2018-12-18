dc8fceb45fdfa813fa8629814e92c592ae02fadb
Jan Michael Auer <jan.auer@sentry.io>
2018-12-18 19:25:27 +0100

    feat(general): Remove all unknown attributes in store normalization

--- general/src/store/remove_other.rs
+++ general/src/store/remove_other.rs
@@ -1,25 +1,39 @@
-use crate::processor::{ProcessingState, Processor};
+use crate::processor::{ProcessValue, ProcessingState, Processor};
 use crate::protocol::Event;
-use crate::types::{Annotated, ErrorKind, Meta, ValueAction};
+use crate::types::{Annotated, ErrorKind, Meta, Object, Value, ValueAction};
 
 pub struct RemoveOtherProcessor;
 
 impl Processor for RemoveOtherProcessor {
+    fn process_other(&mut self, other: &mut Object<Value>, _state: &ProcessingState<'_>) {
+        // Drop unknown attributes at all levels without error messages
+        other.clear();
+    }
+
     fn process_event(
         &mut self,
         event: &mut Event,
         _meta: &mut Meta,
-        _state: &ProcessingState<'_>,
+        state: &ProcessingState<'_>,
     ) -> ValueAction {
-        for (key, value) in event.other.iter_mut() {
-            if key == "applecrashreport" || key == "device" || key == "repos" || key == "query" {
-                continue;
-            }
+        // Move the current map out so we don't clear it in `process_other`
+        let mut other = std::mem::replace(&mut event.other, Default::default());
 
-            // Replace the value and all existing meta with an error
+        // Drop known legacy attributes at top-level without errors
+        other.remove("applecrashreport");
+        other.remove("device");
+        other.remove("repos");
+        other.remove("query");
+
+        // Replace remaining values and all existing meta with an errors
+        for value in other.values_mut() {
             *value = Annotated::from_error(ErrorKind::InvalidAttribute, None);
         }
 
+        // Recursively clean all `other`s now. Note that this won't touch the event's other
+        event.process_child_values(self, state);
+
+        event.other = other;
         ValueAction::Keep
     }
 }
