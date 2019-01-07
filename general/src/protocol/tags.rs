61b174f892a32dd3621b62d1bf14409dea6a56fc
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-01-07 17:25:19 +0100

    fix: Right tag key normalization, not for values

--- general/src/protocol/tags.rs
+++ general/src/protocol/tags.rs
@@ -48,8 +48,8 @@ impl FromValue for TagEntry {
         type TagTuple = (Annotated<LenientString>, Annotated<LenientString>);
         TagTuple::from_value(value).map_value(|(key, value)| {
             TagEntry(
-                key.map_value(|x| x.into_inner().replace(" ", "-")),
-                value.map_value(|x| x.into_inner().replace(" ", "-")),
+                key.map_value(|x| x.into_inner().replace(" ", "-").trim().to_string()),
+                value.map_value(|x| x.into_inner().trim().to_string()),
             )
         })
     }
