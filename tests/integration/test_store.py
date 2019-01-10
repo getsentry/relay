8147f3dcf9b24ebaff6279f4a2eb2b31b61ddb1d
Jan Michael Auer <jan.auer@sentry.io>
2019-01-10 15:30:52 +0100

    test: Add an integration test for proxy configuration

--- tests/integration/test_store.py
+++ tests/integration/test_store.py
@@ -128,3 +128,20 @@ def test_static_config(mini_sentry, relay):
         raise AssertionError(
             f"Exceptions happened in mini_sentry: {mini_sentry.test_failures}"
         )
+
+
+def test_proxy_config(mini_sentry, relay):
+    from time import sleep
+    project_config = mini_sentry.basic_project_config()
+
+    def configure_proxy(dir):
+        os.remove(dir.join("credentials.json"))
+
+    relay_options = {'relay': {'mode': 'proxy'}}
+    relay = relay(mini_sentry, options=relay_options, prepare=configure_proxy)
+    mini_sentry.project_configs[42] = project_config
+    sleep(1)  # There is no upstream auth, so just wait for relay to initialize
+
+    relay.send_event(42)
+    event = mini_sentry.captured_events.get(timeout=1)
+    assert event["logentry"] == {"formatted": "Hello, World!"}
