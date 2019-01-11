560334ef969891017738f6f0a75b3f00bf32468e
Jan Michael Auer <jan.auer@sentry.io>
2019-01-11 11:54:48 +0100

    feat(general): Parse request bodies

--- general/src/store/normalize/request.rs
+++ general/src/store/normalize/request.rs
@@ -1,6 +1,5 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use serde::de::IgnoredAny;
 use url::Url;
 
 use crate::protocol::{PairList, Query, Request};
@@ -10,18 +9,6 @@ const ELLIPSIS: char = '\u{2026}';
 
 lazy_static! {
     static ref METHOD_RE: Regex = Regex::new(r"^[A-Z\-_]{3,32}$").unwrap();
-}
-
-fn infer_content_type(body: &Annotated<Value>) -> Option<String> {
-    let body = body.value()?.as_str()?;
-
-    if serde_json::from_str::<IgnoredAny>(body).is_ok() {
-        Some("application/json".to_string())
-    } else if serde_urlencoded::from_str::<IgnoredAny>(body).is_ok() {
-        Some("application/x-www-form-urlencoded".to_string())
-    } else {
-        None
-    }
 }
 
 fn normalize_url(request: &mut Request) {
@@ -109,25 +96,72 @@ fn set_auto_remote_addr(env: &mut Object<Value>, remote_addr: &str) {
     }
 }
 
+/// Checks for the likelyhood of a parsed urlencoded body.
+fn is_valid_urlencoded(object: &Object<Value>) -> bool {
+    // `serde_urlencoded` can decode any string with valid characters into an object. However, we
+    // need to account for false-positives in the following cases:
+    //  - A string "foo" is decoded as {"foo": ""} (check for single empty value)
+    //  - A base64 encoded string "dGU=" also decodes with a single empty value
+    //  - A base64 encoded string "dA==" decodes as {"dA": "="} (check for single =)
+    //  - Any other string containing equals or ampersand will decode properly
+
+    if object.len() > 1 {
+        return true;
+    }
+
+    match object.values().next().and_then(|a| a.as_str()) {
+        Some(s) => s != "" && s != "=",
+        None => false,
+    }
+}
+
+fn parse_raw_data(request: &Request) -> Option<(&'static str, Value)> {
+    let raw = request.data.as_str()?;
+
+    // TODO: Try to decode base64 first
+
+    if let Ok(value) = serde_json::from_str(raw) {
+        return Some(("application/json", value));
+    }
+
+    if let Ok(Value::Object(value)) = serde_urlencoded::from_str(raw) {
+        if is_valid_urlencoded(&value) {
+            return Some(("application/x-www-form-urlencoded", Value::Object(value)));
+        }
+    }
+
+    None
+}
+
+fn normalize_data(request: &mut Request) {
+    // Always derive the `inferred_content_type` from the request body, even if there is a
+    // `Content-Type` header present. This value can technically be ingested (due to the schema) but
+    // should always be overwritten in normalization. Only if inference fails, fall back to the
+    // content type header.
+    if let Some((content_type, parsed_data)) = parse_raw_data(request) {
+        // Retain meta data on the body (e.g. trimming annotations) but remove anything on the
+        // inferred content type.
+        request.data.set_value(Some(parsed_data));
+        request.inferred_content_type = Annotated::from(content_type.to_string());
+    } else {
+        request.inferred_content_type = request
+            .headers
+            .value()
+            .and_then(|headers| headers.get_header("Content-Type"))
+            .map(|value| value.to_string())
+            .into();
+    }
+}
+
 pub fn normalize_request(request: &mut Request, client_ip: Option<&str>) {
     request.method.apply(normalize_method);
     normalize_url(request);
+    normalize_data(request);
 
     if let Some(ref client_ip) = client_ip {
         request
             .env
             .apply(|env, _meta| set_auto_remote_addr(env, client_ip));
-    }
-
-    if request.inferred_content_type.value().is_none() {
-        let content_type = request
-            .headers
-            .value()
-            .and_then(|headers| headers.get_header("Content-Type"))
-            .map(|value| value.to_string())
-            .or_else(|| infer_content_type(&request.data));
-
-        request.inferred_content_type.set_value(content_type);
     }
 
     if let (None, Some(ref mut headers)) = (request.cookies.value(), request.headers.value_mut()) {
@@ -157,7 +191,7 @@ pub fn normalize_request(request: &mut Request, client_ip: Option<&str>) {
 fn test_url_truncation() {
     let mut request = Request {
         url: Annotated::new("http://example.com/path?foo#bar".to_string()),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
@@ -169,7 +203,7 @@ fn test_url_truncation_reversed() {
     let mut request = Request {
         // The query string is empty and the fragment is "foo?bar" here
         url: Annotated::new("http://example.com/path#foo?bar".to_string()),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
@@ -180,7 +214,7 @@ fn test_url_truncation_reversed() {
 fn test_url_with_ellipsis() {
     let mut request = Request {
         url: Annotated::new("http://example.com/path…".to_string()),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
@@ -193,7 +227,7 @@ fn test_url_with_qs_and_fragment() {
 
     let mut request = Request {
         url: Annotated::new("http://example.com/path?some=thing#else".to_string()),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
@@ -207,7 +241,7 @@ fn test_url_with_qs_and_fragment() {
                 Annotated::new("thing".to_string().into()),
             )),]))),
             fragment: Annotated::new("else".to_string()),
-            ..Default::default()
+            ..Request::default()
         }
     );
 }
@@ -223,7 +257,7 @@ fn test_url_precedence() {
             Annotated::new("thing".to_string().into()),
         ))]))),
         fragment: Annotated::new("else".to_string()),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
@@ -237,7 +271,7 @@ fn test_url_precedence() {
                 Annotated::new("thing".to_string().into()),
             )),]))),
             fragment: Annotated::new("else".to_string()),
-            ..Default::default()
+            ..Request::default()
         }
     );
 }
@@ -248,7 +282,7 @@ fn test_query_string_empty_value() {
 
     let mut request = Request {
         url: Annotated::new("http://example.com/path?some".to_string()),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
@@ -261,7 +295,7 @@ fn test_query_string_empty_value() {
                 Annotated::new("some".to_string()),
                 Annotated::new("".to_string().into()),
             )),]))),
-            ..Default::default()
+            ..Request::default()
         }
     );
 }
@@ -272,14 +306,11 @@ fn test_cookies_in_header() {
 
     let mut request = Request {
         url: Annotated::new("http://example.com".to_string()),
-        headers: Annotated::new(Headers(
-            vec![Annotated::new((
-                Annotated::new("Cookie".to_string()),
-                Annotated::new("a=b;c=d".to_string()),
-            ))]
-            .into(),
-        )),
-        ..Default::default()
+        headers: Annotated::new(Headers(PairList(vec![Annotated::new((
+            Annotated::new("Cookie".to_string()),
+            Annotated::new("a=b;c=d".to_string()),
+        ))]))),
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
@@ -316,7 +347,7 @@ fn test_cookies_in_header_not_overridden() {
             Annotated::new("foo".to_string()),
             Annotated::new("bar".to_string()),
         ))]))),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
@@ -334,22 +365,118 @@ fn test_cookies_in_header_not_overridden() {
 fn test_method_invalid() {
     let mut request = Request {
         method: Annotated::new("!!!!".to_string()),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
 
-    assert_eq_dbg!(request.method.0, None);
+    assert_eq_dbg!(request.method.value(), None);
 }
 
 #[test]
 fn test_method_valid() {
     let mut request = Request {
         method: Annotated::new("POST".to_string()),
-        ..Default::default()
+        ..Request::default()
     };
 
     normalize_request(&mut request, None);
 
-    assert_eq_dbg!(request.method.0, Some("POST".to_string()));
+    assert_eq_dbg!(request.method.as_str(), Some("POST"));
+}
+
+#[test]
+fn test_infer_json() {
+    let mut request = Request {
+        data: Annotated::from(Value::String(r#"{"foo":"bar"}"#.to_string())),
+        ..Request::default()
+    };
+
+    let mut expected_value = Object::new();
+    expected_value.insert(
+        "foo".to_string(),
+        Annotated::from(Value::String("bar".into())),
+    );
+
+    normalize_request(&mut request, None);
+    assert_eq_dbg!(
+        request.inferred_content_type.as_str(),
+        Some("application/json")
+    );
+    assert_eq_dbg!(request.data.value(), Some(&Value::Object(expected_value)));
+}
+
+#[test]
+fn test_broken_json_with_fallback() {
+    use crate::protocol::Headers;
+
+    let mut request = Request {
+        data: Annotated::from(Value::String(r#"{"foo":"b"#.to_string())),
+        headers: Annotated::from(Headers(PairList(vec![Annotated::new((
+            Annotated::new("Content-Type".to_string()),
+            Annotated::new("text/plain".to_string()),
+        ))]))),
+        ..Request::default()
+    };
+
+    normalize_request(&mut request, None);
+    assert_eq_dbg!(request.inferred_content_type.as_str(), Some("text/plain"));
+    assert_eq_dbg!(request.data.as_str(), Some(r#"{"foo":"b"#));
+}
+
+#[test]
+fn test_broken_json_without_fallback() {
+    let mut request = Request {
+        data: Annotated::from(Value::String(r#"{"foo":"b"#.to_string())),
+        ..Request::default()
+    };
+
+    normalize_request(&mut request, None);
+    assert_eq_dbg!(request.inferred_content_type.value(), None);
+    assert_eq_dbg!(request.data.as_str(), Some(r#"{"foo":"b"#));
+}
+
+#[test]
+fn test_infer_url_encoded() {
+    let mut request = Request {
+        data: Annotated::from(Value::String(r#"foo=bar"#.to_string())),
+        ..Request::default()
+    };
+
+    let mut expected_value = Object::new();
+    expected_value.insert(
+        "foo".to_string(),
+        Annotated::from(Value::String("bar".into())),
+    );
+
+    normalize_request(&mut request, None);
+    assert_eq_dbg!(
+        request.inferred_content_type.as_str(),
+        Some("application/x-www-form-urlencoded")
+    );
+    assert_eq_dbg!(request.data.value(), Some(&Value::Object(expected_value)));
+}
+
+#[test]
+fn test_infer_url_false_positive() {
+    let mut request = Request {
+        data: Annotated::from(Value::String("dGU=".to_string())),
+        ..Request::default()
+    };
+
+    normalize_request(&mut request, None);
+    assert_eq_dbg!(request.inferred_content_type.value(), None);
+    assert_eq_dbg!(request.data.as_str(), Some("dGU="));
+}
+
+#[test]
+fn test_infer_url_encoded_base64() {
+    let mut request = Request {
+        data: Annotated::from(Value::String("dA==".to_string())),
+        ..Request::default()
+    };
+
+    normalize_request(&mut request, None);
+    assert_eq_dbg!(request.inferred_content_type.value(), None);
+    assert_eq_dbg!(request.data.as_str(), Some("dA=="));
 }
