172cec64292b8a053ae3464b6973894ba6774d2d
Jan Michael Auer <account@jauer.org>
2019-01-22 10:12:01 +0100

    build: Publish stripped debug information (#158)
    * build: Strip debug information in Linux
    
    * build: Bundle macos debug info and upload to Zeus
    
    * build(docker): Allow building without DOCKER_PASS
    
    * build(docker): Add libclang to the build image (required by bindgen)
    
    * ci: Do not cache debug archives

--- scripts/docker-build-linux.sh
+++ scripts/docker-build-linux.sh
@@ -25,7 +25,7 @@ docker build --build-arg DOCKER_ARCH=${DOCKER_ARCH} \
              -t "${BUILD_IMAGE}" -f Dockerfile.build .
 
 # Push build image if possible
-if [ -n "${DOCKER_PASS}" ]; then
+if [ -n "${DOCKER_PASS:-}" ]; then
   echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin || true
   docker push "${BUILD_IMAGE}" || true
 fi
@@ -39,6 +39,16 @@ DOCKER_RUN_OPTS="
 # And now build the project
 docker run $DOCKER_RUN_OPTS \
   cargo build --release --locked --target=${TARGET}
+
+# Strip debug information from the main file
+docker run $DOCKER_RUN_OPTS \
+  objcopy --only-keep-debug target/${TARGET}/release/semaphore{,.debug}
+
+docker run $DOCKER_RUN_OPTS \
+  objcopy --strip-debug --strip-unneeded target/${TARGET}/release/semaphore
+
+docker run $DOCKER_RUN_OPTS \
+  objcopy --add-gnu-debuglink target/${TARGET}/release/semaphore{.debug,}
 
 # Smoke test
 docker run $DOCKER_RUN_OPTS \
