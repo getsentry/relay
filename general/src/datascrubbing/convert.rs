4c79aa9831bf010cc6e43b95e2aa845f5e7848ca
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-08-28 18:14:24 +0200

    fix: Imitate sentrys placeholders

--- general/src/datascrubbing/convert.rs
+++ general/src/datascrubbing/convert.rs
@@ -55,7 +55,7 @@ pub fn to_pii_config(datascrubbing_config: &DataScrubbingConfig) -> Option<PiiCo
                                 .unwrap(),
                         ),
                     }),
-                    redaction: Redaction::Replace("[filtered: sensitive keys]".to_owned().into()),
+                    redaction: Redaction::Replace("[filtered]".to_owned().into()),
                 },
             );
 
@@ -1237,7 +1237,7 @@ THd+9FBxiHLGXNKhG/FRSyREXEt+NyYIf/0cyByc9tNksat794ddUqnLOg0vwSkv
        ⋮      "keyPattern": ".*(fieldy_field|moar_other_field).*",
        ⋮      "redaction": {
        ⋮        "method": "replace",
-       ⋮        "text": "[filtered: sensitive keys]"
+       ⋮        "text": "[filtered]"
        ⋮      }
        ⋮    }
        ⋮  },
@@ -2247,7 +2247,7 @@ THd+9FBxiHLGXNKhG/FRSyREXEt+NyYIf/0cyByc9tNksat794ddUqnLOg0vwSkv
        ⋮      "keyPattern": ".*(mystuff).*",
        ⋮      "redaction": {
        ⋮        "method": "replace",
-       ⋮        "text": "[filtered: sensitive keys]"
+       ⋮        "text": "[filtered]"
        ⋮      }
        ⋮    }
        ⋮  },
@@ -2314,7 +2314,7 @@ THd+9FBxiHLGXNKhG/FRSyREXEt+NyYIf/0cyByc9tNksat794ddUqnLOg0vwSkv
        ⋮      "keyPattern": ".*(myStuff).*",
        ⋮      "redaction": {
        ⋮        "method": "replace",
-       ⋮        "text": "[filtered: sensitive keys]"
+       ⋮        "text": "[filtered]"
        ⋮      }
        ⋮    }
        ⋮  },
@@ -2760,7 +2760,7 @@ THd+9FBxiHLGXNKhG/FRSyREXEt+NyYIf/0cyByc9tNksat794ddUqnLOg0vwSkv
        ⋮      "keyPattern": ".*(session_key).*",
        ⋮      "redaction": {
        ⋮        "method": "replace",
-       ⋮        "text": "[filtered: sensitive keys]"
+       ⋮        "text": "[filtered]"
        ⋮      }
        ⋮    }
        ⋮  },
