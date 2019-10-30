01c18edd486e0a601551ebc2a1a53d9ae389c464
Armin Ronacher <armin.ronacher@active-4.com>
2019-10-30 17:57:22 +0100

    fix: reformat

--- cabi/include/semaphore.h
+++ cabi/include/semaphore.h
@@ -3,11 +3,12 @@
 #ifndef SEMAPHORE_H_INCLUDED
 #define SEMAPHORE_H_INCLUDED
 
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-/*
+/**
  * Represents all possible error codes
  */
 enum SemaphoreErrorCode {
@@ -24,19 +25,19 @@ typedef uint32_t SemaphoreErrorCode;
 
 typedef struct SemaphoreGeoIpLookup SemaphoreGeoIpLookup;
 
-/*
+/**
  * Represents a public key in semaphore.
  */
 typedef struct SemaphorePublicKey SemaphorePublicKey;
 
-/*
+/**
  * Represents a secret key in semaphore.
  */
 typedef struct SemaphoreSecretKey SemaphoreSecretKey;
 
 typedef struct SemaphoreStoreNormalizer SemaphoreStoreNormalizer;
 
-/*
+/**
  * Represents a buffer.
  */
 typedef struct {
@@ -45,7 +46,7 @@ typedef struct {
   bool owned;
 } SemaphoreBuf;
 
-/*
+/**
  * Represents a string.
  */
 typedef struct {
@@ -54,7 +55,7 @@ typedef struct {
   bool owned;
 } SemaphoreStr;
 
-/*
+/**
  * Represents a key pair from key generation.
  */
 typedef struct {
@@ -62,14 +63,14 @@ typedef struct {
   SemaphoreSecretKey *secret_key;
 } SemaphoreKeyPair;
 
-/*
+/**
  * Represents a uuid.
  */
 typedef struct {
   uint8_t data[16];
 } SemaphoreUuid;
 
-/*
+/**
  * Frees a semaphore buf.
  *
  * If the buffer is marked as not owned then this function does not
@@ -77,31 +78,31 @@ typedef struct {
  */
 void semaphore_buf_free(SemaphoreBuf *b);
 
-/*
+/**
  * Creates a challenge from a register request and returns JSON.
  */
 SemaphoreStr semaphore_create_register_challenge(const SemaphoreBuf *data,
                                                  const SemaphoreStr *signature,
                                                  uint32_t max_age);
 
-/*
+/**
  * Clears the last error.
  */
 void semaphore_err_clear(void);
 
-/*
+/**
  * Returns the panic information as string.
  */
 SemaphoreStr semaphore_err_get_backtrace(void);
 
-/*
+/**
  * Returns the last error code.
  *
  * If there is no error, 0 is returned.
  */
 SemaphoreErrorCode semaphore_err_get_last_code(void);
 
-/*
+/**
  * Returns the last error message.
  *
  * If there is no error an empty string is returned.  This allocates new memory
@@ -109,12 +110,12 @@ SemaphoreErrorCode semaphore_err_get_last_code(void);
  */
 SemaphoreStr semaphore_err_get_last_message(void);
 
-/*
+/**
  * Generates a secret, public key pair.
  */
 SemaphoreKeyPair semaphore_generate_key_pair(void);
 
-/*
+/**
  * Randomly generates an relay id
  */
 SemaphoreUuid semaphore_generate_relay_id(void);
@@ -123,42 +124,42 @@ void semaphore_geoip_lookup_free(SemaphoreGeoIpLookup *lookup);
 
 SemaphoreGeoIpLookup *semaphore_geoip_lookup_new(const char *path);
 
-/*
+/**
  * Given just the data from a register response returns the
  * conained relay id without validating the signature.
  */
 SemaphoreUuid semaphore_get_register_response_relay_id(const SemaphoreBuf *data);
 
-/*
+/**
  * Initializes the library
  */
 void semaphore_init(void);
 
 bool semaphore_is_glob_match(const SemaphoreBuf *value, const SemaphoreStr *pat, uint32_t flags);
 
-/*
+/**
  * Frees a public key.
  */
 void semaphore_publickey_free(SemaphorePublicKey *spk);
 
-/*
+/**
  * Parses a public key from a string.
  */
 SemaphorePublicKey *semaphore_publickey_parse(const SemaphoreStr *s);
 
-/*
+/**
  * Converts a public key into a string.
  */
 SemaphoreStr semaphore_publickey_to_string(const SemaphorePublicKey *spk);
 
-/*
+/**
  * Verifies a signature
  */
 bool semaphore_publickey_verify(const SemaphorePublicKey *spk,
                                 const SemaphoreBuf *data,
                                 const SemaphoreStr *sig);
 
-/*
+/**
  * Verifies a signature
  */
 bool semaphore_publickey_verify_timestamp(const SemaphorePublicKey *spk,
@@ -168,22 +169,22 @@ bool semaphore_publickey_verify_timestamp(const SemaphorePublicKey *spk,
 
 SemaphoreStr semaphore_scrub_event(const SemaphoreStr *config, const SemaphoreStr *event);
 
-/*
+/**
  * Frees a secret key.
  */
 void semaphore_secretkey_free(SemaphoreSecretKey *spk);
 
-/*
+/**
  * Parses a secret key from a string.
  */
 SemaphoreSecretKey *semaphore_secretkey_parse(const SemaphoreStr *s);
 
-/*
+/**
  * Verifies a signature
  */
 SemaphoreStr semaphore_secretkey_sign(const SemaphoreSecretKey *spk, const SemaphoreBuf *data);
 
-/*
+/**
  * Converts a secret key into a string.
  */
 SemaphoreStr semaphore_secretkey_to_string(const SemaphoreSecretKey *spk);
@@ -198,7 +199,7 @@ SemaphoreStoreNormalizer *semaphore_store_normalizer_new(const SemaphoreStr *con
 SemaphoreStr semaphore_store_normalizer_normalize_event(SemaphoreStoreNormalizer *normalizer,
                                                         const SemaphoreStr *event);
 
-/*
+/**
  * Frees a semaphore str.
  *
  * If the string is marked as not owned then this function does not
@@ -206,7 +207,7 @@ SemaphoreStr semaphore_store_normalizer_normalize_event(SemaphoreStoreNormalizer
  */
 void semaphore_str_free(SemaphoreStr *s);
 
-/*
+/**
  * Creates a semaphore str from a c string.
  *
  * This sets the string to owned.  In case it's not owned you either have
@@ -219,12 +220,12 @@ void semaphore_test_panic(void);
 
 bool semaphore_translate_legacy_python_json(SemaphoreStr *event);
 
-/*
+/**
  * Returns true if the uuid is nil
  */
 bool semaphore_uuid_is_nil(const SemaphoreUuid *uuid);
 
-/*
+/**
  * Formats the UUID into a string.
  *
  * The string is newly allocated and needs to be released with
@@ -232,7 +233,7 @@ bool semaphore_uuid_is_nil(const SemaphoreUuid *uuid);
  */
 SemaphoreStr semaphore_uuid_to_str(const SemaphoreUuid *uuid);
 
-/*
+/**
  * Validates a register response.
  */
 SemaphoreStr semaphore_validate_register_response(const SemaphorePublicKey *pk,
