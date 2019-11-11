348e14b3308c3d8c80f5147de2fdfea8394ce2bf
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-11-11 16:31:59 +0100

    fix: Formatting

--- tests/load-tests/locustfile.py
+++ tests/load-tests/locustfile.py
@@ -7,6 +7,7 @@ from random import random
 
 
 from yaml import load
+
 try:
     from yaml import CLoader as Loader, CDumper as Dumper, CFullLoader as FullLoader
 except ImportError:
@@ -40,14 +41,16 @@ class EventsCache(object):
             event_dir = cls._get_event_directory()
             events_file = os.path.join(event_dir, "event_index.yml")
             try:
-                with open(events_file, 'r') as file:
+                with open(events_file, "r") as file:
                     file_names = load(file, Loader=FullLoader)
 
             except Exception as err:
-                raise ValueError("Invalid event index file, event_index.yml", events_file)
+                raise ValueError(
+                    "Invalid event index file, event_index.yml", events_file
+                )
 
             for file_name in file_names:
-                file_path = os.path.join(event_dir, file_name + '.json')
+                file_path = os.path.join(event_dir, file_name + ".json")
                 with open(file_path, "r") as file:
                     content = file.read()
                     event_list.append(content)
@@ -82,19 +85,22 @@ class EventsCache(object):
 
 
 def small_event_task(task_set: TaskSet):
-    msg_body = EventsCache.get_event_by_name('small_event')
+    msg_body = EventsCache.get_event_by_name("small_event")
     send_message(task_set, msg_body)
+
 
 def medium_event_task(task_set: TaskSet):
-    msg_body = EventsCache.get_event_by_name('medium_event')
+    msg_body = EventsCache.get_event_by_name("medium_event")
     send_message(task_set, msg_body)
+
 
 def large_event_task(task_set: TaskSet):
-    msg_body = EventsCache.get_event_by_name('large_event')
+    msg_body = EventsCache.get_event_by_name("large_event")
     send_message(task_set, msg_body)
 
+
 def bad_event_task(task_set: TaskSet):
-    msg_body = EventsCache.get_event_by_name('bad_event')
+    msg_body = EventsCache.get_event_by_name("bad_event")
     send_message(task_set, msg_body)
 
 
@@ -103,10 +109,10 @@ def send_message(task_set, msg_body):
     num_projects = task_set.num_projects
     config = task_set.locust.config
 
-    use_fake_projects = config['use_fake_projects']
+    use_fake_projects = config["use_fake_projects"]
 
     if not use_fake_projects:
-        projects = config['projects']
+        projects = config["projects"]
         num_available_projects = len(projects)
         if num_projects > num_available_projects:
             num_projects = num_available_projects
@@ -115,29 +121,33 @@ def send_message(task_set, msg_body):
         project_idx = floor(random() * num_projects)
     if use_fake_projects:
         project_id = project_idx
-        project_key = config["fake_projects"]['key']
+        project_key = config["fake_projects"]["key"]
     else:
-        project_cfg = config['projects'][project_idx]
-        project_id = project_cfg['id']
-        project_key = project_cfg['key']
+        project_cfg = config["projects"][project_idx]
+        project_id = project_cfg["id"]
+        project_key = project_cfg["key"]
 
     url = "/api/{}/store/".format(project_id)
     headers = {
         "X-Sentry-Auth": "Sentry sentry_key={},sentry_version=7".format(project_key),
-        "Content-Type": "application/json; charset=UTF-8"
+        "Content-Type": "application/json; charset=UTF-8",
     }
     return client.post(url, headers=headers, data=msg_body)
 
 
 def _get_semaphore_address(config):
-    semaphore_settings = config.get('semaphore', {})
-    host = semaphore_settings.get('host')
-    port = semaphore_settings.get('port')
+    semaphore_settings = config.get("semaphore", {})
+    host = semaphore_settings.get("host")
+    port = semaphore_settings.get("port")
 
     if host is None:
-        raise "Missing semaphore.host settings from config file:{}".format(_get_config_file_path())
+        raise "Missing semaphore.host settings from config file:{}".format(
+            _get_config_file_path()
+        )
     if port is None:
-        raise "Missing semaphore.port settings from config file:{}".format(_get_config_file_path())
+        raise "Missing semaphore.port settings from config file:{}".format(
+            _get_config_file_path()
+        )
 
     return "{}:{}".format(host, port)
 
@@ -149,10 +159,12 @@ def _get_config():
     """
     file_name = _get_config_file_path()
     try:
-        with open(file_name, 'r') as file:
+        with open(file_name, "r") as file:
             return load(file, Loader=FullLoader)
     except Exception as err:
-        print("Error while getting the configuration file:{}\n {}".format(file_name, err))
+        print(
+            "Error while getting the configuration file:{}\n {}".format(file_name, err)
+        )
         raise ValueError("Invalid configuration")
 
 
