7bcce2945cf58b811fc14ade2d924512b62ea42c
Jan Michael Auer <jan.auer@sentry.io>
2019-12-03 17:00:38 +0100

    fix: Fix segfaults because reasons

--- tests/integration/fixtures/processing.py
+++ tests/integration/fixtures/processing.py
@@ -184,5 +184,5 @@ class EventsConsumer(ConsumerBase):
         assert event.error() is None
 
         v = msgpack.unpackb(event.value(), raw=False, use_list=False)
-        assert v["type"][0] == "event"
+        assert v["type"] == "event"
         return json.loads(v["payload"].decode("utf8")), v
