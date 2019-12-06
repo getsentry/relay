e3a694d400e2b6e69e68a72dcd7b5a1151c34e8b
Anton Ovchinnikov <anton@tonyo.info>
2019-12-06 15:36:15 +0100

    debug: Allow core dumping

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# Enable core dumps. Requires privileged mode.
+if [[ "${RELAY_ENABLE_COREDUMPS:-}" == "1" ]]; then
+  mkdir -p /var/dumps
+  chmod a+rwx /var/dumps
+  echo '/var/dumps/core.%h.%e.%t' > /proc/sys/kernel/core_pattern
+  ulimit -c unlimited
+fi
+
 # For compatibility with older images
 if [ "$1" == "bash" ]; then
   set -- bash
