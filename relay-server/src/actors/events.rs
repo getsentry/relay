2f0463b184dcd9534e23985a83e2ad92b5796bc1
Jan Michael Auer <jan.auer@sentry.io>
2020-02-06 13:39:26 +0100

    feat(protocol): Do not require signal codes in mechanism

--- relay-server/src/actors/events.rs
+++ relay-server/src/actors/events.rs
@@ -459,8 +459,10 @@ impl EventProcessor {
 
         let mut store_processor = StoreProcessor::new(store_config, geoip_lookup);
         metric!(timer(RelayTimers::EventProcessingProcess), {
-            process_value(event, &mut store_processor, ProcessingState::root())
-                .map_err(|_| ProcessingError::InvalidTransaction)?;
+            process_value(event, &mut store_processor, ProcessingState::root()).map_err(|e| {
+                log::trace!("{}", e);
+                ProcessingError::InvalidTransaction
+            })?;
         });
 
         // Event filters assume a normalized event. Unfortunately, this requires us to run
