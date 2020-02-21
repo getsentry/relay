43ce51b4e074aaaf7093779dfe046e3dc22e30b2
Armin Ronacher <armin.ronacher@active-4.com>
2020-02-21 10:09:52 +0100

    feat(sessions): Remove attributes and add degraded state (#490)

--- relay-general/src/protocol/session.rs
+++ relay-general/src/protocol/session.rs
@@ -19,8 +19,10 @@ pub enum SessionStatus {
     Exited,
     /// The session resulted in an application crash.
     Crashed,
-    /// The session an unexpected abrupt termination (not crashing).
+    /// The session had an unexpected abrupt termination (not crashing).
     Abnormal,
+    /// The session is degraded (errors happened but it did not crash)
+    Degraded,
 }
 
 impl Default for SessionStatus {
@@ -42,6 +44,7 @@ impl FromStr for SessionStatus {
             "ok" => SessionStatus::Ok,
             "crashed" => SessionStatus::Crashed,
             "abnormal" => SessionStatus::Abnormal,
+            "degraded" => SessionStatus::Degraded,
             "exited" => SessionStatus::Exited,
             _ => return Err(ParseSessionStatusError),
         })
@@ -54,6 +57,7 @@ impl fmt::Display for SessionStatus {
             SessionStatus::Ok => write!(f, "ok"),
             SessionStatus::Crashed => write!(f, "crashed"),
             SessionStatus::Abnormal => write!(f, "abnormal"),
+            SessionStatus::Degraded => write!(f, "degraded"),
             SessionStatus::Exited => write!(f, "exited"),
         }
     }
@@ -67,12 +71,6 @@ fn is_empty_string(opt: &Option<String>) -> bool {
 #[serde(default)]
 #[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
 pub struct SessionAttributes {
-    /// The operating system name, corresponding to the os context.
-    pub os: Option<String>,
-    /// The full operating system version string, corresponding to the os context.
-    pub os_version: Option<String>,
-    /// The device famility identifier, corresponding to the device context.
-    pub device_family: Option<String>,
     /// The release version string.
     pub release: Option<String>,
     /// The environment identifier.
@@ -81,11 +79,7 @@ pub struct SessionAttributes {
 
 impl SessionAttributes {
     fn is_empty(&self) -> bool {
-        is_empty_string(&self.os)
-            && is_empty_string(&self.os_version)
-            && is_empty_string(&self.device_family)
-            && is_empty_string(&self.release)
-            && is_empty_string(&self.environment)
+        is_empty_string(&self.release) && is_empty_string(&self.environment)
     }
 }
 
@@ -96,15 +90,6 @@ fn default_sequence() -> u64 {
         .as_millis() as u64
 }
 
-fn default_sample_rate() -> f32 {
-    1.0
-}
-
-#[allow(clippy::trivially_copy_pass_by_ref)]
-fn is_default_sample_rate(rate: &f32) -> bool {
-    (*rate - default_sample_rate()) < std::f32::EPSILON
-}
-
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct SessionUpdate {
     /// The session identifier.
@@ -112,7 +97,7 @@ pub struct SessionUpdate {
     pub session_id: Uuid,
     /// The distinct identifier.
     #[serde(rename = "did", default)]
-    pub distinct_id: Uuid,
+    pub distinct_id: Option<String>,
     /// An optional logical clock.
     #[serde(rename = "seq", default = "default_sequence")]
     pub sequence: u64,
@@ -120,12 +105,6 @@ pub struct SessionUpdate {
     pub timestamp: DateTime<Utc>,
     /// The timestamp of when the session itself started.
     pub started: DateTime<Utc>,
-    /// The sample rate.
-    #[serde(
-        default = "default_sample_rate",
-        skip_serializing_if = "is_default_sample_rate"
-    )]
-    pub sample_rate: f32,
     /// An optional duration of the session so far.
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub duration: Option<f64>,
@@ -144,13 +123,7 @@ pub struct SessionUpdate {
 impl SessionUpdate {
     /// Parses a session update from JSON.
     pub fn parse(payload: &[u8]) -> Result<Self, serde_json::Error> {
-        let mut session = serde_json::from_slice::<Self>(payload)?;
-
-        if session.distinct_id.is_nil() {
-            session.distinct_id = session.session_id;
-        }
-
-        Ok(session)
+        serde_json::from_slice(payload)
     }
 
     /// Serializes a session update back into JSON.
@@ -173,7 +146,7 @@ mod tests {
 
         let output = r#"{
   "sid": "8333339f-5675-4f89-a9a0-1c935255ab58",
-  "did": "8333339f-5675-4f89-a9a0-1c935255ab58",
+  "did": null,
   "seq": 4711,
   "timestamp": "2020-02-07T15:17:00Z",
   "started": "2020-02-07T14:16:00Z",
@@ -182,11 +155,10 @@ mod tests {
 
         let update = SessionUpdate {
             session_id: "8333339f-5675-4f89-a9a0-1c935255ab58".parse().unwrap(),
-            distinct_id: "8333339f-5675-4f89-a9a0-1c935255ab58".parse().unwrap(),
+            distinct_id: None,
             sequence: 4711, // this would be a timestamp instead
             timestamp: "2020-02-07T15:17:00Z".parse().unwrap(),
             started: "2020-02-07T14:16:00Z".parse().unwrap(),
-            sample_rate: 1.0,
             duration: None,
             status: SessionStatus::Ok,
             attributes: SessionAttributes::default(),
@@ -206,17 +178,13 @@ mod tests {
     fn test_session_roundtrip() {
         let json = r#"{
   "sid": "8333339f-5675-4f89-a9a0-1c935255ab58",
-  "did": "b3ef3211-58a4-4b36-a9a1-5a55df0d9aaf",
+  "did": "foobarbaz",
   "seq": 42,
   "timestamp": "2020-02-07T15:17:00Z",
   "started": "2020-02-07T14:16:00Z",
-  "sample_rate": 2.0,
   "duration": 1947.49,
   "status": "exited",
   "attrs": {
-    "os": "iOS",
-    "os_version": "13.3.1",
-    "device_family": "iPhone12,3",
     "release": "sentry-test@1.0.0",
     "environment": "production"
   }
@@ -224,17 +192,13 @@ mod tests {
 
         let update = SessionUpdate {
             session_id: "8333339f-5675-4f89-a9a0-1c935255ab58".parse().unwrap(),
-            distinct_id: "b3ef3211-58a4-4b36-a9a1-5a55df0d9aaf".parse().unwrap(),
+            distinct_id: Some("foobarbaz".into()),
             sequence: 42,
             timestamp: "2020-02-07T15:17:00Z".parse().unwrap(),
             started: "2020-02-07T14:16:00Z".parse().unwrap(),
-            sample_rate: 2.0,
             duration: Some(1947.49),
             status: SessionStatus::Exited,
             attributes: SessionAttributes {
-                os: Some("iOS".to_owned()),
-                os_version: Some("13.3.1".to_owned()),
-                device_family: Some("iPhone12,3".to_owned()),
                 release: Some("sentry-test@1.0.0".to_owned()),
                 environment: Some("production".to_owned()),
             },
--- relay-server/src/actors/store.rs
+++ relay-server/src/actors/store.rs
@@ -27,6 +27,11 @@ use crate::utils::instant_to_unix_timestamp;
 
 type ThreadedProducer = rdkafka::producer::ThreadedProducer<DefaultProducerContext>;
 
+lazy_static::lazy_static! {
+    static ref NAMESPACE_DID: Uuid =
+        Uuid::new_v5(&Uuid::NAMESPACE_URL, b"https://sentry.io/#did");
+}
+
 #[derive(Fail, Debug)]
 pub enum StoreError {
     #[fail(display = "failed to send kafka message")]
@@ -43,6 +48,11 @@ pub enum StoreError {
 pub struct StoreForwarder {
     config: Arc<Config>,
     producer: Arc<ThreadedProducer>,
+}
+
+fn make_distinct_id(s: &str) -> Uuid {
+    s.parse()
+        .unwrap_or_else(|_| Uuid::new_v5(&NAMESPACE_DID, s.as_bytes()))
 }
 
 impl StoreForwarder {
@@ -169,16 +179,16 @@ impl StoreForwarder {
             org_id,
             project_id,
             session_id: session.session_id,
-            distinct_id: session.distinct_id,
+            distinct_id: session
+                .distinct_id
+                .as_deref()
+                .map(make_distinct_id)
+                .unwrap_or_default(),
             seq: session.sequence,
             timestamp: types::datetime_to_timestamp(session.timestamp),
             started: types::datetime_to_timestamp(session.started),
-            sample_rate: session.sample_rate,
             duration: session.duration,
             status: session.status,
-            os: session.attributes.os,
-            os_version: session.attributes.os_version,
-            device_family: session.attributes.device_family,
             release: session.attributes.release,
             environment: session.attributes.environment,
             retention_days: event_retention,
@@ -317,12 +327,8 @@ struct SessionKafkaMessage {
     seq: u64,
     timestamp: f64,
     started: f64,
-    sample_rate: f32,
     duration: Option<f64>,
     status: SessionStatus,
-    os: Option<String>,
-    os_version: Option<String>,
-    device_family: Option<String>,
     release: Option<String>,
     environment: Option<String>,
     retention_days: u16,
