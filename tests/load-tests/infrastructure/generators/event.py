5fe662df048f10ac5e34017e4d1b5563586274ff
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-05-22 11:49:47 +0200

    fix fingerprint schema violation

--- tests/load-tests/infrastructure/generators/event.py
+++ tests/load-tests/infrastructure/generators/event.py
@@ -29,7 +29,7 @@ def base_event_generator(
     event_generator = schema_generator(
         event_id=(lambda: uuid.uuid4().hex) if with_event_id else None,
         level=["error", "debug"] if with_level else None,
-        fingerprint=lambda: f"fingerprint{random.randrange(num_event_groups)}",
+        fingerprint=lambda: [f"fingerprint{random.randrange(num_event_groups)}"],
         transaction=[None, lambda: f"mytransaction{random.randrange(100)}"],
         logentry={"formatted":  sentence_generator()},
         logger=["foo.bar.baz", "bam.baz.bad", None],
