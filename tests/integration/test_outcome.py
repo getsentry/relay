d497b92745f93990345369a939d1af4282200bd9
Radu Woinaroski <radu@sentry.io>
2020-06-03 14:56:37 +0200

    trying to figure out why outcome batches are not sent

--- tests/integration/test_outcome.py
+++ tests/integration/test_outcome.py
@@ -1,8 +1,11 @@
 import datetime
 import json
+import uuid
+
+import time
 
 
-def test_outcomes(relay_with_processing, kafka_consumer, mini_sentry):
+def test_outcomes_processing(relay_with_processing, kafka_consumer, mini_sentry):
     relay = relay_with_processing()
     relay.wait_relay_healthcheck()
     outcomes = kafka_consumer("outcomes")
@@ -53,3 +56,81 @@ def test_outcomes(relay_with_processing, kafka_consumer, mini_sentry):
         "remote_addr": "127.0.0.1",
     }
     assert outcome == expected
+
+
+def _send_event(relay):
+    event_id = uuid.uuid1().hex
+    message_text = "some message {}".format(datetime.datetime.now())
+    relay.send_event(
+        42,
+        {
+            "event_id": event_id,
+            "message": message_text,
+            "extra": {"msg_text": message_text},
+        },
+    )
+    return event_id
+
+
+def test_outcomes_non_processing(relay, relay_with_processing, mini_sentry):
+    config = {
+        "relay": {
+            "emit_outcomes": True,
+            "max_outcome_batch_size": 1,
+            "max_outcome_interval_millsec": 1,
+        }
+    }
+
+    relay = relay(mini_sentry, config)
+    relay.wait_relay_healthcheck()
+    # hack mini_sentry configures project 42 (remove the configuration so that we get an error for project 42)
+    mini_sentry.project_configs[42] = None
+
+    event_ids = []
+    for i in range(1):
+        event_id = _send_event(relay)
+        event_ids.append(event_id)
+
+    events = mini_sentry.captured_events
+
+    assert events.empty()  # no events received since all have been for an invalid project id
+    outcomes = mini_sentry.captured_outcomes
+    assert outcomes.qsize() == 1
+
+
+def test_outcomes_non_processing_batching(relay, mini_sentry):
+    config = {
+        "relay": {
+            "emit_outcomes": True,
+            "max_outcome_batch_size": 3,
+            "max_outcome_interval_millsec": 3600 * 1000,  # an hour
+        }
+    }
+
+    relay = relay(config)
+    relay.wait_relay_healthcheck()
+    # hack mini_sentry configures project 42 (remove the configuration so that we get an error for project 42)
+    mini_sentry.project_configs[42] = None
+
+    event_ids = []
+    for i in range(2):
+        event_id = _send_event(relay)
+        event_ids.append(event_id)
+
+    # nothing should be sent at this time
+    outcomes = mini_sentry.captured_outcomes
+    assert outcomes.qsize() == 0
+
+    event_id = _send_event(relay)
+    event_ids.append(event_id)
+
+    # now we should have received a batch with all the events
+    outcomes = mini_sentry.captured_outcomes
+    assert outcomes.qsize() == 1
+
+    outcomes_batch = outcomes.get(block=False)
+
+    outcomes = outcomes_batch.get("outcomes")
+    assert len(outcomes) == 3
+    actual_event_ids = [ outcome.get("event_id") for outcome in outcomes]
+    assert event_ids == actual_event_ids
