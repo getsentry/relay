e62b3c898f6218f9eeaf0c3539081fef23a68b6a
Floris Bruynooghe <flub@sentry.io>
2020-07-17 17:03:15 +0200

    Do not raise exceptions in teardown (#663)
    Any exception in teardown is an error, not a test failure.  Instead
    modify the runtest protocol call step to perform the checks that the
    fixture wants to do at the end of the tests.
    
    The marker system is a great way to transfer state on the test item
    since it uses a plublic API to both add the information as well as
    access it.  The marker itself is not specifically tied to this
    fixture.
    
    This also modifies the default run to include a report of failed
    tests.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -161,3 +161,21 @@ def json_fixture_provider():
         return _JsonFixtureProvider(test_file_path)
 
     return inner
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_call(item):
+    """This handles the extra_failure_checks mark to perform further assertions.
+
+    The mark can be added e.g. by a fixture that wants to run
+    something that can raise pytest.fail.Exception after the test has
+    completed.
+
+    Such checks will not be run when the test already failed, just
+    like multiple assertions within a test will stop the test
+    execution at the first failure.
+    """
+    yield
+    for marker in item.iter_markers("extra_failure_checks"):
+        for check_func in marker.kwargs.get("checks", []):
+            check_func()
