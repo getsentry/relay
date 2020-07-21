ddd81d45892d517eaf706785f8f235542a018616
Jan Michael Auer <mail@jauer.org>
2020-07-21 12:57:55 +0200

    ref(config): Clear up confusion around mebibytes

--- relay-config/src/lib.rs
+++ relay-config/src/lib.rs
@@ -1,10 +1,10 @@
 //! Configuration for the Relay CLI and server.
 #![warn(missing_docs)]
 
+mod byte_size;
 mod config;
-mod types;
 mod upstream;
 
+pub use crate::byte_size::*;
 pub use crate::config::*;
-pub use crate::types::*;
 pub use crate::upstream::*;
