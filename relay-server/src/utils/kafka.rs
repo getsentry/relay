a666ded391f9b4c0ea804b8aabdfa16cd31d2c58
Anton Ovchinnikov <anton@tonyo.info>
2020-07-24 11:58:48 +0200

    More comments

--- relay-server/src/utils/kafka.rs
+++ relay-server/src/utils/kafka.rs
@@ -5,15 +5,22 @@ use relay_common::{metric, LogError};
 
 use crate::metrics::RelayCounters;
 
+/// Kafka producer context that logs producer errors
 pub struct CaptureErrorContext;
 
 impl ClientContext for CaptureErrorContext {}
 
 impl ProducerContext for CaptureErrorContext {
     type DeliveryOpaque = ();
+
+    /// This method is called after attempting to send a message to Kafka.
+    /// It's called asynchronously for every message, so we want to handle errors explicitly here.
     fn delivery(&self, result: &DeliveryResult, _delivery_opaque: Self::DeliveryOpaque) {
         if let Err((error, _message)) = result {
-            log::error!("callback producer error: {}", LogError(error));
+            log::error!(
+                "failed to produce message to Kafka (delivery callback): {}",
+                LogError(error)
+            );
 
             metric!(counter(RelayCounters::ProcessingProduceError) += 1);
         }
