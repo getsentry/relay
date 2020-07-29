5174e6751d61461c01e31d208b8d8a6374d6093a
Floris Bruynooghe <flub@sentry.io>
2020-07-29 18:16:25 +0200

    First step in editing code names
    This is pretty basic, but works.

--- examples/edit_minidump.rs
+++ examples/edit_minidump.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use failure::Fail;
+use scroll::Pread;
 use structopt::clap::AppSettings;
 use structopt::StructOpt;
 
@@ -34,15 +35,38 @@ fn main(argv: CliArgs) -> Result<()> {
         .map(|t| t.raw.stack.memory.rva)
         .collect();
 
-    // We would like to modify `data` directly, `mem_list` however refers to data
-    // non-mutably so we can not directly iterate over the `mem_list` and collect the memory
-    // descriptors instead.
+    // First we need to collect all the locations we would like to mutate.  The Minidump are
+    // its related types borrow the data immutably, however they also have "descriptor"
+    // structs which point at locations inside the minidup without borrowing any data from
+    // them.  So we first collect all those descriptors using the normal API before starting
+    // the mutate the data using the descriptors.
+
+    // Collect the descriptors for mutating memory regions.
     let mem_list: minidump::MinidumpMemoryList = dump
         .get_stream()
         .map_err(|e| e.compat())
         .context("Failed to parse memory regions from minidump")?;
     let mem_descriptors: Vec<minidump::format::MINIDUMP_MEMORY_DESCRIPTOR> =
         mem_list.iter().map(|mem| mem.desc).collect();
+
+    // Collect the descriptors to mutate the filenames in referenced modules.
+    let mod_list: minidump::MinidumpModuleList = dump
+        .get_stream()
+        .map_err(|e| e.compat())
+        .context("Failed to parse modules from minidump")?;
+    let mut file_name_descriptors: Vec<UTF16Descriptor> = Vec::new();
+    for module in mod_list.iter() {
+        let name_size: u32 = data.pread_with(module.raw.module_name_rva as usize, dump.endian)?;
+        file_name_descriptors.push(UTF16Descriptor {
+            rva: module.raw.module_name_rva + std::mem::size_of::<u32>() as u32,
+            size: name_size,
+        });
+        // TODO: add debug names
+        // TODO: deduplicate descriptors.  E.g. a code and debug name could both point to
+        // the same memory.
+    }
+
+    // Time to modify things!
 
     for mem_desc in mem_descriptors {
         let kind = if stack_rvas.contains(&mem_desc.memory.rva) {
@@ -58,9 +82,23 @@ fn main(argv: CliArgs) -> Result<()> {
         scrub_memory_region(kind, dest);
     }
 
+    for desc in file_name_descriptors {
+        let range = std::ops::Range {
+            start: desc.rva as usize,
+            end: (desc.rva + desc.size) as usize,
+        };
+        scrub_code_name(&mut data[range]);
+    }
+
     std::fs::write(argv.output, data)?;
 
     Ok(())
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct UTF16Descriptor {
+    rva: u32,
+    size: u32,
 }
 
 enum MemoryKind {
@@ -75,6 +113,23 @@ fn scrub_memory_region(kind: MemoryKind, data: &mut [u8]) {
         MemoryKind::Stack => (),
         MemoryKind::NonStack => null_data(data),
     }
+}
+
+/// Modify a module's code name
+///
+/// The `data` is a UTF16LE encoded string.  Any modifications must keep it valid and can
+/// not change the size of it.
+// TODO: should really type-enforce this.
+fn scrub_code_name(data: &mut [u8]) {
+    assert!(data.len() % 2 == 0);
+    let letter = b"a\x00"; // 'a' in UTF16LE
+    let mut new: Vec<u8> = Vec::new();
+    for _ in data.chunks(2) {
+        new.push(letter[0]);
+        new.push(letter[1]);
+    }
+    assert_eq!(new.len(), data.len());
+    data.copy_from_slice(new.as_slice());
 }
 
 /// Zero-out all data in the slice.
