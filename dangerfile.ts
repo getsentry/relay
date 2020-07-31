8519493e291c1b7321e1089237ff7c06aea0a43c
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-07-31 15:52:03 +0200

    add changelog logic

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,19 @@
-import {message, danger} from "danger"
+import { danger, fail, message, schedule, warn } from 'danger';
 
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
+
+export default async () => {
+  if (!danger.github) {
+    return;
+  }
+
+  const hasChangelog = danger.git.modified_files.indexOf('CHANGELOG.md') !== -1;
+  const hasPyChangelog = danger.git.modified_files.indexOf('py/CHANGELOG.md') !== -1;
+
+  const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes('#trivial');
+
+  if (!hasChangelog !hasPyChangelog && !isTrivial) {
+    warn('Please add a changelog entry in either CHANGELOG.md or py/CHANGELOG.md, or add #trivial to the PR title.');
+  }
+}
