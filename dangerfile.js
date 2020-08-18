0f72eab000997aa3f4dff118bde908cd30de6bc5
Jan Michael Auer <mail@jauer.org>
2020-08-18 12:18:17 +0200

    fix: JavaScript syntax

--- dangerfile.js
+++ dangerfile.js
@@ -5,9 +5,9 @@ const hasPyChangelog = files.indexOf("py/CHANGELOG.md") !== -1;
 const ERROR_MESSAGE = `
 Please consider adding a changelog entry for the next release:
 
-For changes exposed to the Python package, please add an entry to ``py/CHANGELOG.md``. This includes, but is not limited to event normalization, PII scrubbing, and the protocol.
+For changes exposed to the Python package, please add an entry to \`py/CHANGELOG.md\`. This includes, but is not limited to event normalization, PII scrubbing, and the protocol.
 
-For changes to the Relay server, please add an entry to ``CHANGELOG.md``:
+For changes to the Relay server, please add an entry to \`CHANGELOG.md\`:
  1. For new user-visible functionality under "Features"
  2. For user-visible bug fixes under "Bug Fixes"
  3. For features and bug fixes for internal operation (including processing mode) under "Internal"
