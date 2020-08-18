3b6673655e41b0eaa582277d4a660c5036fced48
Jan Michael Auer <mail@jauer.org>
2020-08-18 12:26:31 +0200

    ref: Tweak the message

--- dangerfile.js
+++ dangerfile.js
@@ -6,14 +6,14 @@ const ERROR_MESSAGE =
   "Please consider adding a changelog entry for the next release.";
 
 const DETAILS = `
-For changes exposed to the Python package, please add an entry to \`py/CHANGELOG.md\`. This includes, but is not limited to event normalization, PII scrubbing, and the protocol.
+For changes exposed to the _Python package_, please add an entry to \`py/CHANGELOG.md\`. This includes, but is not limited to event normalization, PII scrubbing, and the protocol.
 
-For changes to the Relay server, please add an entry to \`CHANGELOG.md\`:
- 1. For new user-visible functionality under "Features"
- 2. For user-visible bug fixes under "Bug Fixes"
- 3. For features and bug fixes for internal operation (including processing mode) under "Internal"
+For changes to the _Relay server_, please add an entry to \`CHANGELOG.md\` under the following heading:
+ 1. **Features**: For new user-visible functionality.
+ 2. **Bug Fixes**: For user-visible bug fixes.
+ 3. **Internal**: For features and bug fixes in internal operation, especially processing mode.
 
-If none of the above apply, you can opt out by adding #skip-changelog to the PR description.
+If none of the above apply, you can opt out by adding _#skip-changelog_ to the PR description.
 `;
 
 const skipChangelog =
