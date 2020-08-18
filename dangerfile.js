b358bbed9bce715e27fd5b6f35e10b558e2768e1
Jan Michael Auer <mail@jauer.org>
2020-08-18 12:15:57 +0200

    ref: Update the message

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,24 @@
-const hasChangelog = danger.git.modified_files.indexOf('CHANGELOG.md') !== -1;
-const hasPyChangelog = danger.git.modified_files.indexOf('py/CHANGELOG.md') !== -1;
+const files = danger.git.modified_files;
+const hasChangelog = files.indexOf("CHANGELOG.md") !== -1;
+const hasPyChangelog = files.indexOf("py/CHANGELOG.md") !== -1;
 
-const skipChangelog = danger.github && (danger.github.pr.body + danger.github.pr.title).includes('#skip-changelog');
+const ERROR_MESSAGE = `
+Please consider adding a changelog entry for the next release:
+
+For changes exposed to the Python package, please add an entry to ``py/CHANGELOG.md``. This includes, but is not limited to event normalization, PII scrubbing, and the protocol.
+
+For changes to the Relay server, please add an entry to ``CHANGELOG.md``:
+ 1. For new user-visible functionality under "Features"
+ 2. For user-visible bug fixes under "Bug Fixes"
+ 3. For features and bug fixes for internal operation (including processing mode) under "Internal"
+
+If none of the above apply, you can opt out by adding #skip-changelog to the PR description.
+`;
+
+const skipChangelog =
+  danger.github &&
+  (danger.github.pr.body + danger.github.pr.title).includes("#skip-changelog");
 
 if (!hasChangelog && !hasPyChangelog && !skipChangelog) {
-  warn('Please add a changelog entry in either CHANGELOG.md or py/CHANGELOG.md, or add #skip-changelog to the PR description or title.');
+  error(ERROR_MESSAGE);
 }
