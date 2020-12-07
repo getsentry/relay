4cddbff1c1f0772ec7d72603b4f768a52c5444e8
RaduW <RaduW@users.noreply.github.com>
2020-12-07 10:54:27 +0000

    deploy: aeb9df046af614f431106abbbb3905cad12436ea

--- implementors/failure/trait.Fail.js
+++ implementors/failure/trait.Fail.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
 implementors["relay_auth"] = [{"text":"impl Fail for ParseRelayVersionError","synthetic":false,"types":[]},{"text":"impl Fail for KeyParseError","synthetic":false,"types":[]},{"text":"impl Fail for UnpackError","synthetic":false,"types":[]}];
-implementors["relay_common"] = [{"text":"impl Fail for ParseEventTypeError","synthetic":false,"types":[]},{"text":"impl Fail for ParseSpanStatusError","synthetic":false,"types":[]}];
 implementors["relay_config"] = [{"text":"impl Fail for ConfigError","synthetic":false,"types":[]},{"text":"impl Fail for ConfigErrorKind","synthetic":false,"types":[]},{"text":"impl Fail for UpstreamError","synthetic":false,"types":[]},{"text":"impl Fail for UpstreamParseError","synthetic":false,"types":[]}];
 implementors["relay_general"] = [{"text":"impl Fail for ScrubMinidumpError","synthetic":false,"types":[]},{"text":"impl Fail for UnknownValueTypeError","synthetic":false,"types":[]},{"text":"impl Fail for ParseSessionStatusError","synthetic":false,"types":[]},{"text":"impl Fail for InvalidRegVal","synthetic":false,"types":[]},{"text":"impl Fail for ParseLevelError","synthetic":false,"types":[]},{"text":"impl Fail for ProcessingAction","synthetic":false,"types":[]}];
 implementors["relay_quotas"] = [{"text":"impl Fail for RateLimitingError","synthetic":false,"types":[]}];
