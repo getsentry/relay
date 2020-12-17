1a5970bcd4482d171a9278c170603cbbdad4aa4d
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-12-17 15:16:12 +0100

    fix(user-report): Make all fields but event-id optional

--- relay-general/src/protocol/user_report.rs
+++ relay-general/src/protocol/user_report.rs
@@ -8,9 +8,15 @@ pub struct UserReport {
     /// The event ID for which this user feedback is created.
     pub event_id: EventId,
     /// The user's name.
+    // Empty strings are in fact stored in the database already
+    #[serde(default)]
     pub name: String,
     /// The user's email address.
+    // Empty strings are in fact stored in the database already
+    #[serde(default)]
     pub email: String,
     /// Comments supplied by the user.
+    // Empty strings are in fact stored in the database already
+    #[serde(default)]
     pub comments: String,
 }
