1345fd7e1c8d66c7ff3f728c82c5d55e0a7b0517
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-12-17 16:26:41 +0100

    allow null

--- relay-general/src/protocol/user_report.rs
+++ relay-general/src/protocol/user_report.rs
@@ -9,9 +9,9 @@ use serde::{Deserialize, Serialize};
 /// accepts goes.
 ///
 /// For example, `{"email": null}` is only invalid because `UserReport(email=None).save()` is. SDKs
-/// may neither send this (we can relax this), but more importantly the ingest consumer may never
-/// receive this... because it would end up crashing the ingest consumer (while in older versions
-/// of Sentry it would simply crash the endpoint).
+/// may neither send this (historically, in Relay we relaxed this already), but more importantly
+/// the ingest consumer may never receive this... because it would end up crashing the ingest
+/// consumer (while in older versions of Sentry it would simply crash the endpoint).
 ///
 /// The database/model schema is a bunch of not-null strings that have (pgsql) defaults, so that's
 /// how we end up with this struct definition.
@@ -20,12 +20,20 @@ pub struct UserReport {
     /// The event ID for which this user feedback is created.
     pub event_id: EventId,
     /// The user's name.
-    #[serde(default)]
+    #[serde(default, deserialize_with = "null_to_default")]
     pub name: String,
     /// The user's email address.
-    #[serde(default)]
+    #[serde(default, deserialize_with = "null_to_default")]
     pub email: String,
     /// Comments supplied by the user.
-    #[serde(default)]
+    #[serde(default, deserialize_with = "null_to_default")]
     pub comments: String,
+}
+
+fn null_to_default<'de, D>(deserializer: D) -> Result<Priority, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let opt = Option::deserialize(deserializer)?;
+    Ok(opt.unwrap_or_else(Priority::lowest))
 }
