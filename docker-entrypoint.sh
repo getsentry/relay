605d6be54f59ffcd17a85da1ce9ad02c21e8889f
Anton Ovchinnikov <anton@tonyo.info>
2021-01-05 17:17:43 +0100

    deploy(docker): Add prestart endpoint to docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,6 +16,28 @@ if [[ -n "${RELAY_DELAY_STARTUP_SECONDS:-}" ]]; then
   sleep "${RELAY_DELAY_STARTUP_SECONDS}"
 fi
 
+# Make sure that a specified URL (e.g. the upstream or a proxy sidecar) is reachable before starting.
+# Only 200 response is accepted as success.
+if [[ -n "${RELAY_PRESTART_ENDPOINT:-}" ]]; then
+  max_retry="${RELAY_PRESTART_MAX_RETRIES:-120}"
+  for attempt in $(seq 1 "$max_retry"); do
+    status=$(curl --max-time 1 --silent --output /dev/null \
+                  --write-out "%{http_code}" \
+                  -H 'Connection: close' \
+                  "${RELAY_PRESTART_ENDPOINT}" \
+              || true)
+    if [[ "$status" == "200" ]]; then
+      break
+    fi
+    echo "Waiting for a 200 response from ${RELAY_PRESTART_ENDPOINT}"
+    sleep 1
+  done
+  if [[ "$attempt" == "$max_retry" ]]; then
+    echo "The prestart endpoint has not returned 200 for $max_retry seconds, exiting!"
+    exit 1
+  fi
+fi
+
 # For compatibility with older images
 if [ "$1" == "bash" ]; then
   set -- bash "${@:2}"
