73b6e0997c048634611930e49ae26b5ee6857baa
Anton Ovchinnikov <anton@tonyo.info>
2021-01-05 19:10:47 +0100

    Parametrize better

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,22 +20,23 @@ fi
 # Only 200 response is accepted as success.
 if [[ -n "${RELAY_PRESTART_ENDPOINT:-}" ]]; then
   max_retry="${RELAY_PRESTART_MAX_RETRIES:-120}"
-  for attempt in $(seq 1 "$max_retry"); do
-    status=$(curl --max-time 1 --silent --output /dev/null \
-                  --write-out "%{http_code}" \
+  curl_timeout="${RELAY_PRESTART_REQUEST_TIMEOUT:-1}"
+  for attempt in $(seq 0 "${max_retry}"); do
+    if [[ "${attempt}" == "${max_retry}" ]]; then
+      echo "The prestart endpoint has not returned 200 after ${max_retry} attempts, exiting!"
+      exit 1
+    fi
+    status=$(curl --max-time "${curl_timeout}" --show-error --silent \
+                  --output /dev/null --write-out "%{http_code}" \
                   -H 'Connection: close' \
                   "${RELAY_PRESTART_ENDPOINT}" \
               || true)
-    if [[ "$status" == "200" ]]; then
+    if [[ "${status}" == "200" ]]; then
       break
     fi
-    echo "Waiting for a 200 response from ${RELAY_PRESTART_ENDPOINT}"
+    echo "Waiting for a 200 response from ${RELAY_PRESTART_ENDPOINT}, got ${status}"
     sleep 1
   done
-  if [[ "$attempt" == "$max_retry" ]]; then
-    echo "The prestart endpoint has not returned 200 for $max_retry seconds, exiting!"
-    exit 1
-  fi
 fi
 
 # For compatibility with older images
