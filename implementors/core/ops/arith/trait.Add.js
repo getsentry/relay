e30027943f62b296b79c8591a60bbdd0478a4656
RyanSkonnord <RyanSkonnord@users.noreply.github.com>
2021-03-16 15:11:54 +0000

    deploy: dacdb0017131435e0cd3c8a181008a8c03531b4b

--- implementors/core/ops/arith/trait.Add.js
+++ implementors/core/ops/arith/trait.Add.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {};
-implementors["relay_common"] = [{"text":"impl Add&lt;Duration&gt; for UnixTimestamp","synthetic":false,"types":[]}];
 implementors["relay_general"] = [{"text":"impl Add&lt;Duration&gt; for Timestamp","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
