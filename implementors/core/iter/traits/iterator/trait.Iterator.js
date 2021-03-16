6e54b3e849e52cb318d4818f9bdd527a0cc29419
jan-auer <jan-auer@users.noreply.github.com>
2021-03-16 16:31:52 +0000

    deploy: 6dce4586131488fe24d340a21aea0730cf70bcdd

--- implementors/core/iter/traits/iterator/trait.Iterator.js
+++ implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["relay_metrics"] = [{"text":"impl Iterator for ParseMetrics&lt;'_&gt;","synthetic":false,"types":[]}];
 implementors["relay_quotas"] = [{"text":"impl&lt;'a&gt; Iterator for RateLimitsIter&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl Iterator for RateLimitsIntoIter","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/iter/traits/marker/trait.FusedIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["relay_metrics"] = [{"text":"impl FusedIterator for ParseMetrics&lt;'_&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
