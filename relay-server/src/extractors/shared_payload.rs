665f3ee8b765e512400f7ae77d3650cb497cbf1b
Michal Kuffa <michal.kuffa@sentry.io>
2021-05-21 20:39:04 +0200

    ref(log): Remove excessive info `done` log for each request

--- relay-server/src/extractors/shared_payload.rs
+++ relay-server/src/extractors/shared_payload.rs
@@ -54,7 +54,6 @@ impl Stream for SharedPayload {
     #[inline]
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
         if self.done.load(Ordering::Relaxed) {
-            relay_log::info!("done");
             return Ok(Async::Ready(None));
         }
 
