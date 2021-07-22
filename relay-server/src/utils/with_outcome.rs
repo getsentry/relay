e21d59d362c9e8611fcdf8aa61e093e92e97eb69
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2021-07-22 16:03:55 +0200

    fix signature

--- relay-server/src/utils/with_outcome.rs
+++ relay-server/src/utils/with_outcome.rs
@@ -5,37 +5,40 @@ use futures::prelude::*;
 use crate::actors::envelopes::EnvelopeContext;
 use crate::actors::outcome::{DiscardReason, Outcome, OutcomeProducer};
 
-pub trait SendWithOutcome<Msg>
-where
-    Msg: Message,
-{
-    fn send_with_outcome_error(
+pub trait SendWithOutcome<A> {
+    fn send_with_outcome_error<Msg>(
         &self,
         message: Msg,
-        envelope_context: &EnvelopeContext,
+        envelope_context: EnvelopeContext,
         outcome_producer: Addr<OutcomeProducer>,
-    ) -> ResponseFuture<Msg::Result, ()>;
+    ) -> ResponseFuture<Msg::Result, ()>
+    where
+        Msg: Message,
+        A: Handler<Msg>,
+        A::Context: ToEnvelope<A, Msg>,
+        Msg: Message + Send + 'static,
+        Msg::Result: Send;
 }
 
-impl<A, Msg> SendWithOutcome<Msg> for Addr<A>
+impl<A> SendWithOutcome<A> for Addr<A>
 where
     A: Actor,
-    A: Handler<Msg>,
-    A::Context: ToEnvelope<A, Msg>,
-    Msg: Message + Send + 'static,
-    Msg::Result: Send,
 {
-    fn send_with_outcome_error(
+    fn send_with_outcome_error<Msg>(
         &self,
         message: Msg,
-        envelope_context: &EnvelopeContext,
+        envelope_context: EnvelopeContext,
         outcome_producer: Addr<OutcomeProducer>,
-    ) -> ResponseFuture<Msg::Result, ()> {
-        let envelope_context = *envelope_context;
-        let fut = self.send(message).map_err(move |err| {
+    ) -> ResponseFuture<Msg::Result, ()>
+    where
+        A: Handler<Msg>,
+        A::Context: ToEnvelope<A, Msg>,
+        Msg: Message + Send + 'static,
+        Msg::Result: Send,
+    {
+        let fut = self.send(message).map_err(move |_| {
             envelope_context
                 .send_outcomes(Outcome::Invalid(DiscardReason::Internal), outcome_producer);
-            ()
         });
         Box::new(fut)
     }
