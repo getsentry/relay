c977beb132b23a6f14ff591519d774640acd96a4
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-11-10 13:44:59 +0100

    fix(logging): Demote parsing error to debug level
    This was temporarily logged as error to investigate what's going on in
    production, but this is really not a fatal error.

--- relay-server/src/actors/envelopes.rs
+++ relay-server/src/actors/envelopes.rs
@@ -1912,7 +1912,7 @@ impl Handler<ProcessMetrics> for EnvelopeProcessor {
                         project_cache.do_send(MergeBuckets::new(public_key, buckets));
                     }
                     Err(error) => {
-                        relay_log::error!("failed to parse metric bucket: {}", LogError(&error));
+                        relay_log::debug!("failed to parse metric bucket: {}", LogError(&error));
                         metric!(counter(RelayCounters::MetricBucketsParsingFailed) += 1);
                     }
                 }
