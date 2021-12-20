586505db5d954e138091c098ea21ab2e6196e50c
jjbayer <jjbayer@users.noreply.github.com>
2021-12-20 11:39:41 +0000

    deploy: 4639455273ed8caccf5c11b929ef42d3a7ac6d61

--- implementors/actix/actor/trait.Actor.js
+++ implementors/actix/actor/trait.Actor.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["relay_common_actors"] = [{"text":"impl Actor for <a class=\"struct\" href=\"relay_common_actors/controller/struct.Controller.html\" title=\"struct relay_common_actors::controller::Controller\">Controller</a>","synthetic":false,"types":["relay_common_actors::controller::Controller"]}];
 implementors["relay_metrics"] = [{"text":"impl Actor for <a class=\"struct\" href=\"relay_metrics/struct.Aggregator.html\" title=\"struct relay_metrics::Aggregator\">Aggregator</a>","synthetic":false,"types":["relay_metrics::aggregation::Aggregator"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ relay_common_actors/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["controller","Defines an actor to control system run and shutdown."]]});
