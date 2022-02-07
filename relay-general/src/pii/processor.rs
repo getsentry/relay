1760f1b6b07051e57099d1a27794d7b08354df78
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2022-02-07 17:40:30 +0100

    Add unit test to replicate the bug

--- relay-general/src/pii/processor.rs
+++ relay-general/src/pii/processor.rs
@@ -356,7 +356,7 @@ fn insert_replacement_chunks(rule: &RuleRef, text: &str, output: &mut Vec<Chunk<
 
 #[cfg(test)]
 use {
-    crate::pii::PiiConfig,
+    crate::pii::{PiiConfig, ReplaceRedaction},
     crate::processor::process_value,
     crate::protocol::{
         Addr, DebugImage, DebugMeta, Event, ExtraValue, Headers, LogEntry, NativeDebugImage,
@@ -944,4 +944,28 @@ fn test_ip_address_hashing_does_not_overwrite_id() {
     );
 
     assert_eq!(user.id.value().unwrap().as_str(), "123");
+}
+
+#[test]
+fn test_replace_replaced_text() {
+    let chunks = vec![Chunk::Redaction {
+        text: "[Filtered]".into(),
+        rule_id: "@password:filter".into(),
+        ty: RemarkType::Substituted,
+    }];
+    let rule = RuleRef {
+        id: String::from("@password:filter"),
+        origin: String::from("@password:filter"),
+        ty: RuleType::Password,
+        redaction: Redaction::Replace(ReplaceRedaction {
+            text: String::from("[Filtered]"),
+        }),
+    };
+    let res = apply_regex_to_chunks(
+        chunks.clone(),
+        &rule,
+        &Regex::new(r#".*"#).unwrap(),
+        ReplaceBehavior::Value,
+    );
+    assert_eq!(chunks, res);
 }
