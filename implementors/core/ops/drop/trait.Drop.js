e72e54c2101f5b827eb135d3b0797c72a424216c
jan-auer <jan-auer@users.noreply.github.com>
2022-04-07 16:10:43 +0000

    deploy: 9ad1c1defda5f78b88e4abe54e5a6ead73e25822

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["relay_metrics"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"relay_metrics/struct.Aggregator.html\" title=\"struct relay_metrics::Aggregator\">Aggregator</a>","synthetic":false,"types":["relay_metrics::aggregation::Aggregator"]}];
+implementors["relay_metrics"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"relay_metrics/struct.Aggregator.html\" title=\"struct relay_metrics::Aggregator\">Aggregator</a>","synthetic":false,"types":["relay_metrics::aggregation::Aggregator"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
