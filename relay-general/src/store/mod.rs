c5c5a6ebd28f7cea44f95b1af2335dc3975f6c6b
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-04-19 15:00:21 +0200

    use is_valid_platform

--- relay-general/src/store/mod.rs
+++ relay-general/src/store/mod.rs
@@ -25,7 +25,7 @@ pub use self::geo::{GeoIpError, GeoIpLookup};
 pub use normalize::breakdowns::{
     get_breakdown_measurements, BreakdownConfig, BreakdownsConfig, SpanOperationsConfig,
 };
-pub use normalize::normalize_dist;
+pub use normalize::{is_valid_platform, normalize_dist};
 pub use transactions::{get_measurement, get_transaction_op, validate_timestamps};
 
 /// The config for store.
--- relay-sampling/src/lib.rs
+++ relay-sampling/src/lib.rs
@@ -16,7 +16,7 @@ use serde_json::{Number, Value};
 use relay_common::{EventType, ProjectKey, Uuid};
 use relay_filter::GlobPatterns;
 use relay_general::protocol::Event;
-use relay_general::store::{get_measurement, get_transaction_op, validate_timestamps};
+use relay_general::store;
 
 /// Defines the type of dynamic rule, i.e. to which type of events it will be applied and how.
 #[derive(Debug, Copy, Clone, Serialize, Deserialize, Eq, PartialEq)]
@@ -432,7 +432,7 @@ impl FieldValueProvider for Event {
                 None => Value::Null,
                 Some(s) => s.as_str().into(),
             },
-            "transaction.duration" => match (self.ty.value(), validate_timestamps(self)) {
+            "transaction.duration" => match (self.ty.value(), store::validate_timestamps(self)) {
                 (Some(&EventType::Transaction), Ok((start, end))) => {
                     let start = start.timestamp_millis();
                     let end = end.timestamp_millis();
@@ -441,17 +441,19 @@ impl FieldValueProvider for Event {
                 }
                 _ => Value::Null,
             },
-            "transaction.op" => match (self.ty.value(), get_transaction_op(self)) {
+            "transaction.op" => match (self.ty.value(), store::get_transaction_op(self)) {
                 (Some(&EventType::Transaction), Some(op_name)) => Value::String(op_name.to_owned()),
                 _ => Value::Null,
             },
-            "event.platform" => self
-                .platform
-                .value()
-                .map_or(Value::Null, |platform| Value::String(platform.to_owned())),
+            "event.platform" => match self.platform.value() {
+                Some(platform) if store::is_valid_platform(platform) => {
+                    Value::String(platform.to_owned())
+                }
+                _ => Value::Null,
+            },
             field_name if field_name.starts_with("transaction.measurements.") => {
                 let measurement_name = &field_name["transaction.measurements.".len()..];
-                if let Some(value) = get_measurement(self, measurement_name) {
+                if let Some(value) = store::get_measurement(self, measurement_name) {
                     value.into()
                 } else {
                     Value::Null
