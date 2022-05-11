71eeb0eeaaff99386fbb4b1e38e9a8e93dfdbdb5
Anton Pirker <anton.pirker@sentry.io>
2022-05-11 13:20:41 +0200

    Moved files around

---
+++ scripts/docker-build-lambda-extension.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+BUILD_IMAGE="sentry-lambda-extension-builder:latest"
+
+echo "Prepare Docker image for building..."
+docker build \
+    -f Dockerfile.lambda-extension \
+    -t "${BUILD_IMAGE}" .
+
+echo "Building the binary..."
+DOCKER_RUN_OPTS="
+  -v $(pwd):/work
+  $BUILD_IMAGE
+"
+
+docker run $DOCKER_RUN_OPTS \
+  cargo build --release --locked --target=x86_64-unknown-linux-gnu
