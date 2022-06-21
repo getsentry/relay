91163d1765b5890e4808f3f6d02c64c492ee042a
jjbayer <jjbayer@users.noreply.github.com>
2022-06-21 11:38:50 +0000

    deploy: 9412847ae0cd5691aaf209699ba3536244a9666f

--- relay_server/sidebar-items.js
+++ relay_server/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["run","Runs a relay web server and spawns all internal worker threads."]],"macro":[["project_states_version","The current version of the project states endpoint."]],"struct":[["ServerError","Common error type for the relay server."]]});
+initSidebarItems({"fn":[["run","Runs a relay web server and spawns all internal worker threads."]],"struct":[["ServerError","Common error type for the relay server."]]});
