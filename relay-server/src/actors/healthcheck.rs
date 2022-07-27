c2ca31979c5471cdbbc36ee61af559f0e1699a36
tobias-wilfert <tobias.wilfert@sentry.io>
2022-07-27 12:53:11 +0200

    Cleaned up healthchecck.rs

--- relay-server/src/actors/healthcheck.rs
+++ relay-server/src/actors/healthcheck.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use actix::SystemService;
 use futures03::compat::Future01CompatExt;
-use once_cell::sync::Lazy;
+use parking_lot::RwLock;
 use tokio::sync::{mpsc, oneshot};
 
 use relay_config::{Config, RelayMode};
@@ -13,55 +13,45 @@ use relay_system::{Controller, Shutdown};
 use crate::actors::upstream::{IsAuthenticated, IsNetworkOutage, UpstreamRelay};
 use crate::statsd::RelayGauges;
 
-static ADDRESS: Lazy<std::sync::Mutex<Option<Addr<HealthCheckMessage>>>> =
-    Lazy::new(|| std::sync::Mutex::new(None));
+lazy_static::lazy_static! {
+    /// Singleton of the `Healthcheck` service.
+    static ref ADDRESS: RwLock<Option<Addr<HealthcheckMessage>>> = RwLock::new(None);
+}
 
-// Wrapper for our data and oneshot chanel(needed to retransmit back the reply)
+/// Internal wrapper of a message sent through an `Addr` with return channel.
 #[derive(Debug)]
 struct Message<T> {
     data: T,
+    // TODO(tobias): This is hard-coded to return `bool`.
     responder: oneshot::Sender<bool>,
 }
 
-// Custom error that acts as an abstraction
+/// An error when [sending](Addr::send) a message to a service fails.
 pub struct SendError;
 
-// tx = transmitter, mpcs = multi-producer, single consumer
+/// Channel for sending public messages into a service.
+///
+/// To send a message, use [`Addr::send`].
 #[derive(Clone, Debug)]
 pub struct Addr<T> {
     tx: mpsc::UnboundedSender<Message<T>>,
 }
 
-// Impl the send function which can then be used to send a shut down message
-/*
-impl<T> ShutdownReceiver for Addr<T>
-where
-    T: From<Shutdown> + std::marker::Send, // FIXME: Check if that is an actual solution
-{
-    fn send<'a>(
-        &'a self,
-        message: Shutdown,
-    ) -> Pin<Box<dyn std::future::Future<Output = ()> + Send + 'a>> {
-        Box::pin(self.send(message.into()).map(|_| ()))
-    }
-
-    fn foo(&self) -> Box<Self> {
-        Bow::new(self.clone())
-    }
-}
-*/
-
-// Name send chosen to keep it compatible with the existing code that is invoking it
 impl<T> Addr<T> {
+    /// Sends an asynchronous message to the service and waits for the response.
+    ///
+    /// The result of the message does not have to be awaited. The message will be delivered and
+    /// handled regardless. The communication channel with the service is unbounded, so backlogs
+    /// could occur when sending too many messages.
+    ///
+    /// Sending the message can fail with `Err(SendError)` if the service has shut down.
     pub async fn send(&self, data: T) -> Result<bool, SendError> {
-        let (responder, rx) = oneshot::channel(); // Generate the oneshot channel
-        let message = Message { data, responder }; // Construct the message
-        self.tx.send(message).map_err(|_| SendError)?; // Send the message and return an error if it fails
-        rx.await.map_err(|_| SendError) // await the result and return it, if anything goes wrong, throw a SendError
+        let (responder, rx) = oneshot::channel();
+        let message = Message { data, responder };
+        self.tx.send(message).map_err(|_| SendError)?;
+        rx.await.map_err(|_| SendError)
     }
 }
-
-// Unchanged
 
 pub struct Healthcheck {
     is_shutting_down: bool,
@@ -69,7 +59,16 @@ pub struct Healthcheck {
 }
 
 impl Healthcheck {
-    // Unchanged
+    /// Returns the singleton instance of the Healthcheck service.
+    ///
+    /// Prior to using this, the service must be started using [`Healthcheck::start`].
+    pub fn from_registry() -> Addr<HealthcheckMessage> {
+        ADDRESS.read().as_ref().unwrap().clone()
+    }
+
+    /// Creates a new instance of the Healthcheck service.
+    ///
+    /// The service does not run. To run the service, use [`start`](Self::start).
     pub fn new(config: Arc<Config>) -> Self {
         Healthcheck {
             is_shutting_down: false,
@@ -78,7 +77,7 @@ impl Healthcheck {
     }
 
     async fn handle_is_healthy(&mut self, message: IsHealthy) -> bool {
-        let upstream = UpstreamRelay::from_registry(); // Legacy, still needed for now
+        let upstream = UpstreamRelay::from_registry();
 
         if self.config.relay_mode() == RelayMode::Managed {
             let fut = upstream.send(IsNetworkOutage).compat();
@@ -92,25 +91,23 @@ impl Healthcheck {
         match message {
             IsHealthy::Liveness => true, // Liveness always returns true
             IsHealthy::Readiness => {
-                // If we are shutting down we are no longer Ready
                 if self.is_shutting_down {
                     return false;
                 }
 
-                // If we need authentication check that we have authentication
                 if self.config.requires_auth()
                     && !upstream
                         .send(IsAuthenticated)
-                        .compat() // Convert from the old futures to the new futures
+                        .compat()
                         .await
                         .unwrap_or(false)
                 {
                     return false;
                 }
 
-                Aggregator::from_registry() // Still legacy?
+                Aggregator::from_registry()
                     .send(AcceptsMetrics)
-                    .compat() // Convert from the old futures to the new futures
+                    .compat()
                     .await
                     .unwrap_or(false)
             }
@@ -119,114 +116,46 @@ impl Healthcheck {
 
     fn handle_shutdown(&mut self) -> bool {
         self.is_shutting_down = true;
-        true
+        true // TODO: This should go away once messages are more generic
     }
 
-    async fn handle(&mut self, message: HealthCheckMessage) -> bool {
+    async fn handle(&mut self, message: HealthcheckMessage) -> bool {
         match message {
-            HealthCheckMessage::Health(message) => self.handle_is_healthy(message).await,
-            HealthCheckMessage::Shutdown => self.handle_shutdown(),
+            HealthcheckMessage::Health(message) => self.handle_is_healthy(message).await,
+            HealthcheckMessage::Shutdown => self.handle_shutdown(),
         }
     }
 
-    // Start the healthCheck actor, returns an Addr so that it can be reached
-    pub fn start(mut self) -> Addr<HealthCheckMessage> {
-        // Make the channel to use for communication
+    /// Start this service, returning an [`Addr`] for communication.
+    pub fn start(mut self) -> Addr<HealthcheckMessage> {
         let (tx, mut rx) = mpsc::unbounded_channel::<Message<_>>();
-        let addr = Addr { tx: tx.clone() };
-        *ADDRESS.lock().unwrap() = Some(addr.clone());
+
+        let addr = Addr { tx };
+        *ADDRESS.write() = Some(addr.clone());
 
         tokio::spawn(async move {
-            // While incoming messages are still being received
             while let Some(message) = rx.recv().await {
+                // TODO(tobias): This does not allow for concurrent execution.
                 let response = self.handle(message.data).await;
                 message.responder.send(response).ok();
             }
         });
 
-        // let mut wrx = Controller::subscribe_v2(); <- this works but could it be because we don't await it?
-
-        // When receiving a shutdown message forward it to our mpsc channel
+        // Forward shutdown signals to the main message channel
+        let shutdown_addr = addr.clone();
         tokio::spawn(async move {
-            // Get the receiving end of the watch channel from the Controller
-            // let _guard = handle.enter();
-            let mut wrx = Controller::subscribe_v2().await; // <-  Is this were the error comes from
+            let mut shutdown_rx = Controller::subscribe_v2().await;
 
-            loop {
-                if wrx.changed().await.is_ok() && wrx.borrow().is_none() {
-                    continue;
+            while shutdown_rx.changed().await.is_ok() {
+                if shutdown_rx.borrow_and_update().is_some() {
+                    let _ = shutdown_addr.send(HealthcheckMessage::Shutdown);
                 }
-                let (responder, _) = oneshot::channel();
-                let _ = tx.send(Message {
-                    data: HealthCheckMessage::Shutdown,
-                    responder,
-                });
-                break;
             }
         });
+
         addr
     }
-
-    // Maybe give back our own Addr and say that it can be converted into the actix Addr?
-    // Because Healthcheck is not an actor so doesn't work?
-    // So escentially need to make a converter? or leave it an Actor
-    pub fn from_registry() -> Addr<HealthCheckMessage> {
-        // TODO: Here need to return the address from the HealthCheck
-        // HTF do we do this, surely we now need to make a registry
-        let guard = ADDRESS.lock().unwrap();
-        match *guard {
-            Some(ref addr) => addr.clone(),
-            None => panic!(),
-        }
-    }
 }
-
-/*
-impl Actor for Healthcheck {
-    type Context = Context<Self>;
-
-    fn started(&mut self, context: &mut Self::Context) {
-        Controller::subscribe(context.address());
-    }
-}
-
-impl Supervised for Healthcheck {}
-
-impl SystemService for Healthcheck {}
-
-impl Default for Healthcheck {
-    fn default() -> Self {
-        unimplemented!("register with the SystemRegistry instead")
-    }
-}
-*/
-
-// - More sophisticated solution -
-/*
-trait Handler<T> {
-    type Result;
-
-    fn handle<'a>(
-        &'a mut self,
-        message: T,
-    ) -> Pin<Box<dyn std::future::Future<Output = Self::Result> + Send + 'a>>;
-}
-
-impl Handler<Shutdown> for Healthcheck {
-    type Result = Result<(), ()>;
-
-    fn handle<'a>(
-        &'a mut self,
-        _message: Shutdown,
-    ) -> Pin<Box<dyn std::future::Future<Output = Self::Result> + Send + 'a>> {
-        let result_future = async move {
-            self.is_shutting_down = true;
-            Ok(())
-        };
-        Box::pin(result_future)
-    }
-}
-*/
 
 #[derive(Clone, Debug)]
 pub enum IsHealthy {
@@ -238,72 +167,19 @@ pub enum IsHealthy {
 }
 
 #[derive(Clone, Debug)]
-pub enum HealthCheckMessage {
+pub enum HealthcheckMessage {
     Health(IsHealthy),
     Shutdown,
 }
 
-// Allows for shutdown to be converted to HealthCheckMessage
-// Needed so that that the shutdown message can be send to multiple actors in the same way
-impl From<Shutdown> for HealthCheckMessage {
+impl From<Shutdown> for HealthcheckMessage {
     fn from(_: Shutdown) -> Self {
-        HealthCheckMessage::Shutdown
+        HealthcheckMessage::Shutdown
     }
 }
 
-impl From<IsHealthy> for HealthCheckMessage {
+impl From<IsHealthy> for HealthcheckMessage {
     fn from(is_healthy: IsHealthy) -> Self {
         Self::Health(is_healthy)
     }
 }
-
-/*
-impl Message for IsHealthy {
-    type Result = Result<bool, ()>;
-}
-
-impl Handler<IsHealthy> for Healthcheck {
-    type Result = ResponseFuture<bool, ()>;
-
-    fn handle(&mut self, message: IsHealthy, context: &mut Self::Context) -> Self::Result {
-        let upstream = UpstreamRelay::from_registry();
-
-        if self.config.relay_mode() == RelayMode::Managed {
-            upstream
-                .send(IsNetworkOutage)
-                .map_err(|_| ())
-                .map(|is_network_outage| {
-                    metric!(
-                        gauge(RelayGauges::NetworkOutage) = if is_network_outage { 1 } else { 0 }
-                    );
-                })
-                .into_actor(self)
-                .spawn(context);
-        }
-
-        match message {
-            IsHealthy::Liveness => Box::new(future::ok(true)),
-            IsHealthy::Readiness => {
-                if self.is_shutting_down {
-                    return Box::new(future::ok(false));
-                }
-
-                let is_aggregator_full = Aggregator::from_registry()
-                    .send(AcceptsMetrics)
-                    .map_err(|_| ());
-                let is_authenticated: Self::Result = if self.config.requires_auth() {
-                    Box::new(upstream.send(IsAuthenticated).map_err(|_| ()))
-                } else {
-                    Box::new(future::ok(true))
-                };
-
-                Box::new(
-                    is_aggregator_full
-                        .join(is_authenticated)
-                        .map(|(a, b)| a && b),
-                )
-            }
-        }
-    }
-}
-*/
