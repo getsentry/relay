833c7339c62183e082466fe09131eb1e53406088
Colton Allen <colton.allen@sentry.io>
2022-08-16 14:08:35 -0500

    Add additional payload fields

--- tests/integration/test_replay_events.py
+++ tests/integration/test_replay_events.py
@@ -1,11 +1,36 @@
+import json
+
+
 def generate_replay_event():
     return {
         "replay_id": "d2132d31b39445f1938d7e21b6bf0ec4",
-        "sequence_id": 0,
         "type": "replay_event",
         "transaction": "/organizations/:orgId/performance/:eventSlug/",
         "start_timestamp": 1597976392.6542819,
+        "segment_id": 0,
+        "replay_start_timestamp": 1597976392.6542819,
         "timestamp": 1597976400.6189718,
+        "urls": ["sentry.io"],
+        "error_ids": ["1", "2"],
+        "trace_ids": ["3", "4"],
+        "dist": "1.12",
+        "platform": "Python",
+        "environment": "production",
+        "release": "version@1.3",
+        "tags": {"transaction": "/organizations/:orgId/performance/:eventSlug/"},
+        "sdk": {"name": "name", "version": "veresion"},
+        "user": {
+            "id": "123",
+            "username": "user",
+            "email": "user@site.com",
+            "ip_address": "127.0.0.1",
+        },
+        "requests": {
+            "url": None,
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15"
+            },
+        },
         "contexts": {
             "trace": {
                 "trace_id": "4C79F60C11214EB38604F4AE0781BFB2",
@@ -34,6 +59,10 @@ def test_replay_event_with_processing(
     assert replay_event["type"] == "replay_event"
     assert replay_event["replay_id"] == "d2132d31b39445f1938d7e21b6bf0ec4"
     assert replay_event_message["retention_days"] == 90
+
+    message = json.loads(bytes(replay_event_message["payload"]))
+    print(message)
+    assert False
 
 
 def test_replay_events_without_processing(mini_sentry, relay_chain):
