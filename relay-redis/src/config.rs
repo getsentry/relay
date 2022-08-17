d3b97781b46e5e7a745d4410720778fe02ad2e96
Oleksandr <olksdr@users.noreply.github.com>
2022-08-17 08:56:14 +0200

    feat(redis): Make connection pool configurable [INGEST-1557] (#1418)
    * feat(redis): Make connections pool configurable [INGEST-1557]
    
    Currently the maximum number of connections managed by the pool is
    hardcoded and set to the magic number of `24` which works, but we must
    make sure we have a posibility to finetune this parameter.
    
    This change adds `max_connections` option into the `Cluster` config,
    keeps the existing `Single` config server without changes for bakwards
    compatibility and also introduces the `SingleOpts` variant to configure single
    server with additional options (with e.g. `max_connections`).
    
    The default number of redis connetions in the pool is kept to `24`.
    
    This should allow us to keep existing configs without changes and still
    give the posibility for finetunning connections in the future.
    
    * ref(redis): keep the configuration in one place
    
    Since there is already configuration for few different types of the
    connection pools and redis clients, also most of the settings are
    repeatable - it is time to move all those config options if one place.
    
    Introducing `RedisConfigOptions` which will be a single place to gather
    all the configuration options, with sensible defaults.

--- relay-redis/src/config.rs
+++ relay-redis/src/config.rs
@@ -1,5 +1,26 @@
 use serde::{Deserialize, Serialize};
 
+const fn default_max_connections() -> u32 {
+    24
+}
+
+/// Additional configuration options for a redis client.
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[serde(default)]
+pub struct RedisConfigOptions {
+    /// Maximum number of connections managed by the pool.
+    #[serde(default = "default_max_connections")]
+    pub max_connections: u32,
+}
+
+impl Default for RedisConfigOptions {
+    fn default() -> Self {
+        Self {
+            max_connections: default_max_connections(),
+        }
+    }
+}
+
 /// Configuration for connecting a redis client.
 #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 #[serde(untagged)]
@@ -10,10 +31,87 @@ pub enum RedisConfig {
         ///
         /// This can also be a single node which is configured in cluster mode.
         cluster_nodes: Vec<String>,
+
+        /// Additional configuration options for the redis client and a connections pool.
+        #[serde(flatten)]
+        options: RedisConfigOptions,
     },
 
     /// Connect to a single Redis instance.
     ///
     /// Contains the `redis://` url to the node.
     Single(String),
+
+    /// Connect to a single Redis instance.
+    ///
+    /// Allows to provide more configuration options, e.g. `max_connections`.
+    SingleWithOpts {
+        /// Containes the `redis://` url to the node.
+        server: String,
+
+        /// Additional configuration options for the redis client and a connections pool.
+        #[serde(flatten)]
+        options: RedisConfigOptions,
+    },
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_redis_single_opts() {
+        let yaml = r###"
+server: "redis://127.0.0.1:6379"
+max_connections: 42
+"###;
+
+        let config: RedisConfig = serde_yaml::from_str(yaml)
+            .expect("Parsed processing redis config: single with options");
+
+        match config {
+            RedisConfig::SingleWithOpts { server, options } => {
+                assert_eq!(options.max_connections, 42);
+                assert_eq!(server, "redis://127.0.0.1:6379");
+            }
+            e => panic!("Expected RedisConfig::SingleWithOpts but got {:?}", e),
+        }
+    }
+
+    #[test]
+    fn test_redis_single_opts_default() {
+        let yaml = r###"
+server: "redis://127.0.0.1:6379"
+"###;
+
+        let config: RedisConfig = serde_yaml::from_str(yaml)
+            .expect("Parsed processing redis config: single with options");
+
+        match config {
+            RedisConfig::SingleWithOpts { options, .. } => {
+                // check if all the defaults are correctly set
+                assert_eq!(options.max_connections, 24);
+            }
+            e => panic!("Expected RedisConfig::SingleWithOpts but got {:?}", e),
+        }
+    }
+
+    // To make sure that we have backwards compatibility and still support the redis configuration
+    // when the single `redis://...` address is provided.
+    #[test]
+    fn test_redis_single() {
+        let yaml = r###"
+"redis://127.0.0.1:6379"
+"###;
+
+        let config: RedisConfig = serde_yaml::from_str(yaml)
+            .expect("Parsed processing redis config: single with options");
+
+        match config {
+            RedisConfig::Single(server) => {
+                assert_eq!(server, "redis://127.0.0.1:6379");
+            }
+            e => panic!("Expected RedisConfig::Single but got {:?}", e),
+        }
+    }
 }
