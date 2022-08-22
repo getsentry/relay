eaed0ad71c09f5186ff07f0bea454c8a53b4c013
Armin Ronacher <armin.ronacher@active-4.com>
2022-08-22 16:40:47 +0200

    Fetch project configs in endpoint for feature flags

--- relay-server/src/endpoints/feature_flags.rs
+++ relay-server/src/endpoints/feature_flags.rs
@@ -1,6 +1,8 @@
+use actix::prelude::*;
 use actix_web::{actix::ResponseFuture, HttpRequest, HttpResponse};
-use futures::{FutureExt, TryFutureExt};
+use futures01::Future;
 
+use crate::actors::project_cache::{GetProjectState, ProjectCache};
 use crate::endpoints::common::{self, BadStoreRequest};
 use crate::extractors::RequestMeta;
 use crate::service::{ServiceApp, ServiceState};
@@ -8,61 +10,64 @@ use crate::service::{ServiceApp, ServiceState};
 use relay_feature_flags::{EvaluationRule, EvaluationType, FeatureDump, FeatureFlag};
 
 fn fetch_feature_flags(
-    _meta: RequestMeta,
+    meta: RequestMeta,
     _request: HttpRequest<ServiceState>,
 ) -> ResponseFuture<HttpResponse, BadStoreRequest> {
-    let fut = async {
-        Ok(HttpResponse::Ok().json(FeatureDump {
-            feature_flags: [
-                (
-                    "accessToProfiling".into(),
-                    FeatureFlag {
-                        tags: Default::default(),
-                        evaluation: vec![
-                            EvaluationRule {
-                                ty: EvaluationType::Match,
-                                percentage: None,
-                                result: Some(true.into()),
-                                payload: None,
-                                tags: [("isSentryDev".into(), "true".into())].into(),
-                            },
-                            EvaluationRule {
-                                ty: EvaluationType::Rollout,
-                                percentage: Some(0.5),
-                                result: Some(true.into()),
-                                payload: None,
-                                tags: Default::default(),
-                            },
-                        ],
-                    },
-                ),
-                (
-                    "profilingEnabled".into(),
-                    FeatureFlag {
-                        tags: Default::default(),
-                        evaluation: vec![
-                            EvaluationRule {
-                                ty: EvaluationType::Match,
-                                percentage: None,
-                                result: Some(true.into()),
-                                payload: None,
-                                tags: [("isSentryDev".into(), "true".into())].into(),
-                            },
-                            EvaluationRule {
-                                ty: EvaluationType::Rollout,
-                                percentage: Some(0.05),
-                                result: Some(true.into()),
-                                payload: None,
-                                tags: Default::default(),
-                            },
-                        ],
-                    },
-                ),
-            ]
-            .into(),
-        }))
-    };
-    Box::new(fut.boxed_local().compat())
+    let fut = ProjectCache::from_registry()
+        .send(GetProjectState::new(meta.public_key()))
+        .map_err(|_| BadStoreRequest::ScheduleFailed)
+        .map(|state_result| {
+            let mut feature_flags = match state_result {
+                Ok(project_state) => project_state.config().feature_flags.clone(),
+                Err(_) => Default::default(),
+            };
+            feature_flags.insert(
+                "@@accessToProfiling".into(),
+                FeatureFlag {
+                    tags: Default::default(),
+                    evaluation: vec![
+                        EvaluationRule {
+                            ty: EvaluationType::Match,
+                            percentage: None,
+                            result: Some(true.into()),
+                            payload: None,
+                            tags: [("isSentryDev".into(), "true".into())].into(),
+                        },
+                        EvaluationRule {
+                            ty: EvaluationType::Rollout,
+                            percentage: Some(0.5),
+                            result: Some(true.into()),
+                            payload: None,
+                            tags: Default::default(),
+                        },
+                    ],
+                },
+            );
+            feature_flags.insert(
+                "@@profilingEnabled".into(),
+                FeatureFlag {
+                    tags: Default::default(),
+                    evaluation: vec![
+                        EvaluationRule {
+                            ty: EvaluationType::Match,
+                            percentage: None,
+                            result: Some(true.into()),
+                            payload: None,
+                            tags: [("isSentryDev".into(), "true".into())].into(),
+                        },
+                        EvaluationRule {
+                            ty: EvaluationType::Rollout,
+                            percentage: Some(0.05),
+                            result: Some(true.into()),
+                            payload: None,
+                            tags: Default::default(),
+                        },
+                    ],
+                },
+            );
+            HttpResponse::Ok().json(FeatureDump { feature_flags })
+        });
+    Box::new(fut)
 }
 
 pub fn configure_app(app: ServiceApp) -> ServiceApp {
