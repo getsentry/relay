6cc200a2802dc2e5043d5f9431f5b18b0577227c
Armin Ronacher <armin.ronacher@active-4.com>
2022-08-24 16:14:12 +0200

    Remove hardcoded flags

--- relay-server/src/endpoints/feature_flags.rs
+++ relay-server/src/endpoints/feature_flags.rs
@@ -1,5 +1,3 @@
-use std::collections::BTreeMap;
-
 use actix::prelude::*;
 use actix_web::{actix::ResponseFuture, HttpRequest, HttpResponse};
 use futures01::Future;
@@ -10,7 +8,7 @@ use crate::extractors::RequestMeta;
 use crate::service::{ServiceApp, ServiceState};
 use crate::utils::ErrorBoundary;
 
-use relay_feature_flags::{EvaluationRule, EvaluationType, FeatureDump, FeatureFlag, FlagKind};
+use relay_feature_flags::FeatureDump;
 
 fn fetch_feature_flags(
     meta: RequestMeta,
@@ -22,7 +20,7 @@ fn fetch_feature_flags(
         .send(GetProjectState::new(meta.public_key()))
         .map_err(|_| BadStoreRequest::ScheduleFailed)
         .map(|state_result| {
-            let mut feature_flags: BTreeMap<String, FeatureFlag> = match state_result {
+            let feature_flags = match state_result {
                 Ok(project_state) => project_state
                     .config()
                     .feature_flags
@@ -34,57 +32,6 @@ fn fetch_feature_flags(
                     .collect(),
                 Err(_) => Default::default(),
             };
-
-            // feed some defaults in for now.
-            feature_flags.insert(
-                "@@accessToProfiling".into(),
-                FeatureFlag {
-                    kind: FlagKind::Boolean,
-                    tags: Default::default(),
-                    evaluation: vec![
-                        EvaluationRule {
-                            ty: EvaluationType::Match,
-                            percentage: None,
-                            result: Some(true.into()),
-                            payload: None,
-                            tags: [("isSentryDev".into(), "true".into())].into(),
-                        },
-                        EvaluationRule {
-                            ty: EvaluationType::Rollout,
-                            percentage: Some(0.5),
-                            result: Some(true.into()),
-                            payload: None,
-                            tags: Default::default(),
-                        },
-                    ],
-                    enabled: true,
-                },
-            );
-            feature_flags.insert(
-                "@@profilingEnabled".into(),
-                FeatureFlag {
-                    kind: FlagKind::Boolean,
-                    tags: Default::default(),
-                    evaluation: vec![
-                        EvaluationRule {
-                            ty: EvaluationType::Match,
-                            percentage: None,
-                            result: Some(true.into()),
-                            payload: None,
-                            tags: [("isSentryDev".into(), "true".into())].into(),
-                        },
-                        EvaluationRule {
-                            ty: EvaluationType::Rollout,
-                            percentage: Some(0.05),
-                            result: Some(true.into()),
-                            payload: None,
-                            tags: Default::default(),
-                        },
-                    ],
-                    enabled: true,
-                },
-            );
-
             HttpResponse::Ok().json(FeatureDump { feature_flags })
         });
     Box::new(fut)
