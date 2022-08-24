6173c585644654ffc450e8d08350670befa412a3
jan-auer <jan-auer@users.noreply.github.com>
2022-08-24 06:02:55 +0000

    deploy: 7124b22c96e7b0e4e8629960aa8d7bd4fcc6c3ce

--- relay/utils/sidebar-items.js
+++ relay/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["get_theme","Returns the theme to use."],["prompt_value","Prompts for a value that has a default."],["prompt_value_no_default","Prompts for a value without a default."]],"struct":[["THEME",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["get_theme","Returns the theme to use."],["prompt_value","Prompts for a value that has a default."],["prompt_value_no_default","Prompts for a value without a default."]],"static":[["THEME",""]]};
