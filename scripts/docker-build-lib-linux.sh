0af58c6de4ae4acc610d54cf0db8c113c4d3784c
Oleksandr Kylymnychenko <oleksandr@sentry.io>
2022-08-31 17:18:17 +0200

    ref: Use one image for all the platforms
    These commit adds quite few significatn changes:
    * bump base image to manylinux2014 (which also bumps libc
      to 2.17)
    * use one image, `quay.io/pypa/manylinux2014_x86_64` for all the
      platforms
    * we create a builder image per platform with all the dependencies and
      tooling required to build the library
    * `using docker/build-push-action@v3` to build images on the fly if
      needed and cache them
    
    This allows to use added Dockefile to cross compile library also locally
    for all the supported platforms, with RPM-based linux distribution
    (Centos 7 is recomended)

--- scripts/docker-build-lib-linux.sh
+++ scripts/docker-build-lib-linux.sh
@@ -2,51 +2,46 @@
 
 set -ex
 
+IMAGE=${IMAGE:-manylinux2014_x86_64}
 TARGET=${TARGET:-aarch64}
 TARGET_LINKER=$(echo $TARGET | tr '[:lower:]' '[:upper:]')
-IMAGE_NAME="${IMAGE_NAME:-linux-crossbuild}-${TARGET}:latest"
+DEFAULT_BUILDER_NAME="relay-cabi-builder-${TARGET}:latest"
+BUILDER_NAME="${BUILDER_NAME:-${DEFAULT_BUILDER_NAME}}"
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
-MANYLINUX_IMAGE="manylinux2010"
-
-export CARGO_BUILD_TARGET="${TARGET}-unknown-linux-gnu"
-
-# Building AARCH64 separatelly using cross compilation on x86_64
-if [ $TARGET == "aarch64" ]; then
-  MANYLINUX_IMAGE="manylinux2014"
-
-  # build container
-  docker build --build-arg=TARGET="${TARGET}" --file py/Dockerfile.cross -t "$IMAGE_NAME" py/
-
-  # run the cross compilation
-  docker run \
-    --rm \
-    -w "/work" \
-    -v "$(pwd):/work" \
-    -e "CARGO_TARGET_${TARGET_LINKER}_UNKNOWN_LINUX_GNU_LINKER"="${TARGET}-linux-gnu-gcc" \
-    -e CARGO_BUILD_TARGET \
-    $IMAGE_NAME \
-      bash -c 'source ~/.bashrc && cargo build -p relay-cabi --release'
-
-  # Fix permissions for shared directories before manylinux run
-  sudo chown -R ${USER_ID}:${GROUP_ID} target/
-  # make sure we do not build the lib twice
-  export SKIP_RELAY_LIB_BUILD=1
+# Set additional env variables if the image is for another target
+# It means we must cross compile
+if [[ $IMAGE != *"${TARGET}"* ]]; then
+  IMAGE="manylinux2014_x86_64"
+  export "CARGO_TARGET_${TARGET_LINKER}_UNKNOWN_LINUX_GNU_LINKER"="${TARGET}-linux-gnu-gcc"
+  export CARGO_BUILD_TARGET="${TARGET}-unknown-linux-gnu"
 fi
 
-MANYLINUX_IMAGE_NAME="${MANYLINUX_IMAGE}_${TARGET}"
+# run the cross compilation
+docker run \
+  --rm \
+  -w "/work" \
+  -v "$(pwd):/work" \
+  -e "CARGO_TARGET_${TARGET_LINKER}_UNKNOWN_LINUX_GNU_LINKER" \
+  -e CARGO_BUILD_TARGET \
+  $BUILDER_NAME \
+    bash -c 'cargo build -p relay-cabi --release'
 
-# we still must build i686 and x86_64 in the manylinux containers, since those are old Centos 6
-# and already have all the devtools and old version libc installed
+# Fix permissions for shared directories before manylinux run
+sudo chown -R ${USER_ID}:${GROUP_ID} target/
+# make sure we do not build the lib twice
+export SKIP_RELAY_LIB_BUILD=1
+
+# craete a wheel for the correct architecture
 docker run \
   --rm \
   -w /work/py \
   -v "$(pwd):/work" \
   -e SKIP_RELAY_LIB_BUILD \
   -e CARGO_BUILD_TARGET \
-  quay.io/pypa/${MANYLINUX_IMAGE_NAME} \
-  sh manylinux.sh
+  quay.io/pypa/manylinux2014_${TARGET} \
+    sh manylinux.sh
 
 # Fix permissions for shared directories
 sudo chown -R ${USER_ID}:${GROUP_ID} target/
