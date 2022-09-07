1f0a1066cca15057284b1b718d21f9591e720d59
tobias-wilfert <tobias.wilfert@sentry.io>
2022-09-07 09:07:36 +0200

    Added doc comment

--- relay-server/src/utils/sleep_handle.rs
+++ relay-server/src/utils/sleep_handle.rs
@@ -3,21 +3,29 @@ use std::pin::Pin;
 use std::task::Poll;
 use std::time::Duration;
 
+/// A future wrapper around [`tokio::time::Sleep`].
+///
+/// This has two internal states, either it is pending indefinite or it wakes up after a certain
+/// duration of time has elapsed.
 pub struct SleepHandle(Option<Pin<Box<tokio::time::Sleep>>>);
 
 impl SleepHandle {
+    /// Creates [`SleepHandle`] and sets its internal state to an indefinitely pending future.
     pub fn idle() -> Self {
         Self(None)
     }
 
+    /// Resets the internal state to an indefinitely pending future.
     pub fn reset(&mut self) {
         self.0 = None;
     }
 
+    /// Sets the internal state to a future that will yield after `duration` time has elapsed.
     pub fn set(&mut self, duration: Duration) {
         self.0 = Some(Box::pin(tokio::time::sleep(duration)));
     }
 
+    /// Checks wether the internal state is currently pending indefinite.
     pub fn is_idle(&self) -> bool {
         self.0.is_none()
     }
