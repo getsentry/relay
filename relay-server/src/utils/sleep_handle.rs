f8d954d717af710995ee913ecd3c56fa6addf024
Jan Michael Auer <mail@jauer.org>
2022-09-21 16:25:20 +0200

    ref: Improve documentation

--- relay-server/src/utils/sleep_handle.rs
+++ relay-server/src/utils/sleep_handle.rs
@@ -5,8 +5,10 @@ use std::time::Duration;
 
 /// A future wrapper around [`tokio::time::Sleep`].
 ///
-/// This has two internal states, either it is pending indefinite or it wakes up after a certain
-/// duration of time has elapsed.
+/// When initialized with [`SleepHandle::idle`], this future is pending indefinitely every time it
+/// is polled. To initiate a delay, use [`set`](Self::set). After the delay has passed, the future
+/// resolves with `()` **exactly once** and resets to idle. To reset the future while it is
+/// sleeping, use [`reset`](Self::reset).
 #[derive(Debug)]
 pub struct SleepHandle(Option<Pin<Box<tokio::time::Sleep>>>);
 
