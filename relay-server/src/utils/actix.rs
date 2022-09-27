1ef3cf8546849405f2c3c33b1b19a1b111a8b810
Jan Michael Auer <mail@jauer.org>
2022-09-27 14:45:06 +0200

    ref(system): Make the main runtime multi-threaded (#1498)
    The main runtime hosts most services and will potentially become the
    only runtime in the long run. In that scenario, it should be multi-
    threaded. However, all expensive operations will likely also move to
    spawn_blocking or the EnvelopeProcessor, so we may want to reduce the
    thread count of the runtime.

--- relay-server/src/utils/actix.rs
+++ relay-server/src/utils/actix.rs
@@ -49,19 +49,20 @@ where
     }
 }
 
-/// Constructs a single threaded tokio [`Runtime`] containing a clone of the actix [`System`].
+/// Constructs a tokio [`Runtime`] configured for running [services](relay_system::Service).
 ///
-/// This is required if you need to send messages from the tokio runtime to actix
-/// actors.
+/// For compatibility, this runtime also registers the actix [`System`]. This is required for
+/// interoperability with actors. To send messages to those actors, use
+/// [`compat::send`](relay_system::compat::send).
 ///
 /// # Panics
 ///
-/// The calling thread must have the actix system enabled, panics if this is invoked
-/// in a thread where actix is not enabled.
-pub fn tokio_runtime_with_actix() -> Runtime {
+/// The calling thread must have the actix system enabled, panics if this is invoked in a thread
+/// where actix is not enabled.
+pub fn create_runtime(threads: usize) -> Runtime {
     let system = System::current();
     tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(1)
+        .worker_threads(threads)
         .enable_all()
         .on_thread_start(clone!(system, || System::set_current(system.clone())))
         .build()
