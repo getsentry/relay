f2526f08df7521eb3782034f5cb790dcac89c4d2
Floris Bruynooghe <flub@sentry.io>
2022-10-06 12:12:53 +0200

    so many typos

--- relay-server/src/utils/rate_limits.rs
+++ relay-server/src/utils/rate_limits.rs
@@ -178,7 +178,7 @@ impl EnvelopeSummary {
         summary
     }
 
-    /// Infers the appropriate [`DataCateogry`] for the envelope [`Item`].
+    /// Infers the appropriate [`DataCategory`] for the envelope [`Item`].
     ///
     /// The inferred category is only applied to the [`EnvelopeSummary`] if there is not yet
     /// a category set.
