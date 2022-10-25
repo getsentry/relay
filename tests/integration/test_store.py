9d5c861e7855765826ee2501fb793ee539b03a37
Joris Bayer <joris.bayer@sentry.io>
2022-10-25 08:59:40 +0200

    test: Try shorter sleep

--- tests/integration/test_store.py
+++ tests/integration/test_store.py
@@ -848,7 +848,8 @@ def test_re_auth(relay, mini_sentry):
     assert counter[0] > 1
 
 
-def test_re_auth_failure(relay, mini_sentry):
+@pytest.mark.parametrize("run", range(10))
+def test_re_auth_failure(relay, mini_sentry, run):
     """
     Test that after a re-authentication failure, relay stops sending messages until is reauthenticated.
 
@@ -891,7 +892,7 @@ def test_re_auth_failure(relay, mini_sentry):
     assert auth_count_1 < auth_count_2
 
     # Give Relay some time to process the auth response and mark itself as not ready
-    sleep(1)
+    sleep(0.1)
 
     # send a message, it should not come through while the authentication has failed
     relay.send_event(project_id, {"message": "123"})
@@ -910,7 +911,7 @@ def test_re_auth_failure(relay, mini_sentry):
     assert auth_count_2 < auth_count_3
 
     # Give Relay some time to process the auth response and mark itself as ready
-    sleep(1)
+    sleep(0.1)
 
     # now we should be re-authenticated and we should have the event
 
