659d5f6355c7d4f3a30b50d0cff64206012d8373
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-10-25 10:38:46 +0200

    Changes elapsed time to u64

--- relay-profiling/src/measurements.rs
+++ relay-profiling/src/measurements.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Measurement {
     // nanoseconds elapsed since the start of the profile (wall clock)
-    elapsed_since_start_ns: i64,
+    elapsed_since_start_ns: u64,
     unit: String,
     value: f64,
 }
