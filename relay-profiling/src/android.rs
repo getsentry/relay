9ffb63bff46a477ea00b820866af95f5884ca78d
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-10-26 11:26:44 +0200

    Fix measurements to be a Vec of measurements

--- relay-profiling/src/android.rs
+++ relay-profiling/src/android.rs
@@ -61,7 +61,7 @@ struct AndroidProfile {
     profile: AndroidTraceLog,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    measurements: Option<HashMap<String, Measurement>>,
+    measurements: Option<HashMap<String, Vec<Measurement>>>,
 }
 
 impl AndroidProfile {
--- relay-profiling/src/sample.rs
+++ relay-profiling/src/sample.rs
@@ -154,7 +154,7 @@ struct SampleProfile {
     transactions: Vec<TransactionMetadata>,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    measurements: Option<HashMap<String, Measurement>>,
+    measurements: Option<HashMap<String, Vec<Measurement>>>,
 }
 
 impl SampleProfile {
