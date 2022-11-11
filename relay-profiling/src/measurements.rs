0ed6a958e42e8ffab620307f8feeab2b59dd9108
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-11-11 14:48:20 +0100

    Enforce and standardize profile measurement units

--- relay-profiling/src/measurements.rs
+++ relay-profiling/src/measurements.rs
@@ -1,17 +1,34 @@
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Serialize};
 
 use crate::utils::deserialize_number_from_string;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Measurement {
+    #[serde(deserialize_with = "deserialize_measurement_unit")]
     unit: String,
     values: Vec<MeasurementValue>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct MeasurementValue {
-    // nanoseconds elapsed since the start of the profile (wall clock)
+    // nanoseconds elapsed since the start of the profile
     #[serde(deserialize_with = "deserialize_number_from_string")]
     elapsed_since_start_ns: u64,
     value: f64,
+}
+
+fn deserialize_measurement_unit<'de, D>(deserializer: D) -> Result<String, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    let s: &str = de::Deserialize::deserialize(deserializer)?;
+
+    match s {
+        "nanosecond" | "ns" => Ok("nanosecond".to_string()),
+        "hertz" | "hz" => Ok("hertz".to_string()),
+        _ => Err(de::Error::custom(format!(
+            "invalid measurement unit: {}",
+            s
+        ))),
+    }
 }
