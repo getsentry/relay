5c1c2d6bed6ad339df650ec898b1fb8db24bd871
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-11-11 15:35:01 +0100

    Replace custom deserializer with enum

--- relay-profiling/src/measurements.rs
+++ relay-profiling/src/measurements.rs
@@ -1,11 +1,10 @@
-use serde::{de, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
 use crate::utils::deserialize_number_from_string;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Measurement {
-    #[serde(deserialize_with = "deserialize_measurement_unit")]
-    unit: String,
+    unit: MeasurementUnit,
     values: Vec<MeasurementValue>,
 }
 
@@ -17,18 +16,11 @@ pub struct MeasurementValue {
     value: f64,
 }
 
-fn deserialize_measurement_unit<'de, D>(deserializer: D) -> Result<String, D::Error>
-where
-    D: de::Deserializer<'de>,
-{
-    let s: &str = de::Deserialize::deserialize(deserializer)?;
-
-    match s {
-        "nanosecond" | "ns" => Ok("nanosecond".to_string()),
-        "hertz" | "hz" => Ok("hertz".to_string()),
-        _ => Err(de::Error::custom(format!(
-            "invalid measurement unit: {}",
-            s
-        ))),
-    }
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum MeasurementUnit {
+    #[serde(alias = "ns")]
+    Nanosecond,
+    #[serde(alias = "hz")]
+    Hertz,
 }
