1195066c5a3ec75b064cc718de7f13576cb6cc03
Oleksandr Kylymnychenko <oleksandr@sentry.io>
2022-11-16 12:46:45 +0100

    chore(kafka): Add more information into the error

--- relay-kafka/src/producer/utils.rs
+++ relay-kafka/src/producer/utils.rs
@@ -1,5 +1,5 @@
 use rdkafka::producer::{DeliveryResult, ProducerContext};
-use rdkafka::ClientContext;
+use rdkafka::{ClientContext, Message};
 
 use relay_log::LogError;
 use relay_statsd::metric;
@@ -18,10 +18,18 @@ impl ProducerContext for CaptureErrorContext {
     /// This method is called after attempting to send a message to Kafka.
     /// It's called asynchronously for every message, so we want to handle errors explicitly here.
     fn delivery(&self, result: &DeliveryResult, _delivery_opaque: Self::DeliveryOpaque) {
-        if let Err((error, _message)) = result {
-            relay_log::error!(
-                "failed to produce message to Kafka (delivery callback): {}",
-                LogError(error)
+        if let Err((error, message)) = result {
+            relay_log::with_scope(
+                |scope| {
+                    scope.set_tag("topic", message.topic());
+                    scope.set_tag("payload_len", message.payload_len())
+                },
+                || {
+                    relay_log::error!(
+                        "failed to produce message to Kafka (delivery callback): {}",
+                        LogError(error)
+                    )
+                },
             );
 
             metric!(counter(KafkaCounters::ProcessingProduceError) += 1);
