44b4ab3c380c0e01534075d739f30881ca987b9f
Colton Allen <cmanallen90@gmail.com>
2022-12-07 09:51:00 -0600

    Update relay-server/src/actors/processor.rs
    Co-authored-by: Oleksandr <1931331+olksdr@users.noreply.github.com>

--- relay-server/src/actors/processor.rs
+++ relay-server/src/actors/processor.rs
@@ -1020,8 +1020,6 @@ impl EnvelopeProcessorService {
                 }
             }
             ItemType::ReplayRecording => {
-                // XXX: Temporarily, only the Sentry org will be allowed to parse replays while
-                // we measure the impact of this change.
                 if replays_enabled {
                     let parsed_recording =
                         relay_replays::recording::process_recording(&item.payload());
