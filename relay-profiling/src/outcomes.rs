23594890c22f557a30488497332f4c4ffca3afc9
Pierre Massat <pierre.massat@sentry.io>
2022-12-08 13:46:52 -0500

    fix(profiling): Warn on invalid JSON errors

--- relay-profiling/src/outcomes.rs
+++ relay-profiling/src/outcomes.rs
@@ -3,6 +3,9 @@ use crate::ProfileError;
 pub fn discard_reason(err: ProfileError) -> &'static str {
     match err {
         ProfileError::CannotSerializePayload => "profiling_failed_serialization",
+        ProfileError::InvalidBase64Value => "profiling_invalid_base64_value",
+        ProfileError::InvalidJson(_) => "profiling_invalid_json",
+        ProfileError::InvalidSampledProfile => "profiling_invalid_sampled_profile",
         ProfileError::InvalidTransactionMetadata => "profiling_invalid_transaction_metadata",
         ProfileError::MalformedSamples => "profiling_malformed_samples",
         ProfileError::MalformedStacks => "profiling_malformed_stacks",
@@ -10,6 +13,5 @@ pub fn discard_reason(err: ProfileError) -> &'static str {
         ProfileError::NoTransactionAssociated => "profiling_no_transaction_associated",
         ProfileError::NotEnoughSamples => "profiling_not_enough_samples",
         ProfileError::PlatformNotSupported => "profiling_platform_not_supported",
-        _ => "profiling_unknown",
     }
 }
