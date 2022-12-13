d5b9a2b5c17e38bc30308dcab44fad130a2def1c
Colton Allen <cmanallen90@gmail.com>
2022-12-13 12:33:55 -0600

    More punctuation on relay-config/src/config.rs
    Co-authored-by: Oleksandr <1931331+olksdr@users.noreply.github.com>

--- relay-config/src/config.rs
+++ relay-config/src/config.rs
@@ -1717,7 +1717,7 @@ impl Config {
         self.values.limits.max_profile_size.as_bytes()
     }
 
-    /// Returns the maximum payload size for a replay
+    /// Returns the maximum payload size for a replay.
     pub fn max_replay_size(&self) -> usize {
         self.values.limits.max_replay_size.as_bytes()
     }
