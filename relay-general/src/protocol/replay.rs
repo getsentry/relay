37da37eed603c704e5fb4c6b57cd2b1602b17c67
Colton Allen <colton.allen@sentry.io>
2022-12-20 09:13:26 -0600

    Add documentation

--- relay-general/src/protocol/replay.rs
+++ relay-general/src/protocol/replay.rs
@@ -37,18 +37,119 @@ use std::net::IpAddr as RealIPAddr;
 #[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
 #[metastructure(process_func = "process_replay", value_type = "Replay")]
 pub struct Replay {
+    /// Unique identifier of this event.
+    ///
+    /// Hexadecimal string representing a uuid4 value. The length is exactly 32 characters. Dashes
+    /// are not allowed. Has to be lowercase.
+    ///
+    /// Even though this field is backfilled on the server with a new uuid4, it is strongly
+    /// recommended to generate that uuid4 clientside. There are some features like user feedback
+    /// which are easier to implement that way, and debugging in case events get lost in your
+    /// Sentry installation is also easier.
+    ///
+    /// Example:
+    ///
+    /// ```json
+    /// {
+    ///   "event_id": "fc6d8c0c43fc4630ad850ee518f1b9d0"
+    /// }
+    /// ```
     pub event_id: Annotated<EventId>,
-    pub replay_id: Annotated<EventId>,
+
+    /// Replay identifier.
+    ///
+    /// Hexadecimal string representing a uuid4 value. The length is exactly 32 characters. Dashes
+    /// are not allowed. Has to be lowercase.
+    ///
+    /// Example:
+    ///
+    /// ```json
+    /// {
+    ///   "replay_id": "fc6d8c0c43fc4630ad850ee518f1b9d0"
+    /// }
+    /// ```
+    pub replay_id: Annotated<uuid::Uuid>,
+
+    /// The type of sampling that captured the replay.
+    ///
+    /// A string enumeration.  One of "session" or "error".
+    ///
+    /// Example:
+    ///
+    /// ```json
+    /// {
+    ///   "replay_type": "session"
+    /// }
+    /// ```
     pub replay_type: Annotated<String>,
+
+    /// Segment identifier.
+    ///
+    /// A number representing a unique segment identifier in the chain of replay segments.
+    /// Segment identifiers are temporally ordered but can be received by the Relay service in any
+    /// order.
+    ///
+    /// Example:
+    ///
+    /// ```json
+    /// {
+    ///   "segment_id": 10
+    /// }
+    /// ```
     pub segment_id: Annotated<u64>,
+
+    /// Timestamp when the event was created.
+    ///
+    /// Indicates when the segment was created in the Sentry SDK. The format is either a string as
+    /// defined in [RFC 3339](https://tools.ietf.org/html/rfc3339) or a numeric (integer or float)
+    /// value representing the number of seconds that have elapsed since the [Unix
+    /// epoch](https://en.wikipedia.org/wiki/Unix_time).
+    ///
+    /// Timezone is assumed to be UTC if missing.
+    ///
+    /// Sub-microsecond precision is not preserved with numeric values due to precision
+    /// limitations with floats (at least in our systems). With that caveat in mind, just send
+    /// whatever is easiest to produce.
+    ///
+    /// All timestamps in the event protocol are formatted this way.
+    ///
+    /// # Example
+    ///
+    /// All of these are the same date:
+    ///
+    /// ```json
+    /// { "timestamp": "2011-05-02T17:41:36Z" }
+    /// { "timestamp": "2011-05-02T17:41:36" }
+    /// { "timestamp": "2011-05-02T17:41:36.000" }
+    /// { "timestamp": 1304358096.0 }
+    /// ```
     pub timestamp: Annotated<Timestamp>,
+
+    /// Timestamp when the replay was created.  Typically only specified on the initial segment.
     pub replay_start_timestamp: Annotated<Timestamp>,
+
+    /// A list of URLs visted during the lifetime of the segment.
     pub urls: Annotated<Array<String>>,
+
+    /// A list of error-ids discovered during the lifetime of the segment.
     pub error_ids: Annotated<Array<String>>,
+
+    /// A list of trace-ids discovered during the lifetime of the segment.
     pub trace_ids: Annotated<Array<String>>,
+
+    /// Contexts describing the environment (e.g. device, os or browser).
     pub contexts: Annotated<Contexts>,
+
+    /// Platform identifier of this event (defaults to "other").
+    ///
+    /// A string representing the platform the SDK is submitting from. This will be used by the
+    /// Sentry interface to customize various components in the interface.
     pub platform: Annotated<String>,
 
+    /// The release version of the application.
+    ///
+    /// **Release versions must be unique across all projects in your organization.** This value
+    /// can be the git SHA for the given project, or a product identifier with a semantic version.
     #[metastructure(
         max_chars = "tag_value",
         required = "false",
@@ -58,6 +159,13 @@ pub struct Replay {
     )]
     pub release: Annotated<LenientString>,
 
+    /// Program's distribution identifier.
+    ///
+    /// The distribution of the application.
+    ///
+    /// Distributions are used to disambiguate build or deployment variants of the same release of
+    /// an application. For example, the dist can be the build number of an XCode build or the
+    /// version code of an Android build.
     #[metastructure(
         allow_chars = "a-zA-Z0-9_.-",
         trim_whitespace = "true",
@@ -66,6 +174,11 @@ pub struct Replay {
     )]
     pub dist: Annotated<String>,
 
+    /// The environment name, such as `production` or `staging`.
+    ///
+    /// ```json
+    /// { "environment": "production" }
+    /// ```
     #[metastructure(
         max_chars = "environment",
         nonempty = "true",
@@ -74,18 +187,25 @@ pub struct Replay {
     )]
     pub environment: Annotated<String>,
 
+    /// Custom tags for this event.
+    ///
+    /// A map or list of tags for this event. Each tag must be less than 200 characters.
     #[metastructure(skip_serialization = "empty", pii = "maybe")]
     pub tags: Annotated<Tags>,
 
+    /// Static value. Should always be "replay_event".
     #[metastructure(field = "type")]
     pub ty: Annotated<String>,
 
+    /// Information about the user who triggered this event.
     #[metastructure(skip_serialization = "empty")]
     pub user: Annotated<User>,
 
+    /// Information about a web request that occurred during the event.
     #[metastructure(skip_serialization = "empty")]
     pub request: Annotated<Request>,
 
+    /// Information about the Sentry SDK that generated this event.
     #[metastructure(field = "sdk")]
     #[metastructure(skip_serialization = "empty")]
     pub sdk: Annotated<ClientSdkInfo>,
