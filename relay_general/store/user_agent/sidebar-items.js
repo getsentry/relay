392c1324c3cd19643016221476084164902dfb6f
cmanallen <cmanallen@users.noreply.github.com>
2023-01-12 15:26:13 +0000

    deploy: 6ebaf355ef20a2c10c5d1ced99f5a296b31c396c

---
+++ relay_general/store/user_agent/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["normalize_user_agent",""],["normalize_user_agent_generic",""]]};
--- relay_replays/sidebar-items.js
+++ relay_replays/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["normalize_replay_event","Validates and accepts a replay-event item as input and returns modified output."]],"mod":[["recording",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["recording",""]]};
