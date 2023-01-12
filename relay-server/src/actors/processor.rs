getsentry/relay

commit 588e46a4c8c01bf40bfdd7b2fdeeb9b30f2d4003

feat(replays): Enable PII scrubbing for all organizations (#1678)
#skip-changelog

Co-authored-by: Oleksandr <1931331+olksdr@users.noreply.github.com>

relay-server/src/actors/processor.rs
@@ -1042,7 +1042,7 @@ impl EnvelopeProcessorService {
             ItemType::ReplayRecording => {
                 // XXX: Temporarily, only the Sentry org will be allowed to parse replays while
                 // we measure the impact of this change.
-                if replays_enabled && state.project_state.organization_id == Some(1) {
+                if replays_enabled {
                     // Limit expansion of recordings to the max replay size. The payload is
                     // decompressed temporarily and then immediately re-compressed. However, to
                     // limit memory pressure, we use the replay limit as a good overall limit for

tests/integration/test_replay_recordings.py
@@ -1,4 +1,4 @@
-import time
+import zlib
 
 from sentry_sdk.envelope import Envelope, Item, PayloadRef
 
@@ -92,8 +92,6 @@ def test_chunked_replay_recordings_processing(
     assert replay_recording["received"]
     assert type(replay_recording["received"]) == int
 
-    outcomes_consumer.assert_empty()
-
 
 def test_nonchunked_replay_recordings_processing(
     mini_sentry, relay_with_processing, replay_recordings_consumer, outcomes_consumer
@@ -117,7 +115,8 @@ def test_nonchunked_replay_recordings_processing(
             ["attachment_type", "replay_recording"],
         ]
     )
-    envelope.add_item(Item(payload=PayloadRef(bytes=b"test"), type="replay_recording"))
+    payload = recording_payload(b"[]")
+    envelope.add_item(Item(payload=PayloadRef(bytes=payload), type="replay_recording"))
 
     relay.send_envelope(project_id, envelope)
 
@@ -129,7 +128,12 @@ def test_nonchunked_replay_recordings_processing(
     assert replay_recording["org_id"] == org_id
     assert type(replay_recording["received"]) == int
     assert replay_recording["retention_days"] == 90
-    assert replay_recording["payload"] == b"test"
+    assert replay_recording["payload"] == payload
     assert replay_recording["type"] == "replay_recording_not_chunked"
 
     outcomes_consumer.assert_empty()
+
+
+def recording_payload(bits: bytes):
+    compressed_payload = zlib.compress(bits)
+    return b'{"segment_id": 0}\n' + compressed_payload

commit d86d066f1d949a10e14fc17afaf5a5b4e6a886ea

instr(replays): Add timer metric to recording processing (#1742)
We noticed an increase in total processing time which we suspect was
caused by https://github.com/getsentry/relay/pull/1678.

Add a statsd metric to measure replay recording processing in isolation.

relay-server/src/actors/processor.rs
@@ -1094,7 +1094,9 @@ impl EnvelopeProcessorService {
                     // allocations.
                     let limit = self.config.max_replay_size();
                     let parsed_recording =
-                        relay_replays::recording::process_recording(&item.payload(), limit);
+                        metric!(timer(RelayTimers::ReplayRecordingProcessing), {
+                            relay_replays::recording::process_recording(&item.payload(), limit)
+                        });
 
                     match parsed_recording {
                         Ok(recording) => {

relay-server/src/statsd.rs
@@ -298,6 +298,9 @@ pub enum RelayTimers {
 
     /// Time in milliseconds spent on converting a transaction event into a metric.
     TransactionMetricsExtraction,
+
+    /// Time in milliseconds spent on parsing, normalizing and scrubbing replay recordings.
+    ReplayRecordingProcessing,
 }
 
 impl TimerMetric for RelayTimers {
@@ -328,6 +331,7 @@ impl TimerMetric for RelayTimers {
             RelayTimers::TimestampDelay => "requests.timestamp_delay",
             RelayTimers::OutcomeAggregatorFlushTime => "outcomes.aggregator.flush_time",
             RelayTimers::TransactionMetricsExtraction => "metrics.extraction.transactions",
+            RelayTimers::ReplayRecordingProcessing => "replay.recording.process",
         }
     }
 }