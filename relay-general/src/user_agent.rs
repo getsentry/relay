e95c8c3ec59ab98f0efcc786fab2c81c96a002a5
Tor <tor.saebjoernsen@sentry.io>
2023-01-24 15:27:50 +0100

    Update relay-general/src/user_agent.rs
    Co-authored-by: Joris Bayer <joris.bayer@sentry.io>

--- relay-general/src/user_agent.rs
+++ relay-general/src/user_agent.rs
@@ -26,7 +26,7 @@ static UA_PARSER: Lazy<UserAgentParser> = Lazy::new(|| {
     )
 });
 
-/// this has both the user agent string and the client hints, useful for the scenarios where
+/// This has both the user agent string and the client hints, useful for the scenarios where
 /// you will use either information from client hints if it exists and if not fall back to
 /// user agent string.
 ///
