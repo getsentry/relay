aba3f0044668790b4d5addbf3a3f311949f24eb9
Joris Bayer <joris.bayer@sentry.io>
2023-01-26 11:54:01 +0100

    Use largest fixture

--- relay-replays/benches/benchmarks.rs
+++ relay-replays/benches/benchmarks.rs
@@ -5,7 +5,7 @@ use criterion::{criterion_group, criterion_main};
 use relay_replays::recording::_deserialize_event;
 
 fn bench_recording(c: &mut Criterion) {
-    let payload = include_bytes!("../tests/fixtures/rrweb-event-5.json");
+    let payload = include_bytes!("../tests/fixtures/rrweb.json");
 
     c.bench_with_input(BenchmarkId::new("rrweb", 1), &payload, |b, &_| {
         b.iter(|| _deserialize_event(payload));
