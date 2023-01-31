50c7dee4578a206fba331ac3394aee5b2ad59d8d
TBS1996 <TBS1996@users.noreply.github.com>
2023-01-31 09:55:58 +0000

    deploy: bd18149e663814bcab888f8ffda0e4f693debea4

--- relay_general/store/user_agent/sidebar-items.js
+++ relay_general/store/user_agent/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["normalize_user_agent",""],["normalize_user_agent_generic",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["get_version",""],["is_known",""],["normalize_user_agent",""],["normalize_user_agent_info_generic",""]]};
