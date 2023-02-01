969596cf4542615a0afabf82221e0713e0349142
Andrii Soldatenko <andrii.soldatenko@sentry.io>
2023-02-01 10:15:43 +0100

    remove second &mut

--- relay-server/src/actors/processor.rs
+++ relay-server/src/actors/processor.rs
@@ -799,7 +799,7 @@ impl EnvelopeProcessorService {
     /// are out of range after clock drift correction.
     fn process_sessions(&self, state: &mut ProcessEnvelopeState) {
         let received = state.envelope_context.received_at();
-        let extracted_metrics = &mut &mut state.extracted_metrics.project_metrics;
+        let extracted_metrics = &mut state.extracted_metrics.project_metrics;
         let metrics_config = state.project_state.config().session_metrics;
         let envelope = &mut state.envelope;
         let client = envelope.meta().client().map(|x| x.to_owned());
