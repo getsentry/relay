3d9b6c3956aad57cc1c13012af6d28eba03c3d95
jan-auer <jan-auer@users.noreply.github.com>
2023-02-08 13:40:09 +0000

    deploy: 576a719b86aab5ffbb9601e0fc2cfadb04bc589a

---
+++ relay_general/utils/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["Glob","A simple glob matcher."],["GlobBuilder","`GlobBuilder` provides the posibility to fine tune the final [`Glob`], mainly what capture groups will be enabled in the underlying regex."],["GlobMatcher","Helper for glob matching"]]};
