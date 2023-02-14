736da9e6c7cdca651a934951be680644ba2302c3
Jan Michael Auer <mail@jauer.org>
2023-02-14 14:08:16 +0100

    build(log): Upgrade log dependencies (#1847)
    Updates `env_logger`. For human readable output we have used
    `pretty_env_logger`, which is no longer compatible with the latest
    `env_logger` version. This PR vendors the relevant bits of the
    implementation.
    
    Previously, the `relay-log` crate hard-coded a list of all workspace
    members to set log levels based on the configuration. This has been
    refactored:
    
    - The list of workspace members is now created in a build script.
    - The default level filter sets every workspace member to TRACE.
    - There is a global `max_level` filter based on the overall log level
      config.

--- relay-log/build.rs
+++ relay-log/build.rs
@@ -1,4 +1,7 @@
-use std::io;
+use std::env;
+use std::fs::{self, File};
+use std::io::{self, Write};
+use std::path::Path;
 use std::process::{Command, Stdio};
 
 fn emit_release_var() -> Result<(), io::Error> {
@@ -21,6 +24,43 @@ fn emit_release_var() -> Result<(), io::Error> {
     Ok(())
 }
 
+fn list_crates() -> Vec<String> {
+    let mut crates = Vec::new();
+
+    for result in fs::read_dir("../").unwrap() {
+        let entry = result.unwrap();
+
+        if !entry.file_type().unwrap().is_dir() {
+            continue;
+        }
+
+        if let Some(s) = entry.file_name().to_str() {
+            if s.starts_with("relay") {
+                crates.push(s.to_owned());
+            }
+        }
+    }
+
+    crates
+}
+
+fn emit_crate_list() -> Result<(), io::Error> {
+    let crates = list_crates();
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("constants.gen.rs");
+    let mut f = File::create(dest_path).unwrap();
+
+    write!(f, "const CRATE_NAMES: &[&str] = &[")?;
+    for name in &crates {
+        write!(f, "\"{name}\",")?;
+    }
+    writeln!(f, "];")?;
+
+    Ok(())
+}
+
 fn main() {
     emit_release_var().ok();
+    emit_crate_list().unwrap();
 }
