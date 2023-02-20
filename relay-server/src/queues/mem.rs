f12502d6d60b90ae4308b05f4837a1a80773d451
Joris Bayer <joris.bayer@sentry.io>
2023-02-20 17:53:11 +0100

    basic multi queue

--- relay-server/src/queues/mem.rs
+++ relay-server/src/queues/mem.rs
@@ -2,19 +2,59 @@ use std::collections::{BTreeMap, VecDeque};
 
 use crate::queues::MultiQueue;
 
-pub struct MemQueue<K, T>(BTreeMap<K, VecDeque<T>>);
+pub struct MemQueue<K: Sized + std::cmp::Ord + Copy, T>(BTreeMap<K, VecDeque<T>>);
 
-impl<K, T> MultiQueue<K, T> for MemQueue<K, T> {
-    fn push_back(&mut self, queue_id: K, item: T) {
-        let x = &mut self.0;
-        x.entry(queue_id).or_default().push_back(item);
+impl<K: Sized + std::cmp::Ord + Copy, T> MemQueue<K, T> {
+    fn new() -> Self {
+        MemQueue(BTreeMap::new())
+    }
+}
+
+impl<K: Sized + std::cmp::Ord + Copy, T> MultiQueue<K, T> for MemQueue<K, T> {
+    fn push_back(&mut self, queue_id: &K, item: T) {
+        self.0.entry(*queue_id).or_default().push_back(item)
     }
 
-    fn pop_front(&mut self, queue_id: K) -> Option<T> {
-        todo!()
+    fn pop_front(&mut self, queue_id: &K) -> Option<T> {
+        self.0.get_mut(queue_id).and_then(|q| q.pop_front())
     }
 
-    fn len(&self, queue_id: K) -> usize {
-        todo!()
+    fn len(&self, queue_id: &K) -> usize {
+        match self.0.get(queue_id) {
+            Some(q) => q.len(),
+            None => 0usize,
+        }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use super::*;
+
+    #[test]
+    fn test_basics() {
+        let mut q = MemQueue::new();
+        assert_eq!(q.pop_front(&123), None);
+        q.push_back(&1, "a");
+        q.push_back(&2, "b");
+        assert_eq!(q.pop_front(&1), Some("a"));
+        assert_eq!(q.pop_front(&2), Some("b"));
+        assert_eq!(q.pop_front(&1), None);
+        assert_eq!(q.pop_front(&2), None);
+    }
+
+    // fn test_single_queues() {
+    //     let mut q = Arc::new(MemQueue::new());
+
+    //     q1 = Queue
+    //     assert_eq!(q.pop_front(&123), None);
+    //     q.push_back(&1, "a");
+    //     q.push_back(&2, "b");
+    //     assert_eq!(q.pop_front(&1), Some("a"));
+    //     assert_eq!(q.pop_front(&2), Some("b"));
+    //     assert_eq!(q.pop_front(&1), None);
+    //     assert_eq!(q.pop_front(&2), None);
+    // }
 }
--- relay-server/src/queues/mod.rs
+++ relay-server/src/queues/mod.rs
@@ -2,14 +2,14 @@ pub mod mem;
 
 /// Combine multiple logical queues into a single structure so we can efficiently handle 10.000s of queues.
 pub trait MultiQueue<K, T> {
-    fn push_back(&mut self, queue_id: K, item: T);
-    fn pop_front(&mut self, queue_id: K) -> Option<T>;
-    fn len(&self, queue_id: K) -> usize;
+    fn push_back(&mut self, queue_id: &K, item: T);
+    fn pop_front(&mut self, queue_id: &K) -> Option<T>;
+    fn len(&self, queue_id: &K) -> usize;
 }
 
-// pub struct Queue<K, T, MultiQueue<K, T> > {
+// pub struct Queue<Q: MultiQueue<K, T>> {
 //     queue_id: K,
-//     backend: MQ,
+//     backend: Q,
 // }
 
 // impl<K, T, MQ: MultiQueue<K, T>> Queue<K, T, MQ> {
