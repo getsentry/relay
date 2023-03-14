df1958bdabfe69fa2280f4d71227c75c483f63ff
jan-auer <jan-auer@users.noreply.github.com>
2023-03-14 12:50:47 +0000

    deploy: 7ad83f3b2dee6e87682c1b7ab5bc91b83a188ada

--- implementors/actix/actor/trait.Actor.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"relay_system":[["impl Actor for <a class=\"struct\" href=\"relay_system/struct.Controller.html\" title=\"struct relay_system::Controller\">Controller</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/actix/handler/trait.Message.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"relay_system":[["impl Message for <a class=\"struct\" href=\"relay_system/struct.Shutdown.html\" title=\"struct relay_system::Shutdown\">Shutdown</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
