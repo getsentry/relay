7bf5764c13e5557e8b743092c8b121c6f3ecf4fe
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-03-20 11:20:23 +0100

    Revert span.data typing
    This reverts commit b710ffdaa1eb0f95e5a0cd5fa80ee10fc4d73e5a.

--- relay-general/src/protocol/span.rs
+++ relay-general/src/protocol/span.rs
@@ -1,6 +1,4 @@
-use crate::protocol::{
-    DataElement, JsonLenientString, OperationType, SpanId, SpanStatus, Timestamp, TraceId,
-};
+use crate::protocol::{JsonLenientString, OperationType, SpanId, SpanStatus, Timestamp, TraceId};
 use crate::types::{Annotated, Object, Value};
 
 #[derive(Clone, Debug, Default, PartialEq, Empty, FromValue, IntoValue, ProcessValue)]
@@ -47,7 +45,7 @@ pub struct Span {
 
     /// Arbitrary additional data on a span, like `extra` on the top-level event.
     #[metastructure(pii = "maybe")]
-    pub data: Annotated<DataElement>,
+    pub data: Annotated<Object<Value>>,
 
     // TODO remove retain when the api stabilizes
     /// Additional arbitrary fields for forwards compatibility.
@@ -57,8 +55,6 @@ pub struct Span {
 
 #[cfg(test)]
 mod tests {
-    use std::collections::BTreeMap;
-
     use chrono::{TimeZone, Utc};
     use similar_asserts::assert_eq;
 
@@ -75,17 +71,7 @@ mod tests {
   "op": "operation",
   "span_id": "fa90fdead5f74052",
   "trace_id": "4c79f60c11214eb38604f4ae0781bfb2",
-  "status": "ok",
-  "data": {
-    "http": {
-      "query": "is_sample_query=true",
-      "not_query": "this is not the query"
-    },
-    "more_fields": {
-      "how_many": "yes",
-      "many": "a lot!"
-    }
-  }
+  "status": "ok"
 }"#;
 
         let span = Annotated::new(Span {
@@ -99,38 +85,6 @@ mod tests {
             trace_id: Annotated::new(TraceId("4c79f60c11214eb38604f4ae0781bfb2".into())),
             span_id: Annotated::new(SpanId("fa90fdead5f74052".into())),
             status: Annotated::new(SpanStatus::Ok),
-            data: Annotated::new(DataElement {
-                http: Annotated::new(HttpElement {
-                    query: Annotated::new(Value::String("is_sample_query=true".to_owned())),
-                    other: {
-                        let mut map = BTreeMap::new();
-                        map.insert(
-                            "not_query".to_owned(),
-                            Annotated::new(Value::String("this is not the query".to_owned())),
-                        );
-                        map
-                    },
-                }),
-                other: {
-                    let mut inner_map = BTreeMap::new();
-                    inner_map.insert(
-                        "how_many".to_owned(),
-                        Annotated::new(Value::String("yes".to_owned())),
-                    );
-                    inner_map.insert(
-                        "many".to_owned(),
-                        Annotated::new(Value::String("a lot!".to_owned())),
-                    );
-
-                    let mut outter_map = BTreeMap::new();
-                    outter_map.insert(
-                        "more_fields".to_owned(),
-                        Annotated::new(Value::Object(inner_map)),
-                    );
-
-                    outter_map
-                },
-            }),
             ..Default::default()
         });
         assert_eq!(json, span.to_json_pretty().unwrap());
