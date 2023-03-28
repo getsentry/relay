328d569306c2ce8a56984d4d5afa036220ffe241
Jan Michael Auer <mail@jauer.org>
2023-03-28 15:55:57 +0200

    fix: Ignore UTF-8 content encoding

--- relay-server/src/middlewares/decompression.rs
+++ relay-server/src/middlewares/decompression.rs
@@ -11,12 +11,19 @@ use crate::utils::ApiErrorResponse;
 /// This is to be used along with the [`RequestDecompressionLayer`].
 pub fn remove_empty_encoding<B>(mut request: Request<B>) -> Request<B> {
     if let header::Entry::Occupied(entry) = request.headers_mut().entry(header::CONTENT_ENCODING) {
-        if entry.get().as_bytes() == b"" {
+        if should_ignore_encoding(entry.get().as_bytes()) {
             entry.remove();
         }
     }
 
     request
+}
+
+/// Returns `true` if this content-encoding value should be ignored.
+fn should_ignore_encoding(value: &[u8]) -> bool {
+    // sentry-ruby/5.x sends an empty string
+    // sentry.java.android/2.0.0 sends "UTF-8"
+    value == b"" || value.eq_ignore_ascii_case(b"utf-8")
 }
 
 /// Error function to be used with [`RequestDecompressionLayer`].
