f8e1fd817a9c702a5bc7f2af1bd9c2cb14b8d64e
Joshua Ferge <josh.ferge@sentry.io>
2023-04-04 14:34:12 -0700

    add more info for replay context

--- relay-general/src/protocol/contexts/replay.rs
+++ relay-general/src/protocol/contexts/replay.rs
@@ -2,6 +2,12 @@ use crate::protocol::EventId;
 use crate::types::{Annotated, Object, Value};
 
 /// Replay context
+///
+/// The replay context contains the replay_id of the session replay if the event
+/// occurred during a replay. The replay_id is added onto the dynamic sampling context
+/// on the javascript SDK which propagates it through the trace. In relay, we take
+/// this value from the DSC and create a context which contains only the replay_id
+/// This context is never set on the client for events, only on relay.
 #[derive(Clone, Debug, Default, PartialEq, Empty, FromValue, IntoValue, ProcessValue)]
 #[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
 pub struct ReplayContext {
