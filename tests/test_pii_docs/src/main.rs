12e79ba7e1cbc53035a2fe953f59de8bbb02cfdf
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-04-20 14:46:12 +0200

    wip

--- tests/test_pii_docs/src/main.rs
+++ tests/test_pii_docs/src/main.rs
@@ -16,4 +16,17 @@ struct SubStruct {
     field1: String,
     #[metastructure(pii = "true")]
     field2: FooBar,
+    // Checks that it goes spearately into both HasGeneric and InnerGeneric to check for pii fields.
+    mystery_field: HasGeneric<InnerGeneric>,
+}
+
+struct HasGeneric<T> {
+    inner_generic: T,
+    #[metastructure(pii = "maybe")]
+    maybe_sensitive_stuff: u32,
+}
+
+struct InnerGeneric {
+    #[metastructure(pii = "true")]
+    bitcoin_wallet_key: String,
 }
