96bf56f28e02b7e8984aceb1de5a18e3e3f357bd
Joris Bayer <joris.bayer@sentry.io>
2023-04-25 11:58:20 +0200

    fix

--- relay-server/src/utils/rate_limits.rs
+++ relay-server/src/utils/rate_limits.rs
@@ -578,7 +578,7 @@ where
         if summary.checkin_quantity > 0 {
             let item_scoping = scoping.item(DataCategory::Monitor);
             let checkin_limits = (self.check)(item_scoping, summary.checkin_quantity)?;
-            enforcement.replays = CategoryLimit::new(
+            enforcement.check_ins = CategoryLimit::new(
                 DataCategory::Monitor,
                 summary.checkin_quantity,
                 checkin_limits.longest(),
@@ -977,9 +977,12 @@ mod tests {
 
         let outcomes = enforcement
             .get_outcomes(&envelope, &scoping())
-            .map(|outcome| (outcome.category, outcome.quantity))
+            .map(|outcome| (outcome.outcome, outcome.category, outcome.quantity))
             .collect::<Vec<_>>();
-        insta::assert_debug_snapshot!(outcomes, @"");
+        assert_eq!(
+            outcomes,
+            vec![(Outcome::RateLimited(None), DataCategory::Monitor, 1)]
+        )
     }
 
     #[test]
