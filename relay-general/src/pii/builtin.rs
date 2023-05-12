d5d6136af026a619d0074630e1df7808efa9eee2
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-05-12 17:11:34 +0200

    wip

--- relay-general/src/pii/builtin.rs
+++ relay-general/src/pii/builtin.rs
@@ -401,6 +401,30 @@ mod tests {
         value: Annotated<String>,
     }
 
+    macro_rules! assert_rule_not_applied {
+        (
+            rule = $rule:expr; input = $input:expr;
+        ) => {{
+            let config = PiiConfig {
+                applications: {
+                    let mut map = BTreeMap::new();
+                    map.insert(ValueType::String.into(), vec![$rule.to_string()]);
+                    map
+                },
+                ..PiiConfig::default()
+            };
+            let input = $input.to_string();
+            let compiled = config.compiled();
+            let mut processor = PiiProcessor::new(&compiled);
+            let mut root = Annotated::new(FreeformRoot {
+                value: Annotated::new(input),
+            });
+            process_value(&mut root, &mut processor, ProcessingState::root()).unwrap();
+            let root = root.0.unwrap();
+            assert_eq!(root.value.value().unwrap(), $input);
+        }};
+    }
+
     macro_rules! assert_text_rule {
         (
             rule = $rule:expr; input = $input:expr; output = $output:expr; remarks = $remarks:expr;
@@ -809,7 +833,7 @@ mod tests {
     }
 
     #[test]
-    fn test_iban() {
+    fn test_iban_different_rules() {
         assert_text_rule!(
             rule = "@iban";
             input = "some iban: DE89370400440532013000!";
@@ -874,9 +898,144 @@ mod tests {
                 Remark::with_range(RemarkType::Pseudonymized, "0", (11, 51)),
             ];
         );
+    }
 
-        /*
-         */
+    #[test]
+    fn test_invalid_iban_codes() {
+        assert_text_rule!(
+            rule = "@iban";
+            input = "some iban: NO9386011117945!";
+            output = "some iban: [iban]!";
+            remarks = vec![Remark::with_range(RemarkType::Substituted, "@iban", (11, 17))];
+        );
+
+        // Norway has the shortest iban, here I removed the final number, which means it should not
+        // be scrubbed.
+        assert_rule_not_applied!(
+            rule = "@iban";
+            input = "some iban: NO938601111794!";
+        );
+
+        assert_text_rule!(
+            rule = "@iban";
+            input = "some iban: RU0204452560040702810412345678901!";
+            output = "some iban: [iban]!";
+            remarks = vec![Remark::with_range(RemarkType::Substituted, "@iban", (11, 17))];
+        );
+
+        // Russia has the longest iban. Here I add a final number, which means the rule won't be
+        // applied as it exceeds the maximum iban length.
+        assert_rule_not_applied!(
+            rule = "@iban";
+            input = "some iban: RU02044525600407028104123456789018!";
+        );
+
+        assert_text_rule!(
+            rule = "@iban";
+            input = "some iban: NO9386011117945!";
+            output = "some iban: [iban]!";
+            remarks = vec![Remark::with_range(RemarkType::Substituted, "@iban", (11, 17))];
+        );
+
+        // Don't apply if it doesnt start with two uppercase letters.
+        assert_rule_not_applied!(
+            rule = "@iban";
+            input = "some iban: N19386011117945!";
+        );
+    }
+
+    #[test]
+    fn test_valid_iban_codes() {
+        // list taken from: `https://github.com/jschaedl/iban-validation/blob/master/tests/ValidatorTest.php`
+        // Todo: verify that it's okay to copy it.
+        let valid_iban_codes = vec![
+            "AD1200012030200359100100",
+            "AE070331234567890123456",
+            "AL47212110090000000235698741",
+            "AT611904300234573201",
+            "AZ21NABZ00000000137010001944",
+            "BA391290079401028494",
+            "BE68539007547034",
+            "BG80BNBG96611020345678",
+            "BH67BMAG00001299123456",
+            "BR1800360305000010009795493C1",
+            "BY13NBRB3600900000002Z00AB00",
+            "CH9300762011623852957",
+            "CR05015202001026284066",
+            "CY17002001280000001200527600",
+            "CZ6508000000192000145399",
+            "DE89370400440532013000",
+            "DK5000400440116243",
+            "DO28BAGR00000001212453611324",
+            "EE382200221020145685",
+            "EG380019000500000000263180002",
+            "ES9121000418450200051332",
+            "FI2112345600000785",
+            "FO6264600001631634",
+            "FR1420041010050500013M02606",
+            "GB29NWBK60161331926819",
+            "GE29NB0000000101904917",
+            "GI75NWBK000000007099453",
+            "GL8964710001000206",
+            "GR1601101250000000012300695",
+            "GT82TRAJ01020000001210029690",
+            "HR1210010051863000160",
+            "HU42117730161111101800000000",
+            "IE29AIBK93115212345678",
+            "IL620108000000099999999",
+            "IQ98NBIQ850123456789012",
+            "IS140159260076545510730339",
+            "IT60X0542811101000000123456",
+            "JO94CBJO0010000000000131000302",
+            "KW81CBKU0000000000001234560101",
+            "KZ86125KZT5004100100",
+            "LB62099900000001001901229114",
+            "LC55HEMM000100010012001200023015",
+            "LI21088100002324013AA",
+            "LT121000011101001000",
+            "LU280019400644750000",
+            "LV80BANK0000435195001",
+            "LY83002048000020100120361",
+            "MC5811222000010123456789030",
+            "MD24AG000225100013104168",
+            "ME25505000012345678951",
+            "MK07250120000058984",
+            "MR1300020001010000123456753",
+            "MT84MALT011000012345MTLCAST001S",
+            "MU17BOMM0101101030300200000MUR",
+            "NL91ABNA0417164300",
+            "NO9386011117947",
+            "PK36SCBL0000001123456702",
+            "PL61109010140000071219812874",
+            "PS92PALS000000000400123456702",
+            "PT50000201231234567890154",
+            "QA58DOHB00001234567890ABCDEFG",
+            "RO49AAAA1B31007593840000",
+            "RS35260005601001611379",
+            "SA0380000000608010167519",
+            "SC18SSCB11010000000000001497USD",
+            "SE4550000000058398257466",
+            "SI56263300012039086",
+            "SK3112000000198742637541",
+            "SM86U0322509800000000270100",
+            "SV62CENR00000000000000700025",
+            "TL380080012345678910157",
+            "TN5910006035183598478831",
+            "TR330006100519786457841326",
+            "UA213223130000026007233566001",
+            "VA59001123000012345678",
+            "VG96VPVG0000012345678901",
+            "XK051212012345678906",
+        ];
+
+        for iban_code in valid_iban_codes {
+            assert_text_rule!(
+                rule = "@iban";
+                input = &format!("some iban: {}!", iban_code);
+                output = "some iban: [iban]!";
+                remarks = vec![Remark::with_range(RemarkType::Substituted, "@iban", (11, 17))];
+            );
+        }
     }
 
     #[test]
