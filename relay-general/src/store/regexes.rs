be7afa336a3e98a9d7c34ce1a87326c3aa6f6612
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-05-16 13:32:10 +0200

    Add docstrings to regex

--- relay-general/src/store/regexes.rs
+++ relay-general/src/store/regexes.rs
@@ -89,6 +89,14 @@ pub static CACHE_NORMALIZER_REGEX: Lazy<Regex> = Lazy::new(|| {
     .unwrap()
 });
 
+/// Regex with multiple capture groups for resource tokens we should scrub.
+///
+/// Resource tokens are the tokens that exist in resource spans that generate
+/// high cardinality or are noise for the product. For example, the hash of the
+/// file next to its name.
+///
+/// Slightly modified Regex from
+/// https://github.com/getsentry/sentry/blob/de5949a9a313d7ef0bf0685f84fe6e981ac38558/src/sentry/utils/performance_issues/base.py#L292-L306
 pub static RESOURCE_NORMALIZER_REGEX: Lazy<Regex> = Lazy::new(|| {
     Regex::new(
         r#"(?xi)
