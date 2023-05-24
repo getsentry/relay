f1513a31b8275b7214ce994978405ba65a4e1b46
jjbayer <jjbayer@users.noreply.github.com>
2023-05-24 13:34:09 +0000

    deploy: eff869d7e92474cf01a2106716c0f6d2e488c234

--- implementors/relay_system/service/trait.Interface.js
+++ implementors/relay_system/service/trait.Interface.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"relay_metrics":[["impl Interface for <a class=\"enum\" href=\"relay_metrics/enum.Aggregator.html\" title=\"enum relay_metrics::Aggregator\">Aggregator</a>"]],
+"relay_metrics":[["impl <a class=\"trait\" href=\"relay_system/service/trait.Interface.html\" title=\"trait relay_system::service::Interface\">Interface</a> for <a class=\"enum\" href=\"relay_metrics/enum.Aggregator.html\" title=\"enum relay_metrics::Aggregator\">Aggregator</a>"]],
 "relay_system":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
