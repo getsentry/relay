d7d18bb0756de165740ba0f563e1f0c65e52130b
Joshua Ferge <josh.ferge@sentry.io>
2023-06-06 18:16:15 -0700

    remove unused test

--- tests/integration/test_replay_combined_payload.py
+++ tests/integration/test_replay_combined_payload.py
@@ -1,39 +1,7 @@
-# import pytest
-# import zlib
-
 from sentry_sdk.envelope import Envelope, Item, PayloadRef
 import msgpack
 from .test_replay_events import generate_replay_sdk_event
 import json
-
-# def test_replay_recordings(mini_sentry, relay_chain):
-#     relay = relay_chain(min_relay_version="latest")
-
-#     project_id = 42
-#     mini_sentry.add_basic_project_config(
-#         project_id, extra={"config": {"features": ["organizations:session-replay"]}}
-#     )
-
-#     replay_id = "515539018c9b4260a6f999572f1661ee"
-
-#     replay_event = generate_replay_sdk_event(replay_id=replay_id)
-
-#     envelope = Envelope(headers=[["event_id", replay_id]])
-#     envelope.add_item(
-#         Item(payload=PayloadRef(bytes=b"{}\n[]"), type="replay_recording")
-#     )
-#     envelope.add_item(Item(payload=PayloadRef(json=replay_event), type="replay_event"))
-
-#     relay.send_envelope(project_id, envelope)
-
-#     envelope = mini_sentry.captured_events.get(timeout=1)
-#     assert len(envelope.items) == 1
-
-#     replay_combined_item = envelope.items[0]
-#     assert session_item.type == "replay_recording"
-
-#     replay_recording = session_item.get_bytes()
-#     assert replay_recording.startswith(b"{}\n")  # The body is compressed
 
 
 def test_replay_combined_with_processing(
