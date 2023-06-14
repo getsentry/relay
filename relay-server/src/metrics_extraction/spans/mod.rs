ea1d63892e13e18ae1c11d69b6ecb253eacc467d
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-06-14 14:43:51 +0200

    feat(spans): Extract transaction's HTTP status code into span tags as `http.status_code` (#2196)
    Each span has its own status code, but also information on the status
    code of the parent transaction they belong to. We're not looking for
    perfect results here and `extract_http_status_code` provides good enough
    values.

--- relay-server/src/metrics_extraction/spans/mod.rs
+++ relay-server/src/metrics_extraction/spans/mod.rs
@@ -1,4 +1,5 @@
 use crate::metrics_extraction::transactions::types::ExtractMetricsError;
+use crate::metrics_extraction::utils::extract_http_status_code;
 use crate::metrics_extraction::utils::http_status_code_from_span;
 use crate::metrics_extraction::utils::{
     extract_transaction_op, get_eventuser_tag, get_trace_context,
@@ -77,6 +78,10 @@ pub(crate) fn extract_span_metrics(
         if let Some(op) = extract_transaction_op(trace_context) {
             shared_tags.insert("transaction.op".to_owned(), op.to_lowercase());
         }
+    }
+
+    if let Some(transaction_http_status_code) = extract_http_status_code(event) {
+        shared_tags.insert("http.status_code".to_owned(), transaction_http_status_code);
     }
 
     let Some(spans) = event.spans.value_mut() else { return Ok(()) };
