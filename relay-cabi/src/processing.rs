5585240ef5fb1c936ad2bd1562c2f93610c22043
Jan Michael Auer <mail@jauer.org>
2023-06-27 14:16:14 +0200

    ref(sampling): Remove unused custom rule condition (#2255)
    The initial implementation of rule evaluation for dynamic sampling was
    supposed to support all inbound filters. Since those filters have more
    complex conditions, such as `is_legacy_browser` or `is_web_crawler`,
    virtual fields and custom conditions were added.
    
    These have since been unused, so this PR removes them without
    replacement. This further allows to remove the `client_ip` from the rule
    matcher interface.

--- relay-cabi/src/processing.rs
+++ relay-cabi/src/processing.rs
@@ -370,13 +370,7 @@ pub unsafe extern "C" fn run_dynamic_sampling(
     // Only if we have both dsc and event we want to run dynamic sampling, otherwise we just return
     // the merged sampling configs.
     let match_result = if let (Ok(event), Ok(dsc)) = (event, dsc) {
-        SamplingMatch::match_against_rules(
-            rules.iter(),
-            event.value(),
-            Some(&dsc),
-            None,
-            Utc::now(),
-        )
+        SamplingMatch::match_against_rules(rules.iter(), event.value(), Some(&dsc), Utc::now())
     } else {
         None
     };
