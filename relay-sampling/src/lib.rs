89f05beadfdffafe7af1e74133d8b13136364a21
Jan Michael Auer <mail@jauer.org>
2023-07-03 17:56:30 +0200

    fix: Change access for `release.build`

--- relay-sampling/src/lib.rs
+++ relay-sampling/src/lib.rs
@@ -692,16 +692,15 @@ impl FieldValueProvider for Event {
                 }
                 _ => Value::Null,
             },
+            "release.build" => self
+                .parse_release()
+                .as_ref()
+                .and_then(|r| r.build_hash())
+                .map_or(Value::Null, Value::from),
             "release.package" => self
                 .parse_release()
                 .as_ref()
                 .and_then(|r| r.package())
-                .map_or(Value::Null, Value::from),
-            "release.version.build" => self
-                .parse_release()
-                .as_ref()
-                .and_then(|r| r.version())
-                .and_then(|v| v.build_code())
                 .map_or(Value::Null, Value::from),
             "release.version.short" => self
                 .parse_release()
