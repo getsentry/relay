15a3c01cde0dd34b001b02416d8546ddc847cfd9
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-07-05 15:05:59 +0200

    wip

--- tests/integration/fixtures/mini_sentry.py
+++ tests/integration/fixtures/mini_sentry.py
@@ -135,3 +135,262 @@ class Sentry(SentryLike):
 
         self.project_configs[project_id] = ret_val
         return ret_val
+
+    def full_project_config(self, project_id, dsn_public_key=None, extra=None):
+        basic = self.basic_project_config(project_id, dsn_public_key)
+        full = {
+            "organizationId": 1,
+            "config": {
+                "excludeFields": [],
+                "filterSettings": {},
+                "scrubIpAddresses": False,
+                "sensitiveFields": [],
+                "scrubDefaults": True,
+                "scrubData": True,
+                "groupingConfig": {
+                    "id": "legacy:2019-03-12",
+                    "enhancements": "eJybzDhxY05qemJypZWRgaGlroGxrqHRBABbEwcC",
+                },
+                "blacklistedIps": ["127.43.33.22"],
+                "trustedRelays": [],
+            },
+        }
+
+        extra = extra or {}
+        extra_config = extra.get("config", {})
+
+        ret_val = {
+            **basic,
+            **full,
+            **extra,
+            "config": {**basic["config"], **full["config"], **extra_config},
+        }
+
+        return ret_val
+
+    def add_full_project_config(self, project_id, dsn_public_key=None, extra=None):
+        ret_val = self.full_project_config(project_id, dsn_public_key, extra)
+        self.project_configs[project_id] = ret_val
+        return ret_val
+
+
+def _get_project_id(public_key, project_configs):
+    for project_id, project_config in project_configs.items():
+        for key_config in project_config["publicKeys"]:
+            if key_config["publicKey"] == public_key:
+                return project_id
+
+
+@pytest.fixture
+def mini_sentry(request):
+    app = Flask(__name__)
+    app.debug = True
+    sentry = None
+
+    authenticated_relays = {}
+
+    def is_trusted(relay_id, project_config):
+        relay_info = authenticated_relays[relay_id]
+        if relay_info.get("internal", False):
+            return True
+        if not project_config:
+            return False
+        return relay_info["publicKey"] in project_config["config"]["trustedRelays"]
+
+    def get_error_message(data):
+        exceptions = data.get("exception", {}).get("values", [])
+        exc_msg = ": ".join(e.get("value", "") for e in reversed(exceptions))
+        message = data.get("message", {})
+        message = message if type(message) == str else message.get("formatted")
+        return exc_msg or message or "unknown error"
+
+    @app.before_request
+    def count_hits():
+        # Consume POST body even if we don't like this request
+        # to no clobber the socket and buffers
+        _ = flask_request.data
+
+        if flask_request.url_rule:
+            sentry.hit(flask_request.url_rule.rule)
+
+        # Store endpoints theoretically support chunked transfer encoding,
+        # but for now, we're conservative and don't allow that anywhere.
+        if flask_request.headers.get("transfer-encoding"):
+            abort(400, "transfer encoding not supported")
+
+        sentry.request_log.append((flask_request.headers, flask_request.url))
+
+    @app.route("/api/0/relays/register/challenge/", methods=["POST"])
+    def get_challenge():
+        relay_id = flask_request.json["relay_id"]
+        assert relay_id == flask_request.headers["x-sentry-relay-id"]
+
+        if relay_id not in sentry.known_relays:
+            abort(403, "unknown relay")
+
+        relay_info = sentry.known_relays[relay_id]
+
+        public_key = flask_request.json["public_key"]
+        version = flask_request.json.get("version")
+        registered_version = relay_info["version"]
+
+        if version is None:
+            if _parse_version(registered_version) >= (20, 8):
+                abort(400, "missing version")
+
+        elif version != registered_version and not (
+            registered_version == "latest" and version == CURRENT_VERSION
+        ):
+            abort(403, "outdated version")
+
+        authenticated_relays[relay_id] = relay_info
+        return jsonify({"token": "123", "relay_id": relay_id})
+
+    @app.route("/api/0/relays/register/response/", methods=["POST"])
+    def check_challenge():
+        relay_id = flask_request.json["relay_id"]
+        assert relay_id == flask_request.headers["x-sentry-relay-id"]
+        assert relay_id in authenticated_relays
+        return jsonify({"relay_id": relay_id})
+
+    @app.route("/api/0/relays/live/", methods=["GET"])
+    def is_live():
+        return jsonify({"is_healthy": True})
+
+    @app.route("/api/666/envelope/", methods=["POST"])
+    def store_internal_error_event():
+        envelope = Envelope.deserialize(flask_request.data)
+        event = envelope.get_event()
+
+        if event is not None and sentry.fail_on_relay_error:
+            error = AssertionError("Relay sent us event: " + get_error_message(event))
+            sentry.test_failures.append(("/api/666/envelope/", error))
+
+        return jsonify({"event_id": uuid.uuid4().hex})
+
+    @app.route("/api/42/envelope/", methods=["POST"])
+    def store_event():
+        assert (
+            flask_request.headers.get("Content-Encoding", "") == "gzip"
+        ), "Relay should always compress store requests"
+        data = gzip.decompress(flask_request.data)
+
+        assert (
+            flask_request.headers.get("Content-Type") == "application/x-sentry-envelope"
+        ), "Relay sent us non-envelope data to store"
+
+        envelope = Envelope.deserialize(data)
+
+        sentry.captured_events.put(envelope)
+        return jsonify({"event_id": uuid.uuid4().hex})
+
+    @app.route("/api/<project>/store/", methods=["POST"])
+    @app.route("/api/<project>/envelope/", methods=["POST"])
+    def store_event_catchall(project):
+        raise AssertionError(f"Unknown project: {project}")
+
+    @app.route("/api/0/relays/projectids/", methods=["POST"])
+    def get_project_ids():
+        project_ids = {}
+        for public_key in flask_request.json["publicKeys"]:
+            project_ids[public_key] = _get_project_id(
+                public_key, sentry.project_configs
+            )
+        return jsonify(projectIds=project_ids)
+
+    @app.route("/api/0/relays/projectconfigs/", methods=["POST"])
+    def get_project_config():
+        relay_id = flask_request.headers["x-sentry-relay-id"]
+        if relay_id not in authenticated_relays:
+            abort(403, "relay not registered")
+
+        configs = {}
+        pending = []
+        version = flask_request.args.get("version")
+        if version in [None, "1"]:
+            for project_id in flask_request.json["projects"]:
+                project_config = sentry.project_configs[int(project_id)]
+                if is_trusted(relay_id, project_config):
+                    configs[project_id] = project_config
+
+        elif version in ["2", "3"]:
+            for public_key in flask_request.json["publicKeys"]:
+                # We store projects by id, but need to return by key
+                for project_config in sentry.project_configs.values():
+                    for key in project_config["publicKeys"]:
+                        if not is_trusted(relay_id, project_config):
+                            continue
+
+                        if key["publicKey"] == public_key:
+                            if (
+                                version == "3"
+                                and sentry.project_config_simulate_pending
+                            ):
+                                pending.append(public_key)
+                            else:
+                                # TODO 11 Nov 2020 (RaduW) horrible hack
+                                #  For some reason returning multiple public keys breaks Relay
+                                # Relay seems to work only with the first key
+                                # Need to figure out why that is.
+                                configs[public_key] = deepcopy(project_config)
+                                configs[public_key]["publicKeys"] = [key]
+
+        else:
+            abort(500, "unsupported version")
+
+        return jsonify(configs=configs, pending=pending)
+
+    @app.route("/api/0/relays/publickeys/", methods=["POST"])
+    def public_keys():
+        relay_id = flask_request.headers["x-sentry-relay-id"]
+        if relay_id not in authenticated_relays:
+            abort(403, "relay not registered")
+
+        ids = flask_request.json["relay_ids"]
+        keys = {}
+        relays = {}
+        for id in ids:
+            relay = authenticated_relays[id]
+            if relay:
+                keys[id] = relay["publicKey"]
+                relays[id] = relay
+
+        return jsonify(public_keys=keys, relays=relays)
+
+    @app.route("/api/0/relays/outcomes/", methods=["POST"])
+    def outcomes():
+        """
+        Mock endpoint for outcomes. SENTRY DOES NOT IMPLEMENT THIS ENDPOINT! This is just used to
+        verify Relay's batching behavior.
+        """
+        relay_id = flask_request.headers["x-sentry-relay-id"]
+        if relay_id not in authenticated_relays:
+            abort(403, "relay not registered")
+
+        outcomes_batch = flask_request.json
+        sentry.captured_outcomes.put(outcomes_batch)
+        return jsonify({})
+
+    @app.errorhandler(500)
+    def fail(e):
+        sentry.test_failures.append((flask_request.url, e))
+        raise e
+
+    def reraise_test_failures():
+        if sentry.test_failures:
+            pytest.fail(
+                "{n} exceptions happened in mini_sentry:\n\n{failures}".format(
+                    n=len(sentry.test_failures), failures=sentry.format_failures()
+                )
+            )
+
+    # This marker is used by pytest_runtest_call in our conftest.py
+    mark = pytest.mark.extra_failure_checks(checks=[reraise_test_failures])
+    request.node.add_marker(mark)
+
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+    server = WSGIServer(application=app, threaded=True)
+    server.start()
+    request.addfinalizer(server.stop)
+    sentry = Sentry(server.server_address, app)
+    return sentry
