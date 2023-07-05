48415a7473b486217fae10a9ad26fe4880819dae
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-07-05 12:10:46 +0200

    wip

--- tests/integration/fixtures/mini_sentry.py
+++ tests/integration/fixtures/mini_sentry.py
@@ -150,6 +150,7 @@ class Sentry(SentryLike):
                 "sensitiveFields": [],
                 "scrubDefaults": True,
                 "scrubData": True,
+                "transactionMetrics": {"version": 1},
                 "groupingConfig": {
                     "id": "legacy:2019-03-12",
                     "enhancements": "eJybzDhxY05qemJypZWRgaGlroGxrqHRBABbEwcC",
