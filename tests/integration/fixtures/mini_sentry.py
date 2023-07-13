85467d54552637e7aca211c6b489ee2038a830da
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-07-13 16:53:09 +0200

    wip

--- tests/integration/fixtures/mini_sentry.py
+++ tests/integration/fixtures/mini_sentry.py
@@ -112,6 +112,9 @@ class Sentry(SentryLike):
             "config": {
                 "allowedDomains": ["*"],
                 "trustedRelays": list(self.iter_public_keys()),
+                "transactionMetrics": {
+                    "version": 1,
+                },
                 "piiConfig": {
                     "rules": {},
                     "applications": {
