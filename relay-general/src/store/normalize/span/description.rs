222538623abd3faf14993c41f45bcca1762ba17a
Joris Bayer <joris.bayer@sentry.io>
2023-07-18 13:20:52 +0200

    feat(spans): Simplify scrubbed description for HTTP spans (#2324)
    Simplify parameterization of http.client span descriptions. Strip
    out everything except the HTTP method, hostname and port (unless 80 or
    443). Wildcard everything but the hostname and TLD e.g., `POST
    api.github.com/repos?param=value` becomes `POST *.github.com` and `POST
    http://127.0.0.1:10006/discover/snql` becomes `POST
    https://127.0.0.1:10006`.
    
    This is a temporary solution to bring cardinality to an acceptable
    level. We might return to a smarter solution, including automatic URL
    clustering, later.

--- relay-general/src/store/normalize/span/description.rs
+++ relay-general/src/store/normalize/span/description.rs
@@ -2,13 +2,13 @@
 use std::borrow::Cow;
 use std::collections::BTreeMap;
 
+use itertools::Itertools;
 use once_cell::sync::Lazy;
 use regex::Regex;
+use url::Url;
 
 use crate::protocol::Span;
-use crate::store::regexes::{
-    REDIS_COMMAND_REGEX, RESOURCE_NORMALIZER_REGEX, TRANSACTION_NAME_NORMALIZER_REGEX,
-};
+use crate::store::regexes::{REDIS_COMMAND_REGEX, RESOURCE_NORMALIZER_REGEX};
 use crate::store::{scrub_identifiers_with_regex, SpanDescriptionRule};
 use crate::types::{Annotated, ProcessingAction, ProcessingResult, Remark, RemarkType, Value};
 
@@ -57,10 +57,6 @@ static SQL_COLLAPSE_PLACEHOLDERS: Lazy<Regex> = Lazy::new(|| {
 static SQL_ALREADY_NORMALIZED_REGEX: Lazy<Regex> =
     Lazy::new(|| Regex::new(r#"/\?|\$1|%s"#).unwrap());
 
-/// Regex used by [`scrub_http`] in addition to [`TRANSACTION_NAME_NORMALIZER_REGEX`].
-static HTTP_SCRUB_ADDITIONAL: Lazy<Regex> =
-    Lazy::new(|| Regex::new(r#"(?i)\b(?P<hex>[0-9a-f][0-9a-f]+)\b"#).unwrap());
-
 /// Attempts to replace identifiers in the span description with placeholders.
 ///
 /// The resulting scrubbed description is stored in `data.description.scrubbed`, and serves as input
@@ -76,7 +72,9 @@ pub(crate) fn scrub_span_description(
     let mut scrubbed = span.description.clone();
 
     let did_scrub = match span.op.value() {
-        Some(op) if op.starts_with("http") => scrub_http(&mut scrubbed)?,
+        Some(op) if op.starts_with("http") => {
+            scrubbed.value_mut().as_mut().map_or(false, scrub_http)
+        }
         Some(op) if op.starts_with("cache") || op == "db.redis" => scrub_redis_keys(&mut scrubbed)?,
         Some(op) if op.starts_with("db") && op != "db.redis" => scrub_sql_queries(&mut scrubbed)?,
         Some(op) if op.starts_with("resource") => scrub_resource_identifiers(&mut scrubbed)?,
@@ -122,11 +120,52 @@ fn is_sql_query_scrubbed(query: &Annotated<String>) -> bool {
         .map_or(false, |q| SQL_ALREADY_NORMALIZED_REGEX.is_match(q))
 }
 
-fn scrub_http(string: &mut Annotated<String>) -> Result<bool, ProcessingAction> {
-    let mut scrubbed =
-        scrub_identifiers_with_regex(string, &TRANSACTION_NAME_NORMALIZER_REGEX, "*")?;
-    scrubbed |= scrub_identifiers_with_regex(string, &HTTP_SCRUB_ADDITIONAL, "*")?;
-    Ok(scrubbed)
+fn scrub_http(string: &mut String) -> bool {
+    let Some((method, url)) = string.split_once(' ') else { return false };
+    let Ok(url) = Url::parse(url) else { return false };
+    let Some(domain) = url.domain() else { return false };
+    let Some(domain) = normalize_domain(domain, url.port()) else { return false };
+    let scheme = url.scheme();
+
+    *string = format!("{method} {scheme}://{domain}");
+    true
+}
+
+fn normalize_domain(domain: &str, port: Option<u16>) -> Option<String> {
+    if let Some(allow_listed) = normalized_domain_from_allowlist(domain, port) {
+        return Some(allow_listed);
+    }
+
+    let mut tokens = domain.rsplitn(3, '.');
+    let tld = tokens.next();
+    let domain = tokens.next();
+    let prefix = tokens.next().map(|_| "*");
+
+    let mut replaced = prefix
+        .iter()
+        .chain(domain.iter())
+        .chain(tld.iter())
+        .join(".");
+
+    if let Some(port) = port {
+        replaced = format!("{replaced}:{port}");
+    }
+
+    if replaced.is_empty() {
+        return None;
+    }
+    Some(replaced)
+}
+
+/// Allow list of domains to not get subdomains scrubbed.
+const DOMAIN_ALLOW_LIST: &[&str] = &["127.0.0.1", "localhost"];
+
+fn normalized_domain_from_allowlist(domain: &str, port: Option<u16>) -> Option<String> {
+    if let Some(domain) = DOMAIN_ALLOW_LIST.iter().find(|allowed| **allowed == domain) {
+        let with_port = port.map_or_else(|| (*domain).to_owned(), |p| format!("{}:{}", domain, p));
+        return Some(with_port);
+    }
+    None
 }
 
 fn scrub_redis_keys(string: &mut Annotated<String>) -> Result<bool, ProcessingAction> {
@@ -301,63 +340,63 @@ mod tests {
         span_description_scrub_only_domain,
         "GET http://service.io",
         "http.client",
-        ""
+        "GET http://service.io"
     );
 
     span_description_test!(
         span_description_scrub_only_urllike_on_http_ops,
         "GET https://www.service.io/resources/01234",
         "http.client",
-        "GET https://www.service.io/resources/*"
+        "GET https://*.service.io"
     );
 
     span_description_test!(
         span_description_scrub_path_ids_end,
         "GET https://www.service.io/resources/01234",
         "http.client",
-        "GET https://www.service.io/resources/*"
+        "GET https://*.service.io"
     );
 
     span_description_test!(
         span_description_scrub_path_ids_middle,
         "GET https://www.service.io/resources/01234/details",
         "http.client",
-        "GET https://www.service.io/resources/*/details"
+        "GET https://*.service.io"
     );
 
     span_description_test!(
         span_description_scrub_path_multiple_ids,
         "GET https://www.service.io/users/01234-qwerty/settings/98765-adfghj",
         "http.client",
-        "GET https://www.service.io/users/*/settings/*"
+        "GET https://*.service.io"
     );
 
     span_description_test!(
         span_description_scrub_path_md5_hashes,
         "GET /clients/563712f9722fb0996ac8f3905b40786f/project/01234",
         "http.client",
-        "GET /clients/*/project/*"
+        ""
     );
 
     span_description_test!(
         span_description_scrub_path_sha_hashes,
         "GET /clients/403926033d001b5279df37cbbe5287b7c7c267fa/project/01234",
         "http.client",
-        "GET /clients/*/project/*"
+        ""
     );
 
     span_description_test!(
         span_description_scrub_hex,
         "GET /shop/de/f43/beef/3D6/my-beef",
         "http.client",
-        "GET /shop/*/*/*/*/my-*"
+        ""
     );
 
     span_description_test!(
         span_description_scrub_path_uuids,
         "GET /clients/8ff81d74-606d-4c75-ac5e-cee65cbbc866/project/01234",
         "http.client",
-        "GET /clients/*/project/*"
+        ""
     );
 
     // TODO(iker): Add span description test for URLs with paths
