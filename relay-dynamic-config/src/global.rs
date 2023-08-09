1659a4abb4b03cea2f979719df3fdb171c34c68b
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-08-09 13:55:04 +0200

    ref(globconfig): Set empty global config

--- relay-dynamic-config/src/global.rs
+++ relay-dynamic-config/src/global.rs
@@ -1,7 +1,4 @@
-use relay_general::store::MeasurementsConfig;
 use serde::{Deserialize, Serialize};
-
-use crate::TaggingRule;
 
 /// A dynamic configuration for all Relays passed down from Sentry.
 ///
@@ -9,50 +6,4 @@ use crate::TaggingRule;
 /// [`ProjectConfig`](crate::ProjectConfig)s small.
 #[derive(Default, Clone, Debug, Serialize, Deserialize)]
 #[serde(default, rename_all = "camelCase")]
-pub struct GlobalConfig {
-    /// Project configuration for measurements.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    measurements: Option<MeasurementsConfig>,
-    /// Project configuration for rules for applying metrics tags depending on
-    /// the event's content.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    metric_conditional_tagging: Option<Vec<TaggingRule>>,
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::GlobalConfig;
-
-    #[test]
-    fn test_global_config_roundtrip() {
-        let json = r#"{
-  "measurements": {
-    "builtinMeasurements": [
-      {
-        "name": "plate.size",
-        "unit": "5"
-      }
-    ],
-    "maxCustomMeasurements": 2
-  },
-  "metricConditionalTagging": [
-    {
-      "condition": {
-        "op": "gt",
-        "name": "food.deliciousness",
-        "value": 8
-      },
-      "targetMetrics": [
-        "tummy.satisfaction"
-      ],
-      "targetTag": "satisfied",
-      "tagValue": "hellyeah"
-    }
-  ]
-}"#;
-
-        let deserialized: GlobalConfig = serde_json::from_str(json).unwrap();
-        let reserialized = serde_json::to_string_pretty(&deserialized).unwrap();
-        assert_eq!(json, reserialized);
-    }
-}
+pub struct GlobalConfig {}
