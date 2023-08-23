70cddfd0a837b2be9e040736818b89e142a9cd23
Joris Bayer <joris.bayer@sentry.io>
2023-08-23 20:10:18 +0200

    Use different chart types for different metric types

--- relay-admin-dashboard/src/stats.rs
+++ relay-admin-dashboard/src/stats.rs
@@ -17,10 +17,15 @@ pub(crate) fn stats() -> Html {
             on_next_message(socket.clone(), move |message| {
                 if let Message::Bytes(message) = message {
                     let message = String::from_utf8_lossy(&message);
-                    let (name, tags, value) = parse_metric(&message);
+                    let Metric {
+                        ty,
+                        name,
+                        tags,
+                        value,
+                    } = parse_metric(&message);
                     // I gave up on the hole reactive framework here and decided
                     // to just pass the data to javascript.
-                    js::update_chart(name, tags, value);
+                    js::update_chart(ty, name, tags, value);
                     update_trigger.force_update();
                 }
             });
@@ -35,15 +40,28 @@ pub(crate) fn stats() -> Html {
     }
 }
 
-fn parse_metric(metric: &str) -> (&str, &str, f32) {
+#[derive(Debug, PartialEq)]
+struct Metric<'a> {
+    ty: &'a str,
+    name: &'a str,
+    tags: &'a str,
+    value: f32,
+}
+
+fn parse_metric(metric: &str) -> Metric {
     let mut parts = metric.split('|');
     let name_and_value = parts.next().unwrap_or("");
-    let _type = parts.next();
+    let ty = parts.next().unwrap_or("");
     let tags = parts.next().unwrap_or("");
 
     let (name, value) = name_and_value.split_once(':').unwrap_or(("", ""));
     let value = value.parse::<f32>().unwrap_or_default();
-    (name, tags, value)
+    Metric {
+        ty,
+        name,
+        tags,
+        value,
+    }
 }
 
 mod js {
@@ -53,7 +71,7 @@ mod js {
     #[wasm_bindgen]
     extern "C" {
         #[wasm_bindgen]
-        pub fn update_chart(metric: &str, tags: &str, value: f32); // TODO: can borrow?
+        pub fn update_chart(ty: &str, metric: &str, tags: &str, value: f32); // TODO: can borrow?
     }
 }
 
@@ -66,11 +84,12 @@ mod tests {
         let metric = "service.back_pressure:0|g|#service:relay_server::actors::processor::EnvelopeProcessorService";
         assert_eq!(
             parse_metric(metric),
-            (
-                "service.back_pressure",
-                "#service:relay_server::actors::processor::EnvelopeProcessorService",
-                0.0
-            )
+            Metric {
+                ty: "g",
+                name: "service.back_pressure",
+                tags: "#service:relay_server::actors::processor::EnvelopeProcessorService",
+                value: 0.0
+            }
         )
     }
 }
