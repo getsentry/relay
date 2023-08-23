a0861fa050d4e1751dc84563bf1e61b1d087fa3f
Joris Bayer <joris.bayer@sentry.io>
2023-08-23 10:44:14 +0200

    Helper function for socket fetch

--- relay-admin-dashboard/src/main.rs
+++ relay-admin-dashboard/src/main.rs
@@ -1,5 +1,7 @@
 use std::borrow::BorrowMut;
+use std::cell::RefCell;
 use std::collections::VecDeque;
+use std::rc::Rc;
 
 use futures::StreamExt;
 use gloo_net::websocket::{futures::WebSocket, Message};
@@ -23,10 +25,25 @@ fn main() {
     yew::Renderer::<App>::new().render();
 }
 
+fn on_next_message(socket: Rc<RefCell<Option<WebSocket>>>, f: impl Fn(Message) + 'static) {
+    wasm_bindgen_futures::spawn_local(async move {
+        // Take the socket so it will not be polled concurrently:
+        let inner_socket = (*socket).borrow_mut().take();
+        if let Some(mut inner_socket) = inner_socket {
+            if let Some(Ok(message)) = inner_socket.next().await {
+                f(message);
+            }
+            // Put the socket back
+            (*socket).borrow_mut().replace(inner_socket);
+        }
+    });
+}
+
 #[function_component(Logs)]
 fn logs() -> Html {
     let update_trigger = use_force_update();
     let log_entries = use_mut_ref(VecDeque::new);
+
     let socket = use_mut_ref(|| {
         Some(WebSocket::open(&format!("ws://{RELAY_URL}/api/relay/logs/")).unwrap())
     });
@@ -34,23 +51,16 @@ fn logs() -> Html {
         let log_entries = log_entries.clone();
         let socket = socket.clone();
         use_effect(move || {
-            let mut socket = socket.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                // Take the socket so it will not be polled concurrently:
-                let inner_socket = socket.borrow_mut().take();
-                if let Some(mut inner_socket) = inner_socket {
-                    if let Some(Ok(Message::Text(message))) = inner_socket.next().await {
-                        let mut log_entries = (*log_entries).borrow_mut();
-                        while log_entries.len() >= MAX_LOG_SIZE {
-                            log_entries.pop_front();
-                        }
-                        log_entries.push_back(message);
-                        update_trigger.force_update();
+            on_next_message(socket.clone(), move |message| {
+                if let Message::Text(message) = message {
+                    let mut log_entries = (*log_entries).borrow_mut();
+                    while log_entries.len() >= MAX_LOG_SIZE {
+                        log_entries.pop_front();
                     }
-                    // Put the socket back
-                    socket.borrow_mut().replace(Some(inner_socket));
+                    log_entries.push_back(message);
+                    update_trigger.force_update();
                 }
-            });
+            })
         });
     }
 
