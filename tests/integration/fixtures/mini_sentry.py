33aaaa503dd220e619bb56013c020908c05646b4
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-08-28 14:57:01 +0200

    wip

--- tests/integration/fixtures/mini_sentry.py
+++ tests/integration/fixtures/mini_sentry.py
@@ -13,6 +13,8 @@ from werkzeug.serving import WSGIRequestHandler
 from pytest_localserver.http import WSGIServer
 from sentry_sdk.envelope import Envelope
 
+from tests.integration.fixtures.config import GLOBAL_CONFIG
+
 
 from . import SentryLike
 
@@ -410,33 +412,3 @@ def mini_sentry(request):  # noqa
     request.addfinalizer(server.stop)
     sentry = Sentry(server.server_address, app)
     return sentry
-
-
-GLOBAL_CONFIG = {
-    "measurements": {
-        "builtinMeasurements": [
-            {"name": "app_start_cold", "unit": "millisecond"},
-            {"name": "app_start_warm", "unit": "millisecond"},
-            {"name": "cls", "unit": "none"},
-            {"name": "fcp", "unit": "millisecond"},
-            {"name": "fid", "unit": "millisecond"},
-            {"name": "fp", "unit": "millisecond"},
-            {"name": "frames_frozen_rate", "unit": "ratio"},
-            {"name": "frames_frozen", "unit": "none"},
-            {"name": "frames_slow_rate", "unit": "ratio"},
-            {"name": "frames_slow", "unit": "none"},
-            {"name": "frames_total", "unit": "none"},
-            {"name": "inp", "unit": "millisecond"},
-            {"name": "lcp", "unit": "millisecond"},
-            {"name": "stall_count", "unit": "none"},
-            {"name": "stall_longest_time", "unit": "millisecond"},
-            {"name": "stall_percentage", "unit": "ratio"},
-            {"name": "stall_total_time", "unit": "millisecond"},
-            {"name": "ttfb.requesttime", "unit": "millisecond"},
-            {"name": "ttfb", "unit": "millisecond"},
-            {"name": "time_to_full_display", "unit": "millisecond"},
-            {"name": "time_to_initial_display", "unit": "millisecond"},
-        ],
-        "maxCustomMeasurements": 10,
-    }
-}
