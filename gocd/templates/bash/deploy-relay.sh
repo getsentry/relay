7611cec4354d84dee101b0bf848594cb035bedcb
Oleksandr Kylymnychenko <oleksandr@sentry.io>
2023-09-14 11:50:13 +0200

    Rename relay-pop -> relay, relay -> relay-internal

--- gocd/templates/bash/deploy-relay.sh
+++ gocd/templates/bash/deploy-relay.sh
@@ -6,5 +6,5 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
 
 /devinfra/scripts/k8s/k8s-deploy.py \
     --label-selector="service=relay" \
-    --image="us.gcr.io/sentryio/relay:${GO_REVISION_RELAY_REPO}" \
+    --image="us.gcr.io/sentryio/relay-internal:${GO_REVISION_RELAY_REPO}" \
     --container-name="relay"
