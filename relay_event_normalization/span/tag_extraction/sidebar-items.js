77974a671d39eb2baf33d025310ccd4ac3eb6781
phacops <phacops@users.noreply.github.com>
2023-09-19 06:24:48 +0000

    deploy: 3b265f2ca42ab32e5203d8150e68848369782c23

--- relay_event_normalization/span/tag_extraction/sidebar-items.js
+++ relay_event_normalization/span/tag_extraction/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["SpanTagKey"],"static":["HTTP_METHOD_EXTRACTOR_REGEX"]};
+window.SIDEBAR_ITEMS = {"enum":["SpanTagKey"],"fn":["extract_shared_tags"],"static":["HTTP_METHOD_EXTRACTOR_REGEX"]};
