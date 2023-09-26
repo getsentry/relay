b3ffc571a199d2d03467db6a5bd5fa68f4510be0
TBS1996 <TBS1996@users.noreply.github.com>
2023-09-26 12:53:14 +0000

    deploy: 7bb785a6840d9fbe47dd1ac89d0b704a09e68f98

--- relay_sampling/evaluation/sidebar-items.js
+++ relay_sampling/evaluation/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["merge_configs_and_match","merge_rules_from_configs","pseudo_random_from_uuid"],"struct":["MatchedRuleIds","SamplingMatch"]};
+window.SIDEBAR_ITEMS = {"enum":["Evaluation"],"struct":["MatchedRuleIds","SamplingEvaluator","SamplingMatch"]};
