55224f73d0ed71e544ed7f492c59b6ef04b1f6d9
Joris Bayer <joris.bayer@sentry.io>
2023-09-28 14:05:06 +0200

    fix merge

--- relay-event-normalization/src/normalize/span/description/sql/mod.rs
+++ relay-event-normalization/src/normalize/span/description/sql/mod.rs
@@ -665,7 +665,7 @@ mod tests {
         "/*some comment `my_function'*/ SELECT * FROM foo WHERE ...",
         "SELECT * FROM foo WHERE ..."
     );
- 
+
     scrub_sql_test!(
         jsonb,
         r#"SELECT * FROM "a" WHERE "a"."b" = $1 AND (obj->'id' <@ '[123]'::jsonb) AND "a"."b" != $2"#,
@@ -694,6 +694,7 @@ mod tests {
         uuid_in_table_name,
         "SELECT * FROM prefix_0123456789abcdef0123456789ABCDEF_006_suffix",
         "SELECT * FROM prefix_00_00_suffix"
+    );
 
     scrub_sql_test!(
         clickhouse,
