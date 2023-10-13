0ad1145dfecccfa2aa52536de2245b3254664a45
jjbayer <jjbayer@users.noreply.github.com>
2023-10-13 07:10:22 +0000

    deploy: 2a3802a4aea61149c029f4f7d4a979959f347f3b

--- implementors/relay_protocol/traits/trait.Getter.js
+++ implementors/relay_protocol/traits/trait.Getter.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "relay_event_schema":[["impl Getter for <a class=\"struct\" href=\"relay_event_schema/protocol/struct.Span.html\" title=\"struct relay_event_schema::protocol::Span\">Span</a>"],["impl Getter for <a class=\"struct\" href=\"relay_event_schema/protocol/struct.Event.html\" title=\"struct relay_event_schema::protocol::Event\">Event</a>"]],
-"relay_sampling":[["impl <a class=\"trait\" href=\"relay_protocol/traits/trait.Getter.html\" title=\"trait relay_protocol::traits::Getter\">Getter</a> for <a class=\"struct\" href=\"relay_sampling/dsc/struct.DynamicSamplingContext.html\" title=\"struct relay_sampling::dsc::DynamicSamplingContext\">DynamicSamplingContext</a>"]]
+"relay_sampling":[["impl Getter for <a class=\"struct\" href=\"relay_sampling/dsc/struct.DynamicSamplingContext.html\" title=\"struct relay_sampling::dsc::DynamicSamplingContext\">DynamicSamplingContext</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
