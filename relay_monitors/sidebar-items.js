238aa4903f74fc7ae199c3ee5e931b18927685b8
evanpurkhiser <evanpurkhiser@users.noreply.github.com>
2023-10-18 23:20:15 +0000

    deploy: 6a343f8f1ad8178d299bf9a67cbcf529ccad0377

--- relay_monitors/sidebar-items.js
+++ relay_monitors/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CheckInStatus","ProcessCheckInError"],"fn":["process_check_in"],"struct":["CheckIn","CheckInContexts","CheckInTrace","MonitorConfig"]};
+window.SIDEBAR_ITEMS = {"enum":["CheckInStatus","ProcessCheckInError"],"fn":["process_check_in"],"struct":["CheckIn","CheckInContexts","CheckInTrace","MonitorConfig","ProcessedCheckInResult"]};
