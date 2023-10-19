517f125ac250a4d6b8c704b5ccfcb629f2683903
Joris Bayer <joris.bayer@sentry.io>
2023-10-19 10:30:37 +0200

    ref: imports

--- relay-protocol/src/lib.rs
+++ relay-protocol/src/lib.rs
@@ -23,17 +23,15 @@ mod macros;
 mod meta;
 mod size;
 mod traits;
-mod utils;
 mod value;
 
 pub use self::annotated::*;
-pub use self::condition::*;
+pub use self::condition::RuleCondition;
 pub use self::impls::*;
 pub use self::macros::*;
 pub use self::meta::*;
 pub use self::size::*;
 pub use self::traits::*;
-pub use self::utils::*;
 pub use self::value::*;
 
 #[cfg(feature = "derive")]
