7ed09c6a05b5ab9ec0d871c8a9f878001a9ee9a1
Pierre Massat <pierre.massat@sentry.io>
2023-10-20 08:27:17 -0400

    Add docs auto-generation
    Co-authored-by: Joris Bayer <joris.bayer@sentry.io>

--- relay-spans/src/lib.rs
+++ relay-spans/src/lib.rs
@@ -1,3 +1,9 @@
+#![warn(missing_docs)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/getsentry/relay/master/artwork/relay-icon.png",
+    html_favicon_url = "https://raw.githubusercontent.com/getsentry/relay/master/artwork/relay-icon.png"
+)]
+
 pub use crate::span::Span;
 
 mod span;
