00c9d966ab96fb61632807835a5b24f5a6ee6043
Shruthi <s3jaganathan@gmail.com>
2023-11-27 09:00:40 -0500

    Update relay-event-normalization/src/normalize/utils.rs
    Co-authored-by: Joris Bayer <joris.bayer@sentry.io>

--- relay-event-normalization/src/normalize/utils.rs
+++ relay-event-normalization/src/normalize/utils.rs
@@ -59,7 +59,7 @@ pub const MOBILE_SDKS: [&str; 4] = [
     "sentry.javascript.react-native",
 ];
 
-/// Allowed value for main thread name
+/// Allowed value for main thread name.
 pub const MAIN_THREAD_NAME: &str = "main";
 
 /// Maximum length of a mobile span or measurement in milliseconds.
