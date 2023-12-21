9f6e5bb4d95a82993c8d7621e1f27fa040fc9cd5
Jan Michael Auer <mail@jauer.org>
2023-12-21 13:50:57 +0100

    ref(server): Create reusable utils for metric outcomes

--- relay-server/src/actors/processor.rs
+++ relay-server/src/actors/processor.rs
@@ -29,7 +29,7 @@ use relay_metrics::{Bucket, BucketsView, MergeBuckets, MetricMeta, MetricNamespa
 use relay_pii::PiiConfigError;
 use relay_profiling::ProfileId;
 use relay_protocol::{Annotated, Value};
-use relay_quotas::{DataCategory, ItemScoping, RateLimits, ReasonCode, Scoping};
+use relay_quotas::{DataCategory, ItemScoping, RateLimits, Scoping};
 use relay_sampling::evaluation::{MatchedRuleIds, ReservoirCounters, ReservoirEvaluator};
 use relay_statsd::metric;
 use relay_system::{Addr, FromMessage, NoResponse, Service};
@@ -52,15 +52,13 @@ use crate::actors::project::ProjectState;
 use crate::actors::project_cache::{AddMetricMeta, ProjectCache};
 use crate::actors::test_store::TestStore;
 use crate::actors::upstream::{SendRequest, UpstreamRelay};
-use crate::envelope::{ContentType, Envelope, Item, ItemType, SourceQuantities};
+use crate::envelope::{ContentType, Envelope, Item, ItemType};
 use crate::extractors::{PartialDsn, RequestMeta};
 use crate::metrics_extraction::transactions::types::ExtractMetricsError;
 use crate::metrics_extraction::transactions::{ExtractedMetrics, TransactionExtractor};
 use crate::service::ServiceError;
 use crate::statsd::{RelayCounters, RelayHistograms, RelayTimers};
-use crate::utils::{
-    self, extract_transaction_count, ExtractionMode, ManagedEnvelope, SamplingResult,
-};
+use crate::utils::{self, ExtractionMode, ManagedEnvelope, SamplingResult};
 
 mod attachment;
 mod dynamic_sampling;
@@ -233,23 +231,6 @@ impl ExtractedMetrics {
             ));
         }
     }
-}
-
-fn source_quantities_from_buckets(
-    buckets: &BucketsView,
-    extraction_mode: ExtractionMode,
-) -> SourceQuantities {
-    buckets
-        .iter()
-        .filter_map(|bucket| extract_transaction_count(&bucket, extraction_mode))
-        .fold(SourceQuantities::default(), |acc, c| {
-            let profile_count = if c.has_profile { c.count } else { 0 };
-
-            SourceQuantities {
-                transactions: acc.transactions + c.count,
-                profiles: acc.profiles + profile_count,
-            }
-        })
 }
 
 /// A state container for envelope processing.
@@ -1373,63 +1354,12 @@ impl EnvelopeProcessorService {
         }
     }
 
-    /// Records the outcomes of the dropped buckets.
-    fn drop_buckets_with_outcomes(
-        &self,
-        reason_code: Option<ReasonCode>,
-        total_buckets: usize,
-        scoping: Scoping,
-        bucket_partitions: &BTreeMap<Option<u64>, Vec<Bucket>>,
-        mode: ExtractionMode,
-    ) {
-        let mut source_quantities = SourceQuantities::default();
-
-        for buckets in bucket_partitions.values() {
-            source_quantities += source_quantities_from_buckets(&BucketsView::new(buckets), mode);
-        }
-
-        let timestamp = Utc::now();
-
-        if source_quantities.transactions > 0 {
-            self.inner.outcome_aggregator.send(TrackOutcome {
-                timestamp,
-                scoping,
-                outcome: Outcome::RateLimited(reason_code.clone()),
-                event_id: None,
-                remote_addr: None,
-                category: DataCategory::Transaction,
-                quantity: source_quantities.transactions as u32,
-            });
-        }
-        if source_quantities.profiles > 0 {
-            self.inner.outcome_aggregator.send(TrackOutcome {
-                timestamp,
-                scoping,
-                outcome: Outcome::RateLimited(reason_code.clone()),
-                event_id: None,
-                remote_addr: None,
-                category: DataCategory::Profile,
-                quantity: source_quantities.profiles as u32,
-            });
-        }
-
-        self.inner.outcome_aggregator.send(TrackOutcome {
-            timestamp,
-            scoping,
-            outcome: Outcome::RateLimited(reason_code),
-            event_id: None,
-            remote_addr: None,
-            category: DataCategory::MetricBucket,
-            quantity: total_buckets as u32,
-        });
-    }
-
     /// Returns `true` if the batches should be rate limited.
     fn rate_limit_batches(
         &self,
         cached_rate_limits: RateLimits,
         scoping: Scoping,
-        bucket_partitions: &BTreeMap<Option<u64>, Vec<Bucket>>,
+        partitions: &BTreeMap<Option<u64>, Vec<Bucket>>,
         max_batch_size_bytes: usize,
         project_state: Arc<ProjectState>,
     ) -> bool {
@@ -1448,7 +1378,7 @@ impl EnvelopeProcessorService {
         };
 
         // We couldn't use the bucket length directly because batching may change the amount of buckets.
-        let total_buckets: usize = bucket_partitions
+        let total_buckets: usize = partitions
             .values()
             .flat_map(|buckets| {
                 // Cheap operation because there's no allocations.
@@ -1459,21 +1389,16 @@ impl EnvelopeProcessorService {
             .sum();
 
         // For limiting the amount of redis calls we make, in case we already passed the limit.
-        if cached_rate_limits
-            .check_with_quotas(quotas, item_scoping)
-            .is_limited()
-        {
-            relay_log::info!("dropping {total_buckets} buckets due to throughput ratelimit");
-            let reason_code = cached_rate_limits
-                .longest()
-                .and_then(|limit| limit.reason_code.clone());
+        let limits = cached_rate_limits.check_with_quotas(quotas, item_scoping);
+        if limits.is_limited() {
+            relay_log::debug!("dropping {total_buckets} buckets due to throughput ratelimit");
 
-            self.drop_buckets_with_outcomes(
-                reason_code,
-                total_buckets,
+            let reason_code = limits.longest().and_then(|limit| limit.reason_code.clone());
+            utils::reject_metrics(
+                self.inner.outcome_aggregator.clone(),
+                utils::extract_metric_quantities(partitions.values().flatten(), mode),
                 scoping,
-                bucket_partitions,
-                mode,
+                Outcome::RateLimited(reason_code),
             );
 
             return true;
@@ -1485,17 +1410,16 @@ impl EnvelopeProcessorService {
             // the count so that other relays will be updated too.
             match rate_limiter.is_rate_limited(quotas, item_scoping, total_buckets, false) {
                 Ok(limits) if limits.is_limited() => {
-                    relay_log::info!(
+                    relay_log::debug!(
                         "dropping {total_buckets} buckets due to throughput ratelimit"
                     );
 
                     let reason_code = limits.longest().and_then(|limit| limit.reason_code.clone());
-                    self.drop_buckets_with_outcomes(
-                        reason_code,
-                        total_buckets,
+                    utils::reject_metrics(
+                        self.inner.outcome_aggregator.clone(),
+                        utils::extract_metric_quantities(partitions.values().flatten(), mode),
                         scoping,
-                        bucket_partitions,
-                        mode,
+                        Outcome::RateLimited(reason_code),
                     );
 
                     self.inner
@@ -1552,7 +1476,7 @@ impl EnvelopeProcessorService {
         let EncodeMetrics {
             buckets,
             scoping,
-            extraction_mode,
+            extraction_mode: mode,
             project_state,
             rate_limits: cached_rate_limits,
             enable_cardinality_limiter,
@@ -1587,7 +1511,11 @@ impl EnvelopeProcessorService {
 
             for batch in BucketsView::new(&buckets).by_size(max_batch_size_bytes) {
                 let mut envelope = Envelope::from_request(None, RequestMeta::outbound(dsn.clone()));
-                envelope.add_item(create_metrics_item(&batch, extraction_mode));
+
+                let mut item = Item::new(ItemType::MetricBuckets);
+                item.set_source_quantities(utils::extract_metric_quantities(&batch, mode));
+                item.set_payload(ContentType::Json, serde_json::to_vec(&buckets).unwrap());
+                envelope.add_item(item);
 
                 let mut envelope = ManagedEnvelope::standalone(
                     envelope,
@@ -1713,15 +1641,6 @@ impl Service for EnvelopeProcessorService {
             }
         });
     }
-}
-
-fn create_metrics_item(buckets: &BucketsView<'_>, extraction_mode: ExtractionMode) -> Item {
-    let source_quantities = source_quantities_from_buckets(buckets, extraction_mode);
-    let mut item = Item::new(ItemType::MetricBuckets);
-    item.set_source_quantities(source_quantities);
-    item.set_payload(ContentType::Json, serde_json::to_vec(&buckets).unwrap());
-
-    item
 }
 
 #[cfg(test)]
--- relay-server/src/utils/metrics_rate_limits.rs
+++ relay-server/src/utils/metrics_rate_limits.rs
@@ -8,6 +8,7 @@ use relay_quotas::{DataCategory, ItemScoping, Quota, RateLimits, Scoping};
 use relay_system::Addr;
 
 use crate::actors::outcome::{DiscardReason, Outcome, TrackOutcome};
+use crate::envelope::SourceQuantities;
 
 /// Contains all data necessary to rate limit metrics or metrics buckets.
 #[derive(Debug)]
@@ -36,7 +37,7 @@ pub struct MetricsLimiter<Q: AsRef<Vec<Quota>> = Vec<Quota>> {
 
 const PROFILE_TAG: &str = "has_profile";
 
-/// Extracts the transaction count from a metric.
+/// Extracts quota information from a metric.
 ///
 /// If the metric was extracted from a or multiple transaction, it returns the amount
 /// of datapoints contained in the bucket.
@@ -44,10 +45,7 @@ const PROFILE_TAG: &str = "has_profile";
 /// Additionally tracks whether the transactions also contained profiling information.
 ///
 /// Returns `None` if the metric was not extracted from transactions.
-pub fn extract_transaction_count(
-    metric: &BucketView<'_>,
-    mode: ExtractionMode,
-) -> Option<TransactionCount> {
+fn count_metric_bucket(metric: BucketView<'_>, mode: ExtractionMode) -> Option<TransactionCount> {
     let mri = match MetricResourceIdentifier::parse(metric.name()) {
         Ok(mri) => mri,
         Err(_) => {
@@ -71,6 +69,54 @@ pub fn extract_transaction_count(
         && metric.tag(PROFILE_TAG) == Some("true");
 
     Some(TransactionCount { count, has_profile })
+}
+
+/// Extracts quota information from a list of metric buckets.
+pub fn extract_metric_quantities<'a, I, V>(buckets: I, mode: ExtractionMode) -> SourceQuantities
+where
+    I: IntoIterator<Item = V>,
+    BucketView<'a>: From<V>,
+{
+    let mut quantities = SourceQuantities::default();
+
+    for bucket in buckets {
+        quantities.buckets += 1;
+        if let Some(count) = count_metric_bucket(bucket.into(), mode) {
+            quantities.transactions += count.count;
+            if count.has_profile {
+                quantities.profiles += count.count;
+            }
+        }
+    }
+
+    quantities
+}
+
+pub fn reject_metrics(
+    addr: Addr<TrackOutcome>,
+    quantities: SourceQuantities,
+    scoping: Scoping,
+    outcome: Outcome,
+) {
+    let timestamp = Utc::now();
+
+    let categories = [
+        (DataCategory::Transaction, quantities.transactions as u32),
+        (DataCategory::Profile, quantities.profiles as u32),
+        (DataCategory::MetricBucket, quantities.buckets as u32),
+    ];
+
+    for (category, quantity) in categories {
+        addr.send(TrackOutcome {
+            timestamp,
+            scoping,
+            outcome: outcome.clone(),
+            event_id: None,
+            remote_addr: None,
+            category,
+            quantity,
+        });
+    }
 }
 
 /// Wether to extract transaction and profile count based on the usage or duration metric.
@@ -118,7 +164,7 @@ impl<Q: AsRef<Vec<Quota>>> MetricsLimiter<Q> {
     ) -> Result<Self, Vec<Bucket>> {
         let counts: Vec<_> = buckets
             .iter()
-            .map(|metric| extract_transaction_count(&BucketView::new(metric), mode))
+            .map(|metric| count_metric_bucket(BucketView::new(metric), mode))
             .collect();
 
         // Accumulate the total transaction count and profile count
