a44019660e15ff5dbae4d45468911d22a6c9a6b6
Nar Saynorath <nar.saynorath@sentry.io>
2024-01-03 12:06:52 -0500

    feat(app-start): Add duration metric for top-level app start spans
    Adds a new MetricSpec and conditions for retrieving tags for a duration
    metric.

--- relay-server/src/metrics_extraction/event.rs
+++ relay-server/src/metrics_extraction/event.rs
@@ -1002,6 +1002,23 @@ mod tests {
                         "resource.render_blocking_status": "blocking"
                     },
                     "hash": "e2fae740cccd3789"
+                },
+                {
+                    "timestamp": 1694732408.3145,
+                    "start_timestamp": 1694732407.8367,
+                    "exclusive_time": 477.800131,
+                    "description": "Cold Start",
+                    "op": "app.start.cold",
+                    "span_id": "97c0ef9770a02f9d",
+                    "parent_span_id": "9756d8d7b2b364ff",
+                    "trace_id": "77aeb1c16bb544a4a39b8d42944947a3",
+                    "data": {
+                        "http.decoded_response_content_length": 128950,
+                        "http.response_content_length": 36170,
+                        "http.response_transfer_size": 36470,
+                        "resource.render_blocking_status": "blocking"
+                    },
+                    "hash": "e2fae740cccd3789"
                 }
             ]
         }
