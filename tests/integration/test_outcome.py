a4407787e3fd3fd3b85230b524082eee4f85d31f
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2024-01-05 10:21:39 +0100

    fix lint

--- tests/integration/test_outcome.py
+++ tests/integration/test_outcome.py
@@ -1593,7 +1593,9 @@ def test_profile_outcomes_rate_limited(
     assert outcomes == expected_outcomes, outcomes
 
 
-def test_foo(mini_sentry, relay_with_processing, metrics_consumer, outcomes_consumer):
+def test_global_throughput_limit(
+    mini_sentry, relay_with_processing, metrics_consumer, outcomes_consumer
+):
     metrics_consumer = metrics_consumer()
     outcomes_consumer = outcomes_consumer()
 
