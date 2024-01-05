f9ab69b7c3c190d18b41c96af0ec19e8563b4e4c
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2024-01-05 11:47:03 +0100

    debug in ci

--- tests/integration/test_outcome.py
+++ tests/integration/test_outcome.py
@@ -1647,7 +1647,7 @@ def test_global_throughput_limit(
         relay.send_metrics_buckets(project_id, [bucket])
         time.sleep(0.5)
 
-    time.sleep(5)
+    time.sleep(30)
     produced_buckets = [m for m, _ in metrics_consumer.get_metrics()]
     outcomes = outcomes_consumer.get_outcomes()
 
