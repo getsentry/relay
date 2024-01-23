69d53990f8ce5ce871373f859db6c122393ebeb7
Pierre Massat <pierre.massat@sentry.io>
2024-01-23 14:57:34 -0500

    Add a metric for the raw INP value

--- relay-dynamic-config/src/defaults.rs
+++ relay-dynamic-config/src/defaults.rs
@@ -450,6 +450,29 @@ fn span_metrics(is_extract_all: bool) -> impl IntoIterator<Item = MetricSpec> {
             category: DataCategory::Span,
             mri: "d:spans/webvital.score.inp@ratio".into(),
             field: Some("span.measurements.score.weight.inp.value".into()),
+            condition: Some(is_allowed_browser.clone()),
+            tags: vec![
+                Tag::with_key("transaction.op")
+                    .from_field("span.sentry_tags.transaction.op")
+                    .always(),
+                Tag::with_key("transaction")
+                    .from_field("span.sentry_tags.transaction")
+                    .always(),
+                Tag::with_key("environment")
+                    .from_field("span.sentry_tags.environment")
+                    .always(),
+                Tag::with_key("release")
+                    .from_field("span.sentry_tags.release")
+                    .always(),
+                Tag::with_key("browser.name")
+                    .from_field("span.sentry_tags.browser.name")
+                    .always(), // already guarded by condition on metric
+            ],
+        },
+        MetricSpec {
+            category: DataCategory::Span,
+            mri: "d:spans/webvital.inp@ratio".into(),
+            field: Some("span.measurements.inp.value".into()),
             condition: Some(is_allowed_browser),
             tags: vec![
                 Tag::with_key("transaction.op")
