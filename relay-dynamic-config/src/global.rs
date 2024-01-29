7f3eb40ba59332af4310e0d06591bb4ba6d3b09f
David Herberth <david.herberth@sentry.io>
2024-01-29 10:48:30 +0100

    deserialize option from empty string

--- relay-dynamic-config/src/global.rs
+++ relay-dynamic-config/src/global.rs
@@ -82,6 +82,9 @@ pub struct Options {
 pub enum CardinalityLimiterMode {
     /// Cardinality limiter is enabled.
     #[default]
+    // De-serialize from the empty string, because the option was added to
+    // Sentry incorrectly which makes Sentry send the empty string as a default.
+    #[serde(alias = "")]
     Enabled,
     /// Cardinality limiter is enabled but cardinality limits are not enforced.
     Passive,
@@ -120,5 +123,20 @@ mod tests {
             .expect("failed to deserialize GlobalConfig");
 
         assert_eq!(deserialized, global_config);
+    }
+
+    #[test]
+    fn test_cardinality_limiter_mode_de_serialize() {
+        let m: CardinalityLimiterMode = serde_json::from_str("\"\"").unwrap();
+        assert_eq!(m, CardinalityLimiterMode::Enabled);
+        let m: CardinalityLimiterMode = serde_json::from_str("\"enabled\"").unwrap();
+        assert_eq!(m, CardinalityLimiterMode::Enabled);
+        let m: CardinalityLimiterMode = serde_json::from_str("\"disabled\"").unwrap();
+        assert_eq!(m, CardinalityLimiterMode::Disabled);
+        let m: CardinalityLimiterMode = serde_json::from_str("\"passive\"").unwrap();
+        assert_eq!(m, CardinalityLimiterMode::Passive);
+
+        let m = serde_json::to_string(&CardinalityLimiterMode::Enabled).unwrap();
+        assert_eq!(m, "\"enabled\"");
     }
 }
