3bb240e863982c398f3488ad58feab3f6d569b50
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2024-01-30 02:55:48 -0500

    feat(browser-starfish): add domain information for relative paths (#2975)
    Two changes here
    1. If the `span.domain` can't be retrieved from the span.description
    because its a relative path, we consumes the `url.scheme` property and
    `server.address` property to get the domain which was added to the SDK
    in this PR
    https://github.com/getsentry/sentry-javascript/issues/10206
    We scrub this domain and place it into `span.domain` in the metrics
    dataset.
    
    2. We also build the domain and store it under `raw_domain` for the
    indexed dataset in order to resolve the following issue.
    https://github.com/getsentry/sentry/issues/63208
    
    ---------
    
    Co-authored-by: Joris Bayer <joris.bayer@sentry.io>

--- relay-server/src/metrics_extraction/event.rs
+++ relay-server/src/metrics_extraction/event.rs
@@ -1009,6 +1009,26 @@ mod tests {
                         "resource.render_blocking_status": "blocking"
                     },
                     "hash": "e2fae740cccd3789"
+                },
+                {
+                    "timestamp": 1694732408.3145,
+                    "start_timestamp": 1694732407.8367,
+                    "exclusive_time": 477.800131,
+                    "description": "/static/myscript-v1.9.23.js",
+                    "op": "resource.script",
+                    "span_id": "97c0ef9770a02f9d",
+                    "parent_span_id": "9756d8d7b2b364ff",
+                    "trace_id": "77aeb1c16bb544a4a39b8d42944947a3",
+                    "data": {
+                        "http.decoded_response_content_length": 128950,
+                        "http.response_content_length": 36170,
+                        "http.response_transfer_size": 36470,
+                        "resource.render_blocking_status": "blocking",
+                        "server.address": "subdomain.example.com:5688",
+                        "url.same_origin": true,
+                        "url.scheme": "https"
+                    },
+                    "hash": "e2fae740cccd3789"
                 }
             ]
         }
