4331e89c854e35c3138ccee88795bdd9bebab803
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2024-02-08 16:16:08 +0100

    fix imports

--- relay-event-normalization/src/event.rs
+++ relay-event-normalization/src/event.rs
@@ -14,8 +14,9 @@ use relay_base_schema::metrics::{
 };
 use relay_event_schema::processor::{self, MaxChars, ProcessingAction, ProcessingState, Processor};
 use relay_event_schema::protocol::{
-    AsPair, Context, ContextInner, Contexts, DeviceClass, Event, EventType, Headers, IpAddr, Level,
-    LogEntry, Measurement, Measurements, NelContext, Request, SpanStatus, Tags, Timestamp, User,
+    AsPair, Context, ContextInner, Contexts, DeviceClass, Event, EventType, Exception, Headers,
+    IpAddr, Level, LogEntry, Measurement, Measurements, NelContext, Request, SpanStatus, Tags,
+    Timestamp, User,
 };
 use relay_protocol::{Annotated, Empty, Error, ErrorKind, Meta, Object, Value};
 use smallvec::SmallVec;
