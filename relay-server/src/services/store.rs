9224202c0cf2aa80b182e397eb0f43a04d0c68e9
Joshua Ferge <josh.ferge@sentry.io>
2024-02-09 11:50:55 -0800

    clean refactor

--- relay-server/src/services/store.rs
+++ relay-server/src/services/store.rs
@@ -284,26 +284,6 @@ impl StoreService {
             send_combined_replay_envelope,
         )?;
 
-        // if let (Some(replay_event), Some(replay_recording)) = (replay_event, replay_recording) {
-        //     let combined_replay_kafka_message = Self::extract_combined_replay_kafka_message(
-        //         event_id.ok_or(StoreError::NoEventId)?,
-        //         replay_event,
-        //         replay_recording,
-        //         scoping,
-        //         start_time,
-        //         retention,
-        //     );
-        //     self.produce(
-        //         KafkaTopic::ReplayRecordings,
-        //         scoping.organization_id,
-        //         KafkaMessage::ReplayRecordingNotChunked(combined_replay_kafka_message),
-        //     )?;
-        //     metric!(
-        //         counter(RelayCounters::ProcessingMessageProduced) += 1,
-        //         event_type = "replay_recording_combined"
-        //     );
-        // }
-
         if event_item.is_none() && attachments.is_empty() {
             // No event-related content. All done.
             return Ok(());
@@ -866,6 +846,7 @@ impl StoreService {
         send_combined_replay_envelope: bool,
     ) -> Result<(), StoreError> {
         if let Some(replay_event) = replay_event {
+            // always produce replay event
             self.produce_replay_event(
                 replay_id,
                 scoping.organization_id,
@@ -876,27 +857,28 @@ impl StoreService {
             )?;
 
             if let Some(replay_recording) = replay_recording {
+                /*
+                produce replay recording with replay event if combined flag is set
+                otherwise produce replay recording without replay event
+                */
                 if send_combined_replay_envelope {
                     self.produce_replay_recording(
                         Some(replay_id),
                         scoping,
                         replay_recording,
-                        Some(replay_event),
-                        start_time,
-                        retention_days,
-                    )?;
-                } else {
-                    self.produce_replay_recording(
-                        Some(replay_id),
-                        scoping,
-                        replay_recording,
-                        None,
+                        if send_combined_replay_envelope {
+                            Some(replay_event)
+                        } else {
+                            None
+                        },
                         start_time,
                         retention_days,
                     )?;
                 }
             }
         } else if let Some(replay_recording) = replay_recording {
+            // this block in theory should never happen, as SDK always sends replay_event and recording together,
+            // but just in case, if we only receive a recording without an event, we'll still produce it.
             self.produce_replay_recording(
                 Some(replay_id),
                 scoping,
