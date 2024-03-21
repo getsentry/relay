e8e46ebbb55827f955515254cf7f493122cf32ee
Joris Bayer <joris.bayer@sentry.io>
2024-03-21 12:36:34 +0100

    instr(kafka): Report queue length

--- relay-kafka/src/statsd.rs
+++ relay-kafka/src/statsd.rs
@@ -1,4 +1,4 @@
-use relay_statsd::{CounterMetric, HistogramMetric};
+use relay_statsd::{CounterMetric, GaugeMetric, HistogramMetric};
 
 pub enum KafkaCounters {
     /// Number of producer errors occurred after an envelope was already enqueued for sending to
@@ -30,6 +30,24 @@ impl HistogramMetric for KafkaHistograms {
     fn name(&self) -> &'static str {
         match self {
             Self::KafkaMessageSize => "kafka.message_size",
+        }
+    }
+}
+
+pub enum KafkaGauges {
+    /// The number of messages waiting to be acknowledged.
+    ///
+    /// See https://docs.confluent.io/platform/7.5/clients/librdkafka/html/rdkafka_8h.html#ad4b3b7659cf9a79d3353810d6b625bb7.
+    ///
+    /// This metric is tagged with:
+    /// - `topic`
+    InFlightCount,
+}
+
+impl GaugeMetric for KafkaGauges {
+    fn name(&self) -> &'static str {
+        match self {
+            KafkaGauges::InFlightCount => "kafka.in_flight_count",
         }
     }
 }
