e3199c0cf1636e587845f1c6b25b0fcc37b0a1a3
Joris Bayer <joris.bayer@sentry.io>
2024-03-25 12:01:03 +0100

    instr(kafka): Emit Kafka client stats

--- relay-kafka/src/producer/utils.rs
+++ relay-kafka/src/producer/utils.rs
@@ -4,13 +4,46 @@ use rdkafka::producer::{DeliveryResult, ProducerContext};
 use rdkafka::{ClientContext, Message};
 use relay_statsd::metric;
 
-use crate::statsd::KafkaCounters;
+use crate::statsd::{KafkaCounters, KafkaGauges};
 
 /// Kafka producer context that logs producer errors.
 #[derive(Debug)]
 pub struct CaptureErrorContext;
 
-impl ClientContext for CaptureErrorContext {}
+impl ClientContext for CaptureErrorContext {
+    /// Report client statistics as statsd metrics.
+    ///
+    /// This method is only called if `statistics.interval.ms` is configured.
+    fn stats(&self, statistics: rdkafka::Statistics) {
+        relay_statsd::metric!(gauge(KafkaGauges::MessageCount) = statistics.msg_cnt);
+        relay_statsd::metric!(gauge(KafkaGauges::MessageCountMax) = statistics.msg_max);
+        relay_statsd::metric!(gauge(KafkaGauges::MessageSize) = statistics.msg_size);
+        relay_statsd::metric!(gauge(KafkaGauges::MessageSizeMax) = statistics.msg_size_max);
+
+        for broker in statistics.brokers.values() {
+            relay_statsd::metric!(
+                gauge(KafkaGauges::OutboundBufferRequests) = broker.outbuf_cnt as u64,
+                broker_name = &broker.name
+            );
+            relay_statsd::metric!(
+                gauge(KafkaGauges::OutboundBufferMessages) = broker.outbuf_msg_cnt as u64,
+                broker_name = &broker.name
+            );
+            if let Some(connects) = broker.connects {
+                relay_statsd::metric!(
+                    gauge(KafkaGauges::Connects) = connects as u64,
+                    broker_name = &broker.name
+                );
+            }
+            if let Some(disconnects) = broker.disconnects {
+                relay_statsd::metric!(
+                    gauge(KafkaGauges::Disconnects) = disconnects as u64,
+                    broker_name = &broker.name
+                );
+            }
+        }
+    }
+}
 
 impl ProducerContext for CaptureErrorContext {
     type DeliveryOpaque = ();
