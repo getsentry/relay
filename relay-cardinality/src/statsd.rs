65b38f2075d8e39b2a036cfbe3cfd0728037b00e
David Herberth <david.herberth@sentry.io>
2024-03-26 15:04:57 +0100

    Revert "ref(cardinality): Switch histogram to gauge" (#3340)
    Turns out datadog gauges suck compared to Sentry gauges.

--- relay-cardinality/src/statsd.rs
+++ relay-cardinality/src/statsd.rs
@@ -1,4 +1,4 @@
-use relay_statsd::{CounterMetric, GaugeMetric, SetMetric, TimerMetric};
+use relay_statsd::{CounterMetric, HistogramMetric, SetMetric, TimerMetric};
 
 /// Counter metrics for the Relay Cardinality Limiter.
 pub enum CardinalityLimiterCounters {
@@ -81,7 +81,7 @@ impl TimerMetric for CardinalityLimiterTimers {
     }
 }
 
-pub enum CardinalityLimiterGauges {
+pub enum CardinalityLimiterHistograms {
     /// Amount of hashes sent to Redis to check the cardinality.
     ///
     /// This metric is tagged with:
@@ -96,7 +96,7 @@ pub enum CardinalityLimiterGauges {
     RedisSetCardinality,
 }
 
-impl GaugeMetric for CardinalityLimiterGauges {
+impl HistogramMetric for CardinalityLimiterHistograms {
     fn name(&self) -> &'static str {
         match *self {
             #[cfg(feature = "redis")]
