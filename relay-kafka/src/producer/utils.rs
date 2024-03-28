58f978ab113478415d6c15fcdebc2741e34cff20
Joris Bayer <joris.bayer@sentry.io>
2024-03-28 08:46:00 +0100

    instr(kafka): More broker stats (#3349)
    Follow-up to #3315: Also emit `int_latency` and `outbuf_latency`
    metrics.

--- relay-kafka/src/producer/utils.rs
+++ relay-kafka/src/producer/utils.rs
@@ -20,7 +20,7 @@ impl ClientContext for Context {
         relay_statsd::metric!(gauge(KafkaGauges::MessageSize) = statistics.msg_size);
         relay_statsd::metric!(gauge(KafkaGauges::MessageSizeMax) = statistics.msg_size_max);
 
-        for broker in statistics.brokers.values() {
+        for (_, broker) in statistics.brokers {
             relay_statsd::metric!(
                 gauge(KafkaGauges::OutboundBufferRequests) = broker.outbuf_cnt as u64,
                 broker_name = &broker.name
@@ -38,6 +38,18 @@ impl ClientContext for Context {
             if let Some(disconnects) = broker.disconnects {
                 relay_statsd::metric!(
                     gauge(KafkaGauges::Disconnects) = disconnects as u64,
+                    broker_name = &broker.name
+                );
+            }
+            if let Some(int_latency) = broker.int_latency {
+                relay_statsd::metric!(
+                    gauge(KafkaGauges::ProducerQueueLatency) = int_latency.max as u64,
+                    broker_name = &broker.name
+                );
+            }
+            if let Some(outbuf_latency) = broker.outbuf_latency {
+                relay_statsd::metric!(
+                    gauge(KafkaGauges::RequestQueueLatency) = outbuf_latency.max as u64,
                     broker_name = &broker.name
                 );
             }
