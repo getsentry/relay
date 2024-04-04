d187e0515b96e3dacbab8c55745c6cacdb764f07
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-04-04 15:43:46 -0400

    fix test

--- relay-dynamic-config/src/defaults.rs
+++ relay-dynamic-config/src/defaults.rs
@@ -132,14 +132,11 @@ fn span_metrics() -> impl IntoIterator<Item = MetricSpec> {
         | is_mobile.clone()
         | is_interaction
         | is_http.clone())
-        | is_cache.clone() & duration_condition.clone();
-
-    let know_modules_condition = (is_db.clone()
-        | is_resource.clone()
-        | is_mobile.clone()
-        | is_http.clone()
-        | is_cache.clone())
         & duration_condition.clone();
+
+    let know_modules_condition =
+        (is_db.clone() | is_resource.clone() | is_mobile.clone() | is_http.clone())
+            & duration_condition.clone();
 
     [
         MetricSpec {
