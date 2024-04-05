289b878545154ce9c85e6c3e794e6cca483f9c34
Jan Michael Auer <mail@jauer.org>
2024-04-05 12:18:30 +0200

    doc: Describe divergence from the public docs

--- relay-base-schema/src/metrics/mod.rs
+++ relay-base-schema/src/metrics/mod.rs
@@ -14,10 +14,11 @@ use std::{borrow::Cow, sync::OnceLock};
 /// Validates a metric name and normalizes it. This is the statsd name, i.e. without type or unit.
 ///
 /// Metric names cannot be empty, must begin with a letter and can consist of ASCII alphanumerics,
-/// underscores, dashes, slashes and periods.
+/// underscores, dashes, and periods. The implementation will further replace dashes with
+/// underscores.
 ///
-/// The function validates that the first character of the metric must be ASCII alphanumeric,
-/// later consecutive invalid characters in the name will be replaced with underscores.
+/// The function validates that the first character of the metric must be ASCII alphanumeric, later
+/// consecutive invalid characters in the name will be replaced with underscores.
 pub fn try_normalize_metric_name(name: &str) -> Option<Cow<'_, str>> {
     static NORMALIZE_RE: OnceLock<Regex> = OnceLock::new();
 
@@ -25,6 +26,7 @@ pub fn try_normalize_metric_name(name: &str) -> Option<Cow<'_, str>> {
         return None;
     }
 
+    // Note: `-` intentionally missing from this list.
     let normalize_re = NORMALIZE_RE.get_or_init(|| Regex::new("[^a-zA-Z0-9_.]+").unwrap());
     Some(normalize_re.replace_all(name, "_"))
 }
