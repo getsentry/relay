0247299c58fa77985a3ecec0874dfb041666eedb
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-04-12 09:47:26 +0200

    Fix

--- relay-kafka/src/config.rs
+++ relay-kafka/src/config.rs
@@ -3,8 +3,6 @@
 //! The configuration can be either;
 //! - [`TopicAssignment::Primary`] - the main and default kafka configuration,
 //! - [`TopicAssignment::Secondary`] - used to configure any additional kafka topic,
-//! - [`TopicAssignment::Sharded`] - if we want to configure multiple kafka clusters,
-//! we can create a mapping of the range of logical shards to the kafka configuration.
 
 use std::collections::BTreeMap;
 
