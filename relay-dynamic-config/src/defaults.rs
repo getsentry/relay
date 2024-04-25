c2c95ad1100153402ce67ce286ad408b7e32cc48
Nar Saynorath <nar.saynorath@sentry.io>
2024-04-25 08:39:51 -0400

    Use gauge

--- relay-dynamic-config/src/defaults.rs
+++ relay-dynamic-config/src/defaults.rs
@@ -729,7 +729,7 @@ fn span_metrics(
         },
         MetricSpec {
             category: DataCategory::Span,
-            mri: "d:spans/mobile.frames_delay@second".into(),
+            mri: "g:spans/mobile.frames_delay@second".into(),
             field: Some("span.measurements.frames.delay.value".into()),
             condition: Some(is_mobile.clone() & duration_condition.clone()),
             tags: vec![
