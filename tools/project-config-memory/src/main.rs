aa6ca5dd028bfe4427e6488ed8639b5c67fb2a00
Joris Bayer <joris.bayer@sentry.io>
2024-04-26 08:42:55 +0200

    alloc_stats

--- tools/project-config-memory/src/main.rs
+++ tools/project-config-memory/src/main.rs
@@ -1,9 +1,11 @@
+use std::alloc::System;
 use std::collections::BTreeMap;
 
 use memory_stats::memory_stats;
 
 use relay_dynamic_config::ProjectConfig;
 use serde::Deserialize;
+use stats_alloc::{Region, StatsAlloc, INSTRUMENTED_SYSTEM};
 
 #[derive(Deserialize)]
 struct Container {
@@ -15,33 +17,30 @@ struct OuterConfig {
     config: ProjectConfig,
 }
 
+#[global_allocator]
+static GLOBAL: &StatsAlloc<System> = &INSTRUMENTED_SYSTEM;
+
 fn main() {
+    let mut region = Region::new(GLOBAL);
+
     let args: Vec<_> = std::env::args().collect();
     let path = &args[1];
     let json = std::fs::read_to_string(path).unwrap();
 
     let before = memory_stats().unwrap().physical_mem;
 
+    region.reset();
     let configs: Container = serde_json::from_str(&json).unwrap();
-
-    // Make sure we parsed the right thing:
-    configs
-        .configs
-        .values()
-        .next()
-        .unwrap()
-        .as_ref()
-        .unwrap()
-        .config
-        .event_retention
-        .unwrap();
+    println!("Stats: {:#?}", region.change());
 
     let after = memory_stats().unwrap().physical_mem;
     let diff = after - before;
+
     println!(
         "{},{},{}",
         json.len(),
         diff,
         diff as f64 / json.len() as f64
     );
+    drop(configs);
 }
