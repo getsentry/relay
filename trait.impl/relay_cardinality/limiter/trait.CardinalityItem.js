be6684cc652db63531c5b3e9b62fbf83a6642ceb
Dav1dde <Dav1dde@users.noreply.github.com>
2024-04-30 14:15:56 +0000

    deploy: 4ee659c76429763c98669a91083ccaf4e89f7fff

--- trait.impl/relay_cardinality/limiter/trait.CardinalityItem.js
+++ trait.impl/relay_cardinality/limiter/trait.CardinalityItem.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"relay_metrics":[["impl <a class=\"trait\" href=\"relay_cardinality/limiter/trait.CardinalityItem.html\" title=\"trait relay_cardinality::limiter::CardinalityItem\">CardinalityItem</a> for <a class=\"struct\" href=\"relay_metrics/struct.Bucket.html\" title=\"struct relay_metrics::Bucket\">Bucket</a>"]]
+"relay_metrics":[["impl CardinalityItem for <a class=\"struct\" href=\"relay_metrics/struct.Bucket.html\" title=\"struct relay_metrics::Bucket\">Bucket</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
