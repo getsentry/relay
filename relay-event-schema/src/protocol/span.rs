2311ab48eda8e08cbc423f4f67d0c42506130f2d
Joris Bayer <joris.bayer@sentry.io>
2024-05-03 15:53:06 +0200

    feat(spans): Map sdk version

--- relay-event-schema/src/protocol/span.rs
+++ relay-event-schema/src/protocol/span.rs
@@ -303,6 +303,10 @@ pub struct SpanData {
     #[metastructure(field = "sentry.sdk.name")]
     pub sdk_name: Annotated<String>,
 
+    /// The sentry SDK version (see [`crate::protocol::ClientSdkInfo`]).
+    #[metastructure(field = "sentry.sdk.version")]
+    pub sdk_version: Annotated<String>,
+
     /// Slow Frames
     #[metastructure(field = "sentry.frames.slow", legacy_alias = "frames.slow")]
     pub frames_slow: Annotated<Value>,
