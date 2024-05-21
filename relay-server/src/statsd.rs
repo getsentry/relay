badf2eb9bb265d3cfe0698197cd07d7eab6ad6bb
Riccardo Busetti <riccardob36@gmail.com>
2024-05-21 11:13:01 +0200

    feat(dynamic-sampling): Track metric every time a DSC is built (#3575)

--- relay-server/src/statsd.rs
+++ relay-server/src/statsd.rs
@@ -664,6 +664,9 @@ pub enum RelayCounters {
     /// Counts how many transactions were created from segment spans.
     #[cfg(feature = "processing")]
     TransactionsFromSpans,
+    /// Counter for when the DSC is missing from an event that comes from an SDK that should support
+    /// it.
+    MissingDynamicSamplingContext,
 }
 
 impl CounterMetric for RelayCounters {
@@ -705,6 +708,7 @@ impl CounterMetric for RelayCounters {
             RelayCounters::DynamicSamplingDecision => "dynamic_sampling_decision",
             #[cfg(feature = "processing")]
             RelayCounters::TransactionsFromSpans => "transactions_from_spans",
+            RelayCounters::MissingDynamicSamplingContext => "missing_dynamic_sampling_context",
         }
     }
 }
