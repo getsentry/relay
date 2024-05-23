c52d243c627399909bb7db7fef052a1c5a56075d
Colton Allen <colton.allen@sentry.io>
2024-05-22 20:15:20 -0500

    Add public and private bucket read operations

--- relay-server/src/endpoints/remote_config.rs
+++ relay-server/src/endpoints/remote_config.rs
@@ -8,47 +8,67 @@ use google_cloud_storage::http::objects::get::GetObjectRequest;
 use crate::service::ServiceState;
 
 pub async fn handle(state: ServiceState) -> impl IntoResponse {
-    // This is temporarily placed here. I have no idea if we'll want to cache this client
-    // instance in some way.
-    //
-    // This should never fail so I'm unwrapping. If it does fail what would the appropriate
-    // fallback handling be? The product doesn't function without a client connection.
-    let config = ClientConfig::default().with_auth().await.unwrap();
-    let client = Client::new(config);
-
     // Configs are tied to the DSN: configuration/<dsn-public-key>/<environment>
     // For now we default to production, environment scoping will be handled later.
-    let filepath = state
+    let maybe_key = state
         .config()
         .public_key()
         .map(|v| format!("configuration/{}/production", v));
 
     // Is the DSN ever null? Why is it optional?
-    if let Some(fp) = filepath {
-        // This is temporary!
-        //
-        // As a proof of concept we'll reach out to GCS for our internal testing.
-        //
-        // In the real production app we won't reach out to GCS directly. We'll proxy Cloud CDN.
-        // We'll likely still need a gcp client to create a signed url. Unless the bucket
-        // is public in which case we'll have a configured url per PoP region. I'm not totally
-        // sure at the moment how private bucket cdn-url signing works. Area of active
-        // exploration. But the docs say it works!
-        //
-        // Errors are ignored! Maybe we should log them?
-        if let Ok(data) = client
-            .download_object(
-                &GetObjectRequest {
-                    bucket: "bucket".to_string(),
-                    object: fp,
-                    ..Default::default()
-                },
-                &Range::default(),
-            )
-            .await
-        {
-            return (StatusCode::OK, axum::Json(data));
+    if let Some(key) = maybe_key {
+        if let Some(response) = fetch_from_public_bucket(key).await {
+            return (StatusCode::OK, axum::Json(response));
         }
     }
     (StatusCode::NOT_FOUND, axum::Json(Vec::new()))
+}
+
+// Half-implementation of what needs to happen to support private bucket access. In reality we'll
+// want to create a signed-url and make an HTTP request to a CDN URL. However, this form of CDN
+// access has not been configured for Sentry yet so we fallback to plain bucket access. This
+// doesn't have a latency impact but does have a COGS impact. This state of affair should be
+// sufficient for a proof of concept or internal testing but should not remain for the private
+// alpha.
+async fn fetch_from_private_bucket(key: String) -> Option<Vec<u8>> {
+    // TODO: Log if credentials do not exist?
+    // TODO: Should this be in some global and passed into this function?
+    let config = ClientConfig::default().with_auth().await.ok()?;
+    let client = Client::new(config);
+
+    // TODO: Log if an error is encountered.
+    // TODO: Stream response to client rather than waiting for the download to complete.
+    client
+        .download_object(
+            &GetObjectRequest {
+                bucket: "remote-config-cdn-private".to_string(),
+                object: key,
+                ..Default::default()
+            },
+            &Range::default(),
+        )
+        .await
+        .ok()
+}
+
+// Half-implementation of what needs to happen to support public bucket access. In reality we'll
+// make an HTTP request to a public CDN URL. The same implications for private bucket access apply
+// here. No latency impact. Slight COGS impact. Unwise to ship to customers.
+async fn fetch_from_public_bucket(key: String) -> Option<Vec<u8>> {
+    // TODO: This isn't a CDN url. The CDN url is an ip-address that would need to be
+    // configured per PoP region.
+    let url = format!(
+        "https://storage.googleapis.com/remote-config-cdn-testing/{}",
+        key
+    );
+
+    // TODO: Log if an error is encountered.
+    // TODO: Stream response to client rather than waiting for the download to complete.
+    match reqwest::get(url).await {
+        Ok(response) => match response.bytes().await {
+            Ok(bytes) => Some(bytes.to_vec()),
+            Err(_) => None,
+        },
+        Err(_) => None,
+    }
 }
