0d174a24565068f591d48c25d6a2f94ae54abb00
edwardgou-sentry <edwardgou-sentry@users.noreply.github.com>
2024-05-28 18:58:46 +0000

    deploy: 8b7612f44d91ce0770b11006fcb526812d6c3f25

--- relay_event_normalization/span/tag_extraction/sidebar-items.js
+++ relay_event_normalization/span/tag_extraction/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["SpanTagKey"],"fn":["extract_measurements","extract_span_tags","extract_tags"],"static":["HTTP_METHOD_EXTRACTOR_REGEX"]};
+window.SIDEBAR_ITEMS = {"enum":["SpanTagKey"],"fn":["extract_measurements","extract_segment_span_tags","extract_span_tags","extract_tags"],"static":["HTTP_METHOD_EXTRACTOR_REGEX"]};
