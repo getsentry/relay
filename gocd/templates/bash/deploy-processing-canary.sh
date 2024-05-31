2e0169d750c16cfb29c0b0a66c0474287e34ad0c
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-05-31 02:32:30 -0400

    ref: use artifact registry for relay deploys (#3621)

--- gocd/templates/bash/deploy-processing-canary.sh
+++ gocd/templates/bash/deploy-processing-canary.sh
@@ -6,5 +6,5 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
 
 /devinfra/scripts/k8s/k8s-deploy.py \
     --label-selector="service=relay,env=canary" \
-    --image="us.gcr.io/sentryio/relay:${GO_REVISION_RELAY_REPO}" \
+    --image="us-central1-docker.pkg.dev/sentryio/relay/relay:${GO_REVISION_RELAY_REPO}" \
     --container-name="relay"
