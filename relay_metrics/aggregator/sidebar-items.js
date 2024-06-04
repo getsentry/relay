9c86828a8ee372cca27b04df35d92d7bce261f3b
iambriccardo <iambriccardo@users.noreply.github.com>
2024-06-04 13:44:57 +0000

    deploy: 4cf796912d4b53cfd7f682586813c795b3c0f323

--- relay_metrics/aggregator/sidebar-items.js
+++ relay_metrics/aggregator/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ShiftKey"],"fn":["tags_cost"],"struct":["AggregateMetricsError","Aggregator","AggregatorConfig"]};
+window.SIDEBAR_ITEMS = {"enum":["FlushBatching"],"fn":["tags_cost"],"struct":["AggregateMetricsError","Aggregator","AggregatorConfig"]};
