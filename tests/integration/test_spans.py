bd47459abb76f265941fe2ca726b85a70ee27e7e
Joris Bayer <joris.bayer@sentry.io>
2024-06-10 16:09:15 +0200

    Update tests/integration/test_spans.py
    Co-authored-by: David Herberth <david.herberth@sentry.io>

--- tests/integration/test_spans.py
+++ tests/integration/test_spans.py
@@ -1596,7 +1596,7 @@ def test_rate_limit_consistent_extracted(
     assert len(spans) == 2
     assert summarize_outcomes() == {(16, 0): 2}  # SpanIndexed, Accepted
     # A limit only for span_indexed does not affect extracted metrics
-    metrics = metrics_consumer.get_metrics(timeout=1, n=10)
+    metrics = metrics_consumer.get_metrics(n=10)
     span_count = sum(
         [m[0]["value"] for m in metrics if m[0]["name"] == "c:spans/usage@none"]
     )
