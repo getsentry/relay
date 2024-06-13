2d4c854f8308e7044e623888d4932afde537c7d3
David Herberth <david.herberth@sentry.io>
2024-06-13 17:29:03 +0200

    pii transmute

--- relay-pii/src/minidumps.rs
+++ relay-pii/src/minidumps.rs
@@ -352,7 +352,8 @@ mod tests {
             // both our dumps different types which is awkward to work with.  So we store
             // the Vec separately to keep the slice alive and pretend we give Minidump a
             // &'static [u8].
-            let slice = unsafe { std::mem::transmute(scrubbed_data.as_slice()) };
+            let slice =
+                unsafe { std::mem::transmute::<&[u8], &'static [u8]>(scrubbed_data.as_slice()) };
             let scrubbed_dump = Minidump::read(slice).expect("scrubbed minidump failed to parse");
             Self {
                 orig_dump,
