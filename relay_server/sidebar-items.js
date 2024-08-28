909a5eb474650aacb26034a5a8279335a82976b5
iambriccardo <iambriccardo@users.noreply.github.com>
2024-08-28 07:17:10 +0000

    deploy: 6198771311a5ad91458e3af93666eb4756064504

--- relay_server/sidebar-items.js
+++ relay_server/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["PolymorphicEnvelopeBuffer"],"fn":["run"],"mod":["spool_utils"],"struct":["SqliteEnvelopeStack","SqliteEnvelopeStore"],"trait":["EnvelopeStack"]};
+window.SIDEBAR_ITEMS = {"enum":["PolymorphicEnvelopeBuffer"],"fn":["run"],"mod":["spool_utils"],"struct":["MemoryChecker","MemoryStat","SqliteEnvelopeStack","SqliteEnvelopeStore"],"trait":["EnvelopeStack"]};
