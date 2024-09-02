12e098b167f316ec1eb5a77dfd3af557f354c295
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-09-02 09:55:13 +0200

    Fix

--- relay-server/src/services/buffer/mod.rs
+++ relay-server/src/services/buffer/mod.rs
@@ -217,7 +217,7 @@ impl Service for EnvelopeBufferService {
                 Ok(buffer) => buffer,
                 Err(error) => {
                     relay_log::error!(
-                        error = &error as &dyn Error,
+                        error = &error as &dyn std::error::Error,
                         "failed to start the envelope buffer service",
                     );
                     std::process::exit(1);
@@ -237,7 +237,7 @@ impl Service for EnvelopeBufferService {
                     () = tokio::time::sleep(self.sleep) => {
                         if let Err(e) = self.try_pop(&mut buffer).await {
                             relay_log::error!(
-                                error = &e as &dyn Error,
+                                error = &e as &dyn std::error::Error,
                                 "failed to pop envelope"
                             );
                         }
