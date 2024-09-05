b76a9b81eccd51d7aa35b76ba34a901b76aa8fea
jan-auer <jan-auer@users.noreply.github.com>
2024-09-05 12:50:15 +0000

    deploy: 6874ca57733460058eb558a8d7797be7e13a6153

--- relay_statsd/prelude/sidebar-items.js
+++ relay_statsd/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"trait":["Compat","Counted","CountedExt","Distributed","Gauged","Histogrammed","Metered","MetricClient","Setted","Timed"]};
+window.SIDEBAR_ITEMS = {"trait":["Counted","CountedExt","Distributed","Gauged","Histogrammed","Metered","MetricClient","Setted","Timed"]};
