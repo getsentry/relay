af856c34b86d77dd4043117f87d4bb54b75d67a2
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-09-27 12:33:02 +0200

    Fix

--- tests/integration/test_redis.py
+++ tests/integration/test_redis.py
@@ -91,15 +91,15 @@ def test_multi_write_redis_client_with_rate_limiting(
     ]
 
     redis_configs = [
-        # First, we double-write/-read from both Redis instances.
+        # First, we double-write/-read to/from both Redis instances.
         {
             "configs": [
                 {"server": "redis://127.0.0.1:6379"},
                 {"server": "redis://127.0.0.1:6380"},
             ]
         },
-        # # Afterward, we point to the secondary instance to make sure we are
-        # # correctly reading the quotas written before.
+        # Afterward, we point to the secondary instance to make sure we are
+        # correctly reading the quotas written before.
         {"server": "redis://127.0.0.1:6380"},
     ]
 
