f4196c163ce756ad206db7eebc6ba9006d9a52ee
Pierre Massat <pierre.massat@sentry.io>
2024-10-08 08:56:48 -0400

    Fix bare URLs

--- relay-profiling/src/types.rs
+++ relay-profiling/src/types.rs
@@ -2,13 +2,13 @@ use serde::{Deserialize, Serialize};
 
 use crate::native_debug_image::NativeDebugImage;
 
-/// This is a serde-friendly version of https://github.com/getsentry/relay/blob/52bc345871b4e5cca19ed73c17730eeef092028b/relay-event-schema/src/protocol/debugmeta.rs#L516
+/// This is a serde-friendly version of <https://github.com/getsentry/relay/blob/52bc345871b4e5cca19ed73c17730eeef092028b/relay-event-schema/src/protocol/debugmeta.rs#L516>
 #[derive(Default, Debug, Serialize, Deserialize, Clone)]
 pub struct DebugMeta {
     images: Vec<NativeDebugImage>,
 }
 
-/// This is a serde-friendly version of https://github.com/getsentry/relay/blob/52bc345871b4e5cca19ed73c17730eeef092028b/relay-event-schema/src/protocol/clientsdk.rs#L8
+/// This is a serde-friendly version of <https://github.com/getsentry/relay/blob/52bc345871b4e5cca19ed73c17730eeef092028b/relay-event-schema/src/protocol/clientsdk.rs#L8>
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ClientSdk {
     pub name: String,
