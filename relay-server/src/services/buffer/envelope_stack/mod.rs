f66a7c97582939a538f7db0648ca4dac99e8bee7
Joris Bayer <joris.bayer@sentry.io>
2024-10-13 18:22:43 +0200

    wip

--- relay-server/src/services/buffer/envelope_stack/mod.rs
+++ relay-server/src/services/buffer/envelope_stack/mod.rs
@@ -2,14 +2,15 @@ use std::future::Future;
 
 use crate::envelope::Envelope;
 
+pub mod file;
 pub mod memory;
 pub mod sqlite;
 
 /// A stack-like data structure that holds [`Envelope`]s.
-pub trait EnvelopeStack: Send + std::fmt::Debug {
+pub trait EnvelopeStack {
     /// The error type that is returned when an error is encountered during reading or writing the
     /// [`EnvelopeStack`].
-    type Error: std::fmt::Debug;
+    type Error;
 
     /// Pushes an [`Envelope`] on top of the stack.
     fn push(&mut self, envelope: Box<Envelope>) -> impl Future<Output = Result<(), Self::Error>>;
