c0e135aff3f3d41f7ac71884b7d72ff3502226c4
Colton Allen <cmanallen90@gmail.com>
2024-10-23 10:59:12 -0500

    fix(replays): Add validation for timestamps sent from the future (#4163)
    If a timestamp is from the future discard with a new reason.

--- relay-server/src/services/processor/replay.rs
+++ relay-server/src/services/processor/replay.rs
@@ -190,6 +190,9 @@ fn handle_replay_event_item(
                 ReplayError::InvalidPayload(_) => {
                     ProcessingError::InvalidReplay(DiscardReason::InvalidReplayEvent)
                 }
+                ReplayError::DateInTheFuture => {
+                    ProcessingError::InvalidReplay(DiscardReason::DateInTheFuture)
+                }
             })
         }
     }
