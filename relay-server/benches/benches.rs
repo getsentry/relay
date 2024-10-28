6851d0ed493056de4891b67d2652b1c54280ba1d
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-10-28 16:05:14 +0100

    Implement

--- relay-server/benches/benches.rs
+++ relay-server/benches/benches.rs
@@ -1,4 +1,5 @@
 use bytes::Bytes;
+use chrono::Utc;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use relay_config::Config;
 use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions};
@@ -66,7 +67,7 @@ fn mock_envelope_with_project_key(project_key: &ProjectKey, size: &str) -> Box<E
     ));
 
     let mut envelope = Envelope::parse_bytes(bytes).unwrap();
-    envelope.set_received_at(Instant::now());
+    envelope.set_received_at(Utc::now());
     envelope
 }
 
