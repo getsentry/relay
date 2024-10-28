54097ea7d45737f0f818c72bf426a8226263aa51
Tony Xiao <txiao@sentry.io>
2024-10-28 14:44:26 -0400

    lint

--- relay-profiling/src/lib.rs
+++ relay-profiling/src/lib.rs
@@ -103,7 +103,7 @@ impl Filterable for MinimalProfile {
     }
 
     fn release(&self) -> Option<&str> {
-        self.release.as_ref().map(|release| release.as_str())
+        self.release.as_deref()
     }
 
     fn transaction(&self) -> Option<&str> {
@@ -122,7 +122,7 @@ impl Filterable for MinimalProfile {
 impl Getter for MinimalProfile {
     fn get_value(&self, path: &str) -> Option<Val<'_>> {
         match path.strip_prefix("event.")? {
-            "release" => self.release.as_ref().map(|release| release.as_str().into()),
+            "release" => self.release.as_deref().map(|release| release.into()),
             "platform" => Some(self.platform.as_str().into()),
             _ => None,
         }
