929428ee49b756ef887869cf1342ac09dd685c1e
Dav1dde <Dav1dde@users.noreply.github.com>
2024-11-28 15:44:08 +0000

    deploy: 912acc511b635d4660dac32eaa706ab8214a7d42

--- trait.impl/core/ops/arith/trait.Mul.js
+++ trait.impl/core/ops/arith/trait.Mul.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["relay_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a> for <a class=\"struct\" href=\"relay_metrics/struct.FiniteF64.html\" title=\"struct relay_metrics::FiniteF64\">FiniteF64</a>"]]]]);
+    var implementors = Object.fromEntries([["relay_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a> for <a class=\"struct\" href=\"relay_metrics/struct.FiniteF64.html\" title=\"struct relay_metrics::FiniteF64\">FiniteF64</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
