db83822227f89ccc6d85faea8b5067e71783de04
Dav1dde <Dav1dde@users.noreply.github.com>
2024-12-18 19:46:23 +0000

    deploy: 163635848e5d2dda451f5c5f9d483da9da48da4d

---
+++ trait.impl/core/ops/arith/trait.SubAssign.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["relay_metrics",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a> for <a class=\"struct\" href=\"relay_metrics/struct.ByNamespace.html\" title=\"struct relay_metrics::ByNamespace\">ByNamespace</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[488]}
