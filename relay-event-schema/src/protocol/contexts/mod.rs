28a3e017c036b9a3b288b481135a58bc0fbba002
Colton Allen <cmanallen90@gmail.com>
2025-01-21 13:17:21 -0600

    feat(flags): Add flags context (#4458)
    Adds a new feature context. The goal is to normalize contexts with the
    key "flags" and the body:
    
    ```json
    {
        "flags": {
            "values": [
                {"flag": "abc", "result": true}
            ]
        }
    }
    ```
    
    Where result is any valid JSON value.

--- relay-event-schema/src/protocol/contexts/mod.rs
+++ relay-event-schema/src/protocol/contexts/mod.rs
@@ -2,6 +2,7 @@ mod app;
 mod browser;
 mod cloud_resource;
 mod device;
+mod flags;
 mod gpu;
 mod monitor;
 mod nel;
@@ -69,6 +70,8 @@ pub enum Context {
     Profile(Box<ProfileContext>),
     /// Information related to Replay.
     Replay(Box<ReplayContext>),
+    /// Information related to Feature flags.
+    Flags(Box<flags::FlagsContext>),
     /// Information related to User Report V2. TODO:(jferg): rename to UserFeedbackContext
     #[metastructure(tag = "feedback")]
     UserReportV2(Box<UserReportV2Context>),
