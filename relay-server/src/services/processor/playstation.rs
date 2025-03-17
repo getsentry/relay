eec54f867724987556695af2f6a2982312bea568
tobias-wilfert <tobias.wilfert@sentry.io>
2025-03-17 15:38:11 +0100

    Add placeholder metric

--- relay-server/src/services/processor/playstation.rs
+++ relay-server/src/services/processor/playstation.rs
@@ -2,15 +2,18 @@
 //!
 //! These functions are included only in the processing mode.
 
-use crate::envelope::{AttachmentType, ItemType};
-use crate::services::processor::should_filter;
-use crate::services::processor::{ErrorGroup, EventFullyNormalized, ProcessingError};
-use crate::services::projects::project::ProjectInfo;
-use crate::utils::TypedEnvelope;
 use relay_config::Config;
 use relay_dynamic_config::Feature;
 use relay_event_schema::protocol::Event;
 use relay_protocol::Annotated;
+
+use crate::envelope::{AttachmentType, ItemType};
+use crate::services::processor::metric;
+use crate::services::processor::should_filter;
+use crate::services::processor::{ErrorGroup, EventFullyNormalized, ProcessingError};
+use crate::services::projects::project::ProjectInfo;
+use crate::statsd::RelayCounters;
+use crate::utils::TypedEnvelope;
 
 pub fn expand(
     managed_envelope: &mut TypedEnvelope<ErrorGroup>,
@@ -29,19 +32,21 @@ pub fn expand(
 
     let envelope = &mut managed_envelope.envelope_mut();
 
-    if let Some(item) = envelope.take_item_by(|item| item.ty() == &ItemType::Attachment) {
-        if let Some(&AttachmentType::Prosperodump) = item.attachment_type() {
-            // TODO: Do some work here
-        }
+    if let Some(_item) = envelope.take_item_by(|item| {
+        item.ty() == &ItemType::Attachment
+            && item.attachment_type() == Some(&AttachmentType::Prosperodump)
+    }) {
+        // TODO: Add the expand logic here
+        metric!(counter(RelayCounters::PlaystationProcessing) += 1);
     }
 
     Ok(())
 }
 
-// FIXME: Decide on weather we also want to keep the double function here to do the extraction work on the custom tags.
 pub fn process(
     _managed_envelope: &mut TypedEnvelope<ErrorGroup>,
     _event: &mut Annotated<Event>,
 ) -> Result<Option<EventFullyNormalized>, ProcessingError> {
+    // TODO: Add the processing logic here.
     Ok(None)
 }
--- relay-server/src/statsd.rs
+++ relay-server/src/statsd.rs
@@ -842,6 +842,9 @@ pub enum RelayCounters {
     /// - `namespace`: the metric namespace.
     #[cfg(feature = "processing")]
     MetricDelayCount,
+    /// The amount of times PlayStation processing was attempted.
+    #[cfg(feature = "processing")]
+    PlaystationProcessing,
 }
 
 impl CounterMetric for RelayCounters {
@@ -888,6 +891,8 @@ impl CounterMetric for RelayCounters {
             RelayCounters::MetricDelaySum => "metrics.delay.sum",
             #[cfg(feature = "processing")]
             RelayCounters::MetricDelayCount => "metrics.delay.count",
+            #[cfg(feature = "processing")]
+            RelayCounters::PlaystationProcessing => "processing.playstation",
         }
     }
 }
