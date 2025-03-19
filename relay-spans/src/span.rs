87f7599432538ff7bb621bcc7cb982b20fb05611
Matt Quinn <matt.quinn@sentry.io>
2025-03-19 10:47:24 -0400

    remove unnecessary clones

--- relay-spans/src/span.rs
+++ relay-spans/src/span.rs
@@ -230,8 +230,8 @@ fn otel_to_sentry_link(otel_link: OtelLink) -> SpanLink {
     .into();
 
     SpanLink {
-        trace_id: TraceId(hex::encode(otel_link.trace_id.clone())).into(),
-        span_id: SpanId(hex::encode(otel_link.span_id.clone())).into(),
+        trace_id: TraceId(hex::encode(otel_link.trace_id)).into(),
+        span_id: SpanId(hex::encode(otel_link.span_id)).into(),
         sampled: (otel_link.flags & W3C_TRACE_CONTEXT_SAMPLED != 0).into(),
         attributes,
         // The parent span ID is not available over OTLP.
