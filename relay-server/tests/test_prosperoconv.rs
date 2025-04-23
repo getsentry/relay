2c5c574ecd843073f95c42ba827beffd902e0637
tobias-wilfert <tobias.wilfert@sentry.io>
2025-04-23 16:00:13 +0200

    Add more elaborate test & update CI to run it

--- relay-server/tests/test_prosperoconv.rs
+++ relay-server/tests/test_prosperoconv.rs
@@ -1,13 +1,16 @@
 // TODO: Although I hate it I think this is the best way to ensure that these tests are not run with --all-features
 #[cfg(all(feature = "prosperoconv", not(feature = "not-prosperoconv")))]
 mod tests {
-    // TODO: This is a place holder test, replace with a better one and update the CI to run them.
-    use relay_prosperoconv::verify;
+    use prosperoconv::process_dump;
+    use sentry::{protocol::Event, Envelope};
+    use std::fs;
 
     #[test]
-    fn test_prosperoconv_verify() {
-        // Call the verify function and check its return value
-        let result = verify();
-        assert_eq!(result, "library successfully included");
+    fn test_prosperoconv() {
+        let file =
+            fs::File::open("../tests/integration/fixtures/native/playstation.prosperodmp").unwrap();
+
+        // TODO: Make this test more elaborate
+        process_dump(&mut Envelope::new(), Event::new(), file).unwrap();
     }
 }
