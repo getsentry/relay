a46f11a098354e94b2ab41b8e43bcfbe1bfb44cf
loewenheim <loewenheim@users.noreply.github.com>
2025-05-23 10:41:07 +0000

    deploy: c2a1c51eec7d3e31d05ec611c2d8039e8afa52ce

--- trait.impl/relay_cardinality/limiter/trait.CardinalityItem.js
+++ trait.impl/relay_cardinality/limiter/trait.CardinalityItem.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["relay_metrics",[["impl <a class=\"trait\" href=\"relay_cardinality/limiter/trait.CardinalityItem.html\" title=\"trait relay_cardinality::limiter::CardinalityItem\">CardinalityItem</a> for <a class=\"struct\" href=\"relay_metrics/struct.Bucket.html\" title=\"struct relay_metrics::Bucket\">Bucket</a>"]]]]);
+    var implementors = Object.fromEntries([["relay_metrics",[["impl CardinalityItem for <a class=\"struct\" href=\"relay_metrics/struct.Bucket.html\" title=\"struct relay_metrics::Bucket\">Bucket</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[305]}
+//{"start":57,"fragment_lengths":[160]}
