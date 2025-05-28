89a0b09fa4e95e5dec5cecd60a32bcec8c3df5c1
David Herberth <david.herberth@sentry.io>
2025-05-28 16:26:24 +0200

    humble beginnings

--- relay-server/src/processing/logs/mod.rs
+++ relay-server/src/processing/logs/mod.rs
@@ -1,18 +1,27 @@
 use smallvec::SmallVec;
 
 use crate::envelope::{Item, ItemType, Items};
-use crate::processing;
+use crate::processing::{self, Context, Counted, Managed, Output, Quantities};
 use crate::services::processor::ProcessingError;
 use crate::utils::ManagedEnvelope;
 
 pub struct LogsProcessor {}
 
+impl LogsProcessor {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
 impl processing::Processor for LogsProcessor {
     type UnitOfWork = EinsLog;
-    type Result = ();
+    type Output = ();
     type Error = ProcessingError;
 
-    fn prepare(&self, envelope: &mut ManagedEnvelope) -> UnitOfWork {
+    fn prepare_envelope(
+        &self,
+        envelope: &mut ManagedEnvelope,
+    ) -> Option<Managed<Self::UnitOfWork>> {
         let otel_logs = envelope
             .envelope_mut()
             .take_items_by(|item| matches!(*item.ty(), ItemType::OtelLog));
@@ -27,11 +36,36 @@ impl processing::Processor for LogsProcessor {
         //     ))
         // }
 
-        EinsLog { otel_logs, logs }
+        let work = EinsLog { otel_logs, logs };
+        Some(Managed::from_envelope(envelope, work))
     }
 
-    fn process(&self, work: UnitOfWork) -> Result<(), ProcessingError> {
-        todo!()
+    fn process(
+        &self,
+        work: Managed<Self::UnitOfWork>,
+        ctx: Context<'_>,
+    ) -> Result<Output<()>, ProcessingError> {
+        // ourlog::filter(
+        //     managed_envelope,
+        //     &self.inner.config,
+        //     &project_info,
+        //     &self.inner.global_config.current(),
+        // );
+        //
+        // self.enforce_quotas(
+        //     managed_envelope,
+        //     Annotated::empty(),
+        //     &mut extracted_metrics,
+        //     &project_info,
+        //     &rate_limits,
+        // )
+        // .await?;
+        //
+        // if_processing!(self.inner.config, {
+        //     ourlog::process(managed_envelope, &project_info)?;
+        // });
+
+        Ok(Output::just(()))
     }
 }
 
@@ -44,4 +78,10 @@ struct EinsLog {
     /// But at this point this has not yet been validated.
     // TODO: validation, that there is only a single log item (for now).
     logs: Items,
+}
+
+impl Counted for EinsLog {
+    fn quantities(&self) -> Quantities {
+        todo!()
+    }
 }
