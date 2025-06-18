d0e94f319169ef6d4fca4fcdc6d5d49df5e73aa0
Sebastian Zivota <loewenheim@mailbox.org>
2025-06-18 14:18:38 +0200

    fix: Remove division that might divide by zero

--- relay-server/src/utils/sizes.rs
+++ relay-server/src/utils/sizes.rs
@@ -90,8 +90,7 @@ pub fn check_envelope_size_limits(
         // For item containers, we want to check that the contained items obey
         // the size limits *on average*.
         // For standalone items, this is just the item size itself.
-        let avg_item_size = item.len() / (item.item_count().unwrap_or(1) as usize);
-        if avg_item_size > max_size {
+        if item.len() > max_size * (item.item_count().unwrap_or(1) as usize) {
             return Err(item
                 .attachment_type()
                 .map(|t| t.into())
