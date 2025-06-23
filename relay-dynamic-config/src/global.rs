8eae031a610b1f6e37462f42ea03838da664417c
Vjeran Grozdanić <vjeran.grozdanic@gmail.com>
2025-06-23 17:02:34 +0200

    feat(ai): ModelCosts version 2 (#4825)
    Introduces new field `models` to ModelCosts struct which will be used to
    calculate cost of LLM calls by type of tokens used. Current solution was
    not flexible enough, and it only supported 2 type of tokens: input and
    output tokens.
    
    Cost calculation has gotten more complex in the last year since this was
    first implemented, and we need more flexible structure which will also
    support future changes in cost calculation.
    
    This PR only introduces new struct, but the cost calculation will be
    changed in a separate PR to make this PR smaller and easier to review.
    
    Part of [TET-645: Automate cost/pricing
    updating](https://linear.app/getsentry/issue/TET-645/automate-costpricing-updating)

--- relay-dynamic-config/src/global.rs
+++ relay-dynamic-config/src/global.rs
@@ -47,7 +47,7 @@ pub struct GlobalConfig {
     pub metric_extraction: ErrorBoundary<MetricExtractionGroups>,
 
     /// Configuration for AI span measurements.
-    #[serde(skip_serializing_if = "is_missing")]
+    #[serde(skip_serializing_if = "is_model_costs_empty")]
     pub ai_model_costs: ErrorBoundary<ModelCosts>,
 
     /// Configuration to derive the `span.op` from other span fields.
@@ -378,11 +378,8 @@ fn is_ok_and_empty(value: &ErrorBoundary<MetricExtractionGroups>) -> bool {
     )
 }
 
-fn is_missing(value: &ErrorBoundary<ModelCosts>) -> bool {
-    matches!(
-        value,
-        &ErrorBoundary::Ok(ModelCosts{ version, ref costs }) if version == 0 && costs.is_empty()
-    )
+fn is_model_costs_empty(value: &ErrorBoundary<ModelCosts>) -> bool {
+    matches!(value, ErrorBoundary::Ok(model_costs) if model_costs.is_empty())
 }
 
 #[cfg(test)]
