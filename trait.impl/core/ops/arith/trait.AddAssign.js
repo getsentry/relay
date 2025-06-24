27fc6fa20131da2ef6a3506d366a4951762b039b
loewenheim <loewenheim@users.noreply.github.com>
2025-06-24 14:11:29 +0000

    deploy: 61dc452a1d4968d4d7dce928b1e86619941084b1

--- trait.impl/core/ops/arith/trait.AddAssign.js
+++ trait.impl/core/ops/arith/trait.AddAssign.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["relay_metrics",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a> for <a class=\"struct\" href=\"relay_metrics/struct.ByNamespace.html\" title=\"struct relay_metrics::ByNamespace\">ByNamespace</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["relay_metrics",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a> for <a class=\"struct\" href=\"relay_metrics/struct.ByNamespace.html\" title=\"struct relay_metrics::ByNamespace\">ByNamespace</a>&lt;T&gt;"]]],["relay_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a> for <a class=\"struct\" href=\"relay_protocol/struct.FiniteF64.html\" title=\"struct relay_protocol::FiniteF64\">FiniteF64</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[488]}
+//{"start":57,"fragment_lengths":[488,312]}
--- trait.impl/core/ops/arith/trait.Rem.js
+++ trait.impl/core/ops/arith/trait.Rem.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["relay_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.Rem.html\" title=\"trait core::ops::arith::Rem\">Rem</a> for <a class=\"struct\" href=\"relay_metrics/struct.FiniteF64.html\" title=\"struct relay_metrics::FiniteF64\">FiniteF64</a>"]]]]);
+    var implementors = Object.fromEntries([["relay_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.Rem.html\" title=\"trait core::ops::arith::Rem\">Rem</a> for <a class=\"struct\" href=\"relay_protocol/struct.FiniteF64.html\" title=\"struct relay_protocol::FiniteF64\">FiniteF64</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[290]}
+//{"start":57,"fragment_lengths":[293]}
