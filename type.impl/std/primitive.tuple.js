9375b60a70b0504c95ebd3965215211716972c43
lcian <lcian@users.noreply.github.com>
2025-06-24 09:10:49 +0000

    deploy: 19bbf1cead470c8aacdf9940cf2d6df4915eba99

--- type.impl/std/primitive.tuple.js
+++ type.impl/std/primitive.tuple.js
@@ -1,9 +1,9 @@
 (function() {
-    var type_impls = Object.fromEntries([["relay_protocol",[]]]);
+    var type_impls = Object.fromEntries([["relay_log",[]],["relay_protocol",[]]]);
     if (window.register_type_impls) {
         window.register_type_impls(type_impls);
     } else {
         window.pending_type_impls = type_impls;
     }
 })()
-//{"start":55,"fragment_lengths":[21]}
+//{"start":55,"fragment_lengths":[16,22]}
