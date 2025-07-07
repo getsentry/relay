dcbfa2d5daafd019ede766fb01b1e8847214b752
David Herberth <david.herberth@sentry.io>
2025-07-07 12:58:55 +0200

    test(logs): Fix flaky log outcome test (#4904)
    Closes: #4903
    
    #skip-changelog

--- tests/integration/asserts/__init__.py
+++ tests/integration/asserts/__init__.py
@@ -1,3 +1,31 @@
+from typing import Callable
 from .time import time_after, time_within, time_within_delta
 
-__all__ = ["time_after", "time_within", "time_within_delta"]
+__all__ = ["time_after", "time_within", "time_within_delta", "matches"]
+
+
+class _Matches(object):
+    def __init__(self, compare_with: Callable[[object], bool]):
+        self._compare_with = compare_with
+
+    def __eq__(self, value: object) -> bool:
+        return self._compare_with(value)
+
+    def __ne__(self, value: object) -> bool:
+        return not self.__eq__(value)
+
+    def __repr__(self):
+        return f"matches<{self._compare_with}>"
+
+
+def matches(f: Callable[[object], bool]):
+    """
+    Helper to dynamically assert values with a function.
+
+    Example::
+
+        assert foo == {
+            "value": matches(lambda x: 3 <= x <= 5)
+        }
+    """
+    return _Matches(f)
