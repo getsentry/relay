ba9382fc7c41ba649cb17a801fa70e512fa8f866
mchen-sentry <icswon1108@gmail.com>
2025-07-08 14:49:58 -0700

    use console scripts

--- gocd/templates/bash/deploy-pop.sh
+++ gocd/templates/bash/deploy-pop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}")
+eval $(project_env_vars --region="${SENTRY_REGION}")
 
 /devinfra/scripts/k8s/k8stunnel
 
-/devinfra/scripts/k8s/k8s-deploy.py \
+k8s-deploy \
     --label-selector="service=relay-pop" \
     --image="us-central1-docker.pkg.dev/internal-sentry/relay/relay-pop:${GO_REVISION_RELAY_REPO}" \
     --container-name="relay"
--- gocd/templates/bash/deploy-processing-canary.sh
+++ gocd/templates/bash/deploy-processing-canary.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}")
+eval $(project_env_vars --region="${SENTRY_REGION}")
 
 /devinfra/scripts/k8s/k8stunnel
 
-/devinfra/scripts/k8s/k8s-deploy.py \
+k8s-deploy.py \
     --label-selector="service=relay,env=canary" \
     --image="us-central1-docker.pkg.dev/internal-sentry/relay/relay:${GO_REVISION_RELAY_REPO}" \
     --container-name="relay"
