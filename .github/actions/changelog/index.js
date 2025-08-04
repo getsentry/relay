16679fe6ac96caebb4eb75aa185cf983a77a345c
David Herberth <david.herberth@sentry.io>
2025-08-04 10:38:07 +0200

    ci(pr): Validate PR title to follow conventions (#5017)
    Closes: #4907
    
    #skip-changelog

--- .github/actions/changelog/index.js
+++ .github/actions/changelog/index.js
@@ -80,6 +80,29 @@ module.exports = async ({github, context, core}) => {
     core.info("CHANGELOG entry is added, we're good to go.");
   }
 
+  async function checkPrTitle(pr) {
+    // Provide an opt out just in case, but this should never be used.
+    const hasIgnoreLabel = (pr.labels || []).some(label => label.name === 'ignore-title');
+    if (hasIgnoreLabel) {
+      return;
+    }
+
+    // From: <https://develop.sentry.dev/engineering-practices/commit-messages/>.
+    const TITLE_RE = /^(ci|build|docs|feat|fix|perf|ref|style|test|meta|license)(\([^)]+\))?: [A-Z].*\w$/;
+    const REVERT_RE = /^Revert ".*"$/;
+
+    if (pr.title.match(TITLE_RE) === null && pr.title.match(REVERT_RE) === null) {
+      core.setFailed('PR title does not match Sentry conventions.');
+      core.info('Please follow the Sentry commit message conventions: https://develop.sentry.dev/engineering-practices/commit-messages/');
+      core.info('')
+      core.info('Format: <type>(<scope>): <subject>');
+      core.info('Subject line must be capitalized and must not end with a period.')
+      return;
+    }
+
+    core.info("PR title matches Sentry conventions!");
+  }
+
   async function checkAll() {
     const {data: pr} = await github.rest.pulls.get({
       owner: context.repo.owner,
@@ -92,6 +115,7 @@ module.exports = async ({github, context, core}) => {
       return;
     }
 
+    await checkPrTitle(pr);
     await checkChangelog(pr);
   }
 
