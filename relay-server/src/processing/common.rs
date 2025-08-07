fb0d61ba7bc5cbfab280521491cb2a36a2f4cc8b
David Herberth <david.herberth@sentry.io>
2025-08-07 11:01:18 +0200

    ref(processing): Reduce code duplication for new processing in processor

---
+++ relay-server/src/processing/common.rs
@@ -0,0 +1,52 @@
+use crate::Envelope;
+use crate::managed::{Managed, Rejected};
+use crate::processing::logs::LogsProcessor;
+use crate::processing::spans::SpansProcessor;
+use crate::processing::{Forward, Processor};
+
+macro_rules! outputs {
+    ($($variant:ident => $ty:ty,)*) => {
+        /// All known [`Processor`](Processor) outputs.
+        #[derive(Debug)]
+        pub enum Outputs {
+            $(
+                $variant(<$ty as Processor>::Output)
+            ),*
+        }
+
+        impl Forward for Outputs {
+            fn serialize_envelope(self) -> Result<Managed<Box<Envelope>>, Rejected<()>> {
+                match self {
+                    $(
+                        Self::$variant(output) => output.serialize_envelope()
+                    ),*
+                }
+            }
+
+            #[cfg(feature = "processing")]
+            fn forward_store(
+                self,
+                s: &relay_system::Addr<crate::services::store::Store>,
+            ) -> Result<(), Rejected<()>> {
+                match self {
+                    $(
+                        Self::$variant(output) => output.forward_store(s)
+                    ),*
+                }
+            }
+        }
+
+        $(
+            impl From<<$ty as Processor>::Output> for Outputs {
+                fn from(value: <$ty as Processor>::Output) -> Self {
+                    Self::$variant(value)
+                }
+            }
+        )*
+    };
+}
+
+outputs!(
+    Logs => LogsProcessor,
+    Spans => SpansProcessor,
+);
