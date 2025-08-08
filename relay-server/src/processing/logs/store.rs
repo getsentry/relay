9d9e242677ba3b8c77aed7c414ec1fee22d627a1
Pierre Massat <pierre.massat@sentry.io>
2025-08-08 10:08:37 -0700

    Lint and format

--- relay-server/src/processing/logs/store.rs
+++ relay-server/src/processing/logs/store.rs
@@ -58,14 +58,14 @@ pub fn convert(log: WithHeader<OurLog>, ctx: &Context) -> Result<StoreTraceItem>
         body: required!(log.body),
         span_id: log.span_id.into_value(),
     };
-    let retention_days: u16 = ctx.retention.unwrap_or(DEFAULT_EVENT_RETENTION).into();
+    let retention_days = ctx.retention.unwrap_or(DEFAULT_EVENT_RETENTION);
 
     let trace_item = TraceItem {
         item_type: TraceItemType::Log.into(),
         organization_id: ctx.scoping.organization_id.value(),
         project_id: ctx.scoping.project_id.value(),
         received: Some(ts(ctx.received_at)),
-        retention_days,
+        retention_days: retention_days.into(),
         downsampled_retention_days: ctx.downsampled_retention.unwrap_or(retention_days).into(),
         timestamp: Some(ts(timestamp.0)),
         trace_id: required!(log.trace_id).to_string(),
