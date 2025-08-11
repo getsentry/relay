c4c4ff84761453cb66e9acbd255b2941c1d67a5f
David Herberth <david.herberth@sentry.io>
2025-08-11 14:09:36 +0200

    review comment

--- relay-server/src/processing/spans/filter.rs
+++ relay-server/src/processing/spans/filter.rs
@@ -3,7 +3,7 @@ use relay_dynamic_config::Feature;
 use crate::processing::Context;
 use crate::processing::spans::{Error, Result};
 
-/// Filters logs sent for a project which does not allow logs ingestion.
+/// Filters standalone spans sent for a project which does not allow standalone span ingestion.
 pub fn feature_flag(ctx: Context<'_>) -> Result<()> {
     match ctx.should_filter(Feature::StandaloneSpanIngestion) {
         true => Err(Error::FilterFeatureFlag),
