c4dbacab87cbf23700d8edd3c1e50df3965bfaae
Kev <6111995+k-fish@users.noreply.github.com>
2025-08-25 12:39:31 -0400

    fix(dep): Comment breaking release sh (#5074)
    ### Summary
    Put the comment in the wrong spot, it was getting included in the docker
    entrypoint

--- scripts/docker-manylinux.sh
+++ scripts/docker-manylinux.sh
@@ -23,13 +23,14 @@ docker run \
   bash -c 'cargo build -p relay-cabi --profile release-cabi'
 
 # create a wheel for the correct architecture
+# Pinned to 2025.08.15-1 since manylinux 2025.08.22 onward removes setuptools
 docker run \
   --rm \
   -w /work/py \
   -v "$(pwd):/work" \
   -e SKIP_RELAY_LIB_BUILD=1 \
   -e CARGO_BUILD_TARGET \
-  quay.io/pypa/manylinux_2_28_${TARGET}:2025.08.15-1 \ # Pinned to 2025.08.15-1 since manylinux 2025.08.22 onward removes setuptools
+  quay.io/pypa/manylinux_2_28_${TARGET}:2025.08.15-1 \
   sh manylinux.sh
 
 # Fix permissions for shared directories
