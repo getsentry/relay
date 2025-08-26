4af262c0e7bf945d9d28a1a3325f7b95dc8f7b36
Sebastian Zivota <loewenheim@users.noreply.github.com>
2025-08-26 13:40:44 +0200

    feat: Add relay-conventions crate (#5049)
    This adds a new workspace crate to parse the attribute definitions in
    the `sentry-conventions` repository at compile time and provide them to
    the rest of Relay. For this purpose it contains `sentry-conventions` as
    a submodule.
    
    Attribute definitions are parsed, converted to a more usable format, and
    stored in a static map. The intention is to provide functions to query
    different information about an attribute, and `write_behavior` is the
    first such function.
    
    All of the naming, internal structure, which information about
    attributes should be included &c., is still massively subject to change
    and I welcome any and all bikeshedding.
    
    Note: this PR is currently blocked on
    https://github.com/getsentry/sentry-conventions/issues/100.
    
    ref: #5048. ref: RELAY-149.
    
    ---------
    
    Co-authored-by: David Herberth <david.herberth@sentry.io>

---
+++ relay-conventions/src/lib.rs
@@ -0,0 +1,62 @@
+//! Attribute definitions extracted from [`sentry-conventions`](https://github.com/getsentry/sentry-conventions).
+//!
+//! This crate contains the `sentry-conventions` repository as a git submodule. Attribute definitions in the submodule
+//! are parsed at compile time and can be accessed via the `attribute_info` function.
+
+use relay_event_schema::processor::Pii;
+
+include!(concat!(env!("OUT_DIR"), "/attribute_map.rs"));
+
+/// Under which names an attribute should be saved.
+#[derive(Debug, Clone, Copy)]
+pub enum WriteBehavior {
+    /// Save the attribute under its current name.
+    ///
+    /// This is the only choice for attributes that aren't deprecated.
+    CurrentName,
+    /// Save the attribute under its replacement name instead.
+    NewName(&'static str),
+    /// Save the attribute under both its current name and
+    /// its replacement name.
+    BothNames(&'static str),
+}
+
+/// Information about an attribute, as defined in `sentry-conventions`.
+#[derive(Debug, Clone)]
+pub struct AttributeInfo {
+    /// How this attribute should be saved.
+    pub write_behavior: WriteBehavior,
+    /// Whether this attribute can contain PII.
+    pub pii: Pii,
+    /// Other attribute names that alias to this attribute.
+    pub aliases: &'static [&'static str],
+}
+
+/// Returns information about an attribute, as defined in `sentry-conventions`.
+pub fn attribute_info(key: &str) -> Option<&'static AttributeInfo> {
+    ATTRIBUTES.get(key)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_http_response_content_length() {
+        let info = ATTRIBUTES.get("http.response_content_length").unwrap();
+
+        insta::assert_debug_snapshot!(info, @r###"
+        AttributeInfo {
+            write_behavior: BothNames(
+                "http.response.body.size",
+            ),
+            pii: False,
+            aliases: [
+                "http.response.body.size",
+                "http.response.header.content-length",
+                "http.response.header['content-length']",
+            ],
+        }
+        "###);
+    }
+}
