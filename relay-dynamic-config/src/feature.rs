a876e71bd5538109352bad4f88f07a0c78295add
Matt Quinn <matt.quinn@sentry.io>
2025-09-11 15:50:08 -0400

    chore(otlp): use organization-scoped flag for enabling the traces endpoint

--- relay-dynamic-config/src/feature.rs
+++ relay-dynamic-config/src/feature.rs
@@ -52,8 +52,8 @@ pub enum Feature {
     StandaloneSpanIngestion,
     /// Enable standalone span ingestion via the `/traces/` OTel endpoint.
     ///
-    /// Serialized as `projects:relay-otel-endpoint`.
-    #[serde(rename = "projects:relay-otel-endpoint")]
+    /// Serialized as `organizations:relay-otlp-traces-endpoint`.
+    #[serde(rename = "organizations:relay-otlp-traces-endpoint")]
     OtelEndpoint,
     /// Enable playstation crash dump ingestion via the `/playstation/` endpoint.
     ///
@@ -119,6 +119,11 @@ pub enum Feature {
     #[doc(hidden)]
     #[serde(rename = "projects:span-metrics-extraction-addons")]
     DeprecatedExtractAddonsSpanMetricsFromEvent,
+    /// This feature has been deprecated in favor of `OtelEndpoint` and is kept for external Relays.
+    ///
+    /// Serialized as `projects:relay-otel-endpoint`.
+    #[serde(rename = "projects:relay-otel-endpoint")]
+    DeprecatedProjectScopedOtelEndpoint,
     /// Forward compatibility.
     #[doc(hidden)]
     #[serde(other)]
