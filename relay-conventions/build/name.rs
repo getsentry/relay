a88c5908b8e89d5054e2651395c236d20b3fd611
Matt Quinn <matt.quinn@sentry.io>
2025-09-17 14:15:12 -0400

    fix clippy in generated file; better(?) comments

--- relay-conventions/build/name.rs
+++ relay-conventions/build/name.rs
@@ -15,22 +15,33 @@ pub struct Name {
     operations: Vec<Operation>,
 }
 
+/// Returns a `TokenStream` representing Rust code defining a `name_for_op_and_attributes` method.
+/// This method constructs a name for the given op and attributes based on the span naming
+/// conventions defined in the `sentry-conventions` repository.
+///
+/// This function depends on the invariants that:
+/// - each list of name templates contains a single template without attributes, and
+/// - the template without attributes is the final template in the list
+///
+/// In other words, each template list must have a fallback name for the case
+/// where all names are missing. These invariants are enforced by a test in `sentry-conventions`.
 pub fn name_file_output(names: impl Iterator<Item = Name>) -> TokenStream {
     let operations = names.flat_map(|name| name.operations);
 
     let match_arms = TokenStream::from_iter(operations.map(|Operation { ops, templates }| {
+        // Each template stanza handles a single template: if all referenced attributes are present,
+        // the name is constructed via that template and returned.
         let template_stanzas = templates.into_iter().flat_map(|template| {
-            let Ok(mut parsed) = TemplateParser::parse(Rule::root, &template) else {
+            let Some(parts) = parse_template_into_parts(&template) else {
                 println!(
                     "sentry_conventions contained unparseable template \"{}\"",
                     template
                 );
                 return None;
             };
-            let root = parsed.next().unwrap();
-            let parts = template_parts_from_root(root);
+
             let first_part = parts.first()?;
-            let attributes: Vec<&str> = parts
+            let referenced_attribute_names: Vec<&str> = parts
                 .iter()
                 .filter_map(|part| match part {
                     TemplatePart::Literal(_) => None,
@@ -38,39 +49,56 @@ pub fn name_file_output(names: impl Iterator<Item = Name>) -> TokenStream {
                 })
                 .collect();
 
-            if attributes.is_empty() {
+            if referenced_attribute_names.is_empty() {
                 // There are no dynamic parts to this name - it consists of a single literal.
                 let static_name = match first_part {
                     TemplatePart::Literal(s) => *s,
                     _ => unreachable!(),
                 };
+                // Because of the enforced invariant that the single literal template is always
+                // last, no explicit return is needed. (Otherwise clippy complains.)
                 return Some(quote! {
-                    return #static_name.to_owned();
+                    #static_name.to_owned()
                 });
             }
 
-            // Ensure that all referenced attributes are present before constructing a name from
-            // them.
-            let if_clauses = attributes.iter().map(|attribute| {
+            // Ensure that all referenced attributes are present in `attributes` before constructing
+            // a name from them.
+            let if_clauses = referenced_attribute_names.iter().map(|attribute_name| {
                 quote! {
-                    attributes.get_value(#attribute).and_then(|val| val.as_str()).is_some()
+                    attributes.get_value(#attribute_name).and_then(|value| value.as_str()).is_some()
                 }
             });
-            let string_appends = TokenStream::from_iter(parts.iter().map(|part| match part {
-                TemplatePart::Literal(s) => quote! { name.push_str(#s); },
-                TemplatePart::Attribute(attribute) => quote! {
-                    name.push_str(attributes.get_value(#attribute).unwrap().as_str().unwrap());
-                },
-            }));
+
+            // Append each part to the String variable `name`.
+            let append_parts_to_string =
+                TokenStream::from_iter(parts.iter().map(|part| match part {
+                    TemplatePart::Literal(s) => {
+                        // This feels like a micro-optimization, but clippy complains without it.
+                        if s.chars().count() == 1 {
+                            let c = s.chars().next();
+                            quote! { name.push(#c); }
+                        } else {
+                            quote! { name.push_str(#s); }
+                        }
+                    }
+                    TemplatePart::Attribute(attribute_name) => quote! {
+                        name.push_str(attributes.get_value(#attribute_name).unwrap().as_str().unwrap());
+                    },
+                }));
+
+            // Assemble the pieces together into a single `if` block.
             Some(quote! {
                 if #(#if_clauses)&&* {
                     let mut name = String::new();
-                    #string_appends
+                    #append_parts_to_string
                     return name;
                 };
             })
         });
 
+        // Assemble the match arm, with `ops` forming the match clause and the list of
+        // `template_stanzas` (one per template) forming the match body.
         quote! {
             #(#ops)|* => {
                 #(#template_stanzas)*
@@ -79,7 +107,6 @@ pub fn name_file_output(names: impl Iterator<Item = Name>) -> TokenStream {
     }));
 
     quote! {
-        #[allow(clippy::style)]
         pub fn name_for_op_and_attributes(op: &str, attributes: &impl relay_protocol::Getter) -> String {
             match op {
                 #match_arms
@@ -94,7 +121,16 @@ enum TemplatePart<'a> {
     Attribute(&'a str),
 }
 
-fn template_parts_from_root(root: Pair<Rule>) -> Vec<TemplatePart> {
+fn parse_template_into_parts(template: &'_ str) -> Option<Vec<TemplatePart<'_>>> {
+    let Ok(mut parsed) = TemplateParser::parse(Rule::root, template) else {
+        println!(
+            "sentry_conventions contained unparseable template \"{}\"",
+            template
+        );
+        return None;
+    };
+    let root = parsed.next().unwrap();
+
     let mut parts: Vec<TemplatePart> = Vec::new();
     for part in root.into_inner() {
         match part.as_rule() {
@@ -104,7 +140,7 @@ fn template_parts_from_root(root: Pair<Rule>) -> Vec<TemplatePart> {
             Rule::root | Rule::attribute => unreachable!(),
         }
     }
-    parts
+    Some(parts)
 }
 
 mod parser {
