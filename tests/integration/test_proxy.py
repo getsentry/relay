9b660e5f296ee309925b0c68544f0abc27058201
tobias-wilfert <tobias.wilfert@sentry.io>
2025-10-14 16:32:51 +0200

    Add initial version of one large test.

--- tests/integration/test_proxy.py
+++ tests/integration/test_proxy.py
@@ -1,4 +1,4 @@
-""" Tests for proxy and static mode. """
+"""Tests for proxy and static mode."""
 
 from datetime import datetime, timedelta, timezone
 import json
@@ -6,7 +6,9 @@ from pathlib import Path
 
 from sentry_sdk.envelope import Envelope, Item, PayloadRef
 
+import pytest
 from .test_dynamic_sampling import get_profile_payload
+import queue
 
 
 def test_span_allowed(mini_sentry, relay):
@@ -101,45 +103,156 @@ def test_replay_allowed(mini_sentry, relay):
     envelope = mini_sentry.captured_events.get(timeout=10)
 
 
-def test_error_event_passthrough(mini_sentry, relay):
-    relay = relay(mini_sentry, options={"relay": {"mode": "proxy"}})
+TRANSACTION = {
+    "event_id": "d2132d31b39445f1938d7e21b6bf0ec4",
+    "type": "transaction",
+    "transaction": "foo",
+    "start_timestamp": 0,
+    "timestamp": 0,
+    "contexts": {
+        "trace": {
+            "trace_id": "1234F60C11214EB38604F4AE0781BFB2",
+            "span_id": "ABCDFDEAD5F74052",
+            "type": "trace",
+        }
+    },
+}
 
-    error_event = {
+PAYLOADS = {
+    "event": {
         "event_id": "d2132d31b39445f1938d7e21b6bf0ec4",
         "message": "test",
         "extra": {"msg_text": "test"},
         "type": "error",
         "environment": "production",
         "release": "foo@1.2.3",
-    }
-
-    envelope = Envelope()
-    envelope.add_item(
-        Item(type="event", payload=PayloadRef(json.dumps(error_event).encode()))
-    )
-    relay.send_envelope(42, envelope)
-
-    captured_event = mini_sentry.captured_events.get(timeout=10).get_event()
-    assert error_event == captured_event
-
-
-def test_user_report_pass_through(relay, mini_sentry):
-    project_id = 42
-    relay = relay(mini_sentry, options={"relay": {"mode": "proxy"}})
-
-    report_payload = {
+    },
+    "transaction": TRANSACTION,
+    "attachment": b"file content",
+    "user_report": {
         "name": "Josh",
         "email": "",
         "comments": "I'm having fun",
         "event_id": "4cec9f3e1f214073b816e0f4de5f59b1",
-    }
+    },
+    "session": {
+        "sid": "8333339f-5675-4f89-a9a0-1c935255ab58",
+        "did": "foobarbaz",
+        "seq": 42,
+        "init": True,
+        "timestamp": 0,
+        "started": datetime.now(timezone.utc).isoformat(),
+        "duration": 1947.49,
+        "status": "exited",
+        "errors": 0,
+        "attrs": {
+            "release": "sentry-test@1.0.0",
+            "environment": "production",
+        },
+    },
+    "sessions": {
+        "aggregates": [
+            {
+                "started": datetime.now(timezone.utc).isoformat(),
+                "did": "foobarbaz",
+                "exited": 2,
+                "errored": 3,
+            },
+        ],
+        "attrs": {
+            "release": "sentry-test@1.0.0",
+            "environment": "production",
+        },
+    },
+    "statsd": "transactions/foo:42|c",
+    "client_report": {
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "discarded_events": [],
+        "rate_limited_events": [
+            {"reason": "generic", "category": "error", "quantity": 1}
+        ],
+    },
+    "profile": get_profile_payload(TRANSACTION),
+    "replay_event": open(
+        Path(__file__).parent.parent / "fixtures" / "replay.json", "rb"
+    ).read(),
+    "replay_recording": b"{}\n[]",
+    "replay_video": b"hello, world!",
+    "check_in": {
+        "check_in_id": "a460c25ff2554577b920fcfacae4e5eb",
+        "monitor_slug": "my-monitor",
+        "status": "in_progress",
+        "duration": 21.0,
+    },
+    "log": {
+        "items": [
+            {
+                "message": "test log",
+                "level": "info",
+                "timestamp": datetime.now(timezone.utc).isoformat(),
+            }
+        ]
+    },
+    "span": {
+        "description": r"test \" with \" escaped \" chars",
+        "op": "default",
+        "span_id": "cd429c44b67a3eb1",
+        "segment_id": "968cff94913ebb07",
+        "start_timestamp": datetime.now(timezone.utc).isoformat(),
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "exclusive_time": 345.0,
+        "trace_id": "ff62a8b040f340bda5d830223def1d81",
+    },
+    "otel_span": {
+        "traceId": "89143b0763095bd9c9955e8175d1fb23",
+        "spanId": "a342abb1214ca181",
+        "name": "my 1st OTel span",
+        "startTimeUnixNano": "1000000000",
+        "endTimeUnixNano": "2000000000",
+        "attributes": [
+            {"key": "sentry.category", "value": {"stringValue": "db"}},
+        ],
+    },
+    "profile_chunk": open(
+        Path(__file__).parent.parent.parent
+        / "relay-profiling/tests/fixtures/sample/v2/valid.json",
+        "rb",
+    ).read(),
+}
 
-    relay.send_user_report(
-        project_id,
-        report_payload,
-    )
+BINARY_ITEMS = {
+    "replay_event",
+    "replay_recording",
+    "replay_video",
+    "attachment",
+    "profile_chunk",
+}
 
-    captured_report = json.loads(
-        mini_sentry.captured_events.get(timeout=10).items[0].payload.get_bytes()
+
+@pytest.mark.parametrize(
+    "item_type",
+    [k for k in PAYLOADS.keys()],
+)
+def test_passthrough(relay, mini_sentry, item_type):
+    relay = relay(mini_sentry, options={"relay": {"mode": "proxy"}})
+
+    payload = PAYLOADS[item_type]
+    envelope = Envelope()
+    envelope.add_item(
+        Item(
+            type=item_type,
+            payload=PayloadRef(
+                payload if item_type in BINARY_ITEMS else json.dumps(payload).encode()
+            ),
+        )
     )
-    assert report_payload == captured_report
+    relay.send_envelope(42, envelope)
+
+    captured_envelope = mini_sentry.captured_events.get(timeout=10)
+    assert len(captured_envelope.items) == 1
+    with pytest.raises(queue.Empty):
+        mini_sentry.captured_outcomes.get(timeout=1)
+    if item_type in BINARY_ITEMS:
+        assert payload == captured_envelope.items[0].payload.get_bytes()
+    else:
+        assert payload == json.loads(captured_envelope.items[0].payload.get_bytes())
