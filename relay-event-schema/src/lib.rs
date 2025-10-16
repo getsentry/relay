5cfc0a5e1ea9723ef71253f10631cdf7c943fa73
David Herberth <david.herberth@sentry.io>
2025-10-16 13:25:17 +0200

    ref(derive): Allow usage of ProcessValue macro in other crates (#5256)
    Use an absolute crate reference `::relay_event_schema::` instead of
    `crate::`, to make it work in the event schema crate, expose `self` as
    `relay_event_schema`.
    
    Also changes `EnumSet` to a re-exported type in `::relay_event_schema`
    to not have to depend on it directly.
    
    Enables a test that previously was commented out but because it couldn't
    be compiled, which now compiles.

--- relay-event-schema/src/lib.rs
+++ relay-event-schema/src/lib.rs
@@ -5,5 +5,7 @@
     html_favicon_url = "https://raw.githubusercontent.com/getsentry/relay/master/artwork/relay-icon.png"
 )]
 
+extern crate self as relay_event_schema;
+
 pub mod processor;
 pub mod protocol;
