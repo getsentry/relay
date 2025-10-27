5bd030a91534ba1ef7d824cbe004bcffa7fd0ced
indykoning <15870933+indykoning@users.noreply.github.com>
2025-10-27 17:35:07 +0100

    Added storebot to tests

--- relay-filter/src/web_crawlers.rs
+++ relay-filter/src/web_crawlers.rs
@@ -119,6 +119,8 @@ mod tests {
             "AdsBot-Google",
             "Googlebot",
             "FeedFetcher-Google",
+            "Storebot-Google",
+            "Mozilla/5.0 (X11; Linux x86_64; Storebot-Google/1.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
             "BingBot",
             "BingPreview",
             "Baiduspider",
