24902a9ff4b174698dca0bbf1e00719b5bd726a0
Sebastian Zivota <loewenheim@mailbox.org>
2025-11-03 14:52:01 +0100

    feat(redis): Add connection and response timeouts
    This adds connection and respnose timeout config options for both Single
    and Cluster Redis clients.
    
    Unfortunately, since `redis` and `deadpool-redis` don't provide enough
    knobs to tweak the default clients, the easiest way to accomplish this
    is by more or less reinventing `deadpool_redis::Manager` and
    `deadpool_redis::Cluster::Manager`, but with timeouts applied.
    Consequently the `deadpool_redis` dependency becomes unused.
    
    TBD: default values and docstrings for the new timeouts.

--- relay-redis/src/config.rs
+++ relay-redis/src/config.rs
@@ -27,6 +27,8 @@ pub struct RedisConfigOptions {
     /// will fail with a timeout error. This setting helps prevent indefinite
     /// blocking when the pool is exhausted.
     pub wait_timeout: Option<u64>,
+    pub connection_timeout: Option<u64>,
+    pub response_timeout: Option<u64>,
     /// Sets the number of times after which the connection will check whether it is active when
     /// being recycled.
     ///
@@ -43,6 +45,8 @@ impl Default for RedisConfigOptions {
             create_timeout: Some(3),
             recycle_timeout: Some(2),
             wait_timeout: None,
+            connection_timeout: todo!(),
+            response_timeout: todo!(),
             recycle_check_frequency: 100,
         }
     }
