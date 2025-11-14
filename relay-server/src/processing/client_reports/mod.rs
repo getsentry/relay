843a1c60f46878ea15d20afcf3d690645b93cc81
tobias-wilfert <tobias.wilfert@sentry.io>
2025-11-14 15:25:29 +0100

    split processing logic into: expand, validate and emit

--- relay-server/src/processing/client_reports/mod.rs
+++ relay-server/src/processing/client_reports/mod.rs
@@ -1,15 +1,41 @@
+use relay_metrics::UnixTimestamp;
+use relay_quotas::DataCategory;
 use relay_system::Addr;
 
 use crate::envelope::{EnvelopeHeaders, Item, ItemType};
-use crate::managed::{Counted, Managed, ManagedEnvelope, Quantities, Rejected};
+use crate::managed::{Counted, Managed, ManagedEnvelope, OutcomeError, Quantities, Rejected};
 use crate::processing::{self, Context, Nothing, Output};
 use crate::services::outcome::TrackOutcome;
 
 mod process;
 
-// TODO: Not sure there is a cleaner way to do this.
 #[derive(Debug, thiserror::Error)]
-pub enum Error {}
+pub enum Error {
+    /// Client report parsing failed.
+    #[error("invalid client report")]
+    InvalidClientReport,
+    /// Processing relay has client outcomes disabled.
+    #[error("client outcomes disabled")]
+    OutcomesDisabled,
+    /// Client report timestamp is too old.
+    #[error("client report timestamp too old")]
+    TimestampTooOld,
+    /// Client report timestamp is too far in the future.
+    #[error("client report timestamp in future")]
+    TimestampInFuture,
+    /// Client report contains an invalid outcome combination.
+    #[error("invalid outcome combination")]
+    InvalidOutcome,
+}
+
+impl OutcomeError for Error {
+    type Error = Self;
+
+    fn consume(self) -> (Option<crate::services::outcome::Outcome>, Self::Error) {
+        // Currently/historically client reports do not emit outcomes.
+        (None, self)
+    }
+}
 
 /// A processor for Client-Reports.
 pub struct ClientReportsProcessor {
@@ -48,15 +74,21 @@ impl processing::Processor for ClientReportsProcessor {
 
     async fn process(
         &self,
-        mut client_reports: Managed<Self::UnitOfWork>,
+        client_reports: Managed<Self::UnitOfWork>,
         ctx: Context<'_>,
     ) -> Result<Output<Self::Output>, Rejected<Self::Error>> {
-        process::process_client_reports(
-            &mut client_reports,
-            ctx.config,
-            ctx.project_info,
-            &self.aggregator,
-        );
+        if !ctx.config.emit_outcomes().any() && ctx.config.processing_enabled() {
+            // if a processing relay has client outcomes disabled we drop them without processing.
+            return Err(client_reports.reject_err(Error::OutcomesDisabled));
+        }
+
+        let client_reports = process::expand(client_reports);
+
+        // FIXME: This guard is taken over from the old code not sure if we still need this.
+        if !client_reports.output_events.is_empty() {
+            let client_reports = process::validate(client_reports, ctx.config, ctx.project_info)?;
+            process::emit(client_reports, &self.aggregator);
+        }
 
         Ok(Output::empty())
     }
@@ -67,7 +99,6 @@ impl processing::Processor for ClientReportsProcessor {
 pub struct SerializedClientReports {
     /// Original envelope headers.
     headers: EnvelopeHeaders,
-
     /// A list of client-reports waiting to be processed.
     ///
     /// All items contained here must be client-reports.
@@ -78,4 +109,58 @@ impl Counted for SerializedClientReports {
     fn quantities(&self) -> Quantities {
         smallvec::smallvec![]
     }
+}
+
+/// Client reports which have been parsed and aggregated from their serialized state.
+pub struct ExpandedClientReports {
+    /// Original envelope headers.
+    headers: EnvelopeHeaders,
+    /// The timestamp of when the reports were created.
+    timestamp: Option<UnixTimestamp>,
+    /// Aggregated outcome events from all parsed client reports.
+    output_events: Vec<OutcomeEvent>,
+}
+
+impl Counted for ExpandedClientReports {
+    fn quantities(&self) -> crate::managed::Quantities {
+        smallvec::smallvec![]
+    }
+}
+
+/// A aggregated single outcome event.
+pub struct OutcomeEvent {
+    outcome_type: ClientReportField,
+    reason: String,
+    category: DataCategory,
+    quantity: u32,
+}
+
+impl Counted for OutcomeEvent {
+    fn quantities(&self) -> crate::managed::Quantities {
+        smallvec::smallvec![]
+    }
+}
+
+/// Client reports which have been validated and converted to trackable outcomes.
+pub struct ValidatedClientReports {
+    outcomes: Vec<TrackOutcome>,
+}
+
+impl Counted for ValidatedClientReports {
+    fn quantities(&self) -> crate::managed::Quantities {
+        smallvec::smallvec![]
+    }
+}
+
+/// Fields of client reports that map to specific [`Outcome`]s without content.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ClientReportField {
+    /// The event has been filtered by an inbound data filter.
+    Filtered,
+    /// The event has been filtered by a sampling rule.
+    FilteredSampling,
+    /// The event has been rate limited.
+    RateLimited,
+    /// The event has already been discarded on the client side.
+    ClientDiscard,
 }
--- relay-server/src/processing/client_reports/process.rs
+++ relay-server/src/processing/client_reports/process.rs
@@ -1,5 +1,4 @@
 use std::collections::BTreeMap;
-use std::error::Error;
 
 use chrono::{Duration as SignedDuration, Utc};
 use relay_common::time::UnixTimestamp;
@@ -12,64 +11,35 @@ use relay_sampling::evaluation::MatchedRuleIds;
 use relay_system::Addr;
 
 use crate::constants::DEFAULT_EVENT_RETENTION;
-use crate::managed::Managed;
-use crate::processing::client_reports::SerializedClientReports;
+use crate::managed::{Managed, Rejected};
+use crate::processing::client_reports::{
+    ClientReportField, Error, ExpandedClientReports, OutcomeEvent, SerializedClientReports,
+    ValidatedClientReports,
+};
 use crate::services::outcome::{Outcome, RuleCategories, TrackOutcome};
 use crate::services::processor::MINIMUM_CLOCK_DRIFT;
 use crate::services::projects::project::ProjectInfo;
 
-/// Fields of client reports that map to specific [`Outcome`]s without content.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub enum ClientReportField {
-    /// The event has been filtered by an inbound data filter.
-    Filtered,
-
-    /// The event has been filtered by a sampling rule.
-    FilteredSampling,
-
-    /// The event has been rate limited.
-    RateLimited,
-
-    /// The event has already been discarded on the client side.
-    ClientDiscard,
-}
-
-/// Validates and extracts client reports.
+/// Parses and aggregates all client reports in their [`ExpandedClientReports`] representation.
 ///
-/// At the moment client reports are primarily used to transfer outcomes from
-/// client SDKs.  The outcomes are removed here and sent directly to the outcomes
-/// system.
-pub fn process_client_reports(
-    client_reports: &mut Managed<SerializedClientReports>,
-    config: &Config,
-    project_info: &ProjectInfo,
-    outcome_aggregator: &Addr<TrackOutcome>,
-) {
-    if !config.emit_outcomes().any() && config.processing_enabled() {
-        // if a processing relay has client outcomes disabled we drop them without processing.
-        return;
-    }
+/// Invalid reports will be discarded.
+/// Events with overlong reasons will be skipped.
+pub fn expand(client_reports: Managed<SerializedClientReports>) -> Managed<ExpandedClientReports> {
+    client_reports.map(|client_reports, records| {
+        let mut timestamp = None;
+        let mut output_events = BTreeMap::new();
 
-    let mut timestamp = None;
-    let mut output_events = BTreeMap::new();
-    let received = client_reports.received_at();
-
-    let clock_drift_processor =
-        ClockDriftProcessor::new(client_reports.headers.sent_at(), received)
-            .at_least(MINIMUM_CLOCK_DRIFT);
-
-    for item in &client_reports.client_reports {
-        match ClientReport::parse(&item.payload()) {
-            Ok(ClientReport {
-                timestamp: report_timestamp,
-                discarded_events,
-                rate_limited_events,
-                filtered_events,
-                filtered_sampling_events,
-            }) => {
-                // Glue all discarded events together and give them the appropriate outcome type
-                let input_events =
-                    discarded_events
+        for item in client_reports.client_reports {
+            match ClientReport::parse(&item.payload()) {
+                Ok(ClientReport {
+                    timestamp: report_timestamp,
+                    discarded_events,
+                    rate_limited_events,
+                    filtered_events,
+                    filtered_sampling_events,
+                }) => {
+                    // Glue all discarded events together and give them the appropriate outcome type
+                    let input_events = discarded_events
                         .into_iter()
                         .map(|discarded_event| (ClientReportField::ClientDiscard, discarded_event))
                         .chain(
@@ -84,33 +54,65 @@ pub fn process_client_reports(
                             (ClientReportField::RateLimited, discarded_event)
                         }));
 
-                for (outcome_type, discarded_event) in input_events {
-                    if discarded_event.reason.len() > 200 {
-                        relay_log::trace!("ignored client outcome with an overlong reason");
-                        continue;
+                    for (outcome_type, discarded_event) in input_events {
+                        if discarded_event.reason.len() > 200 {
+                            relay_log::trace!("ignored client outcome with an overlong reason");
+                            // TODO: Not sure if we want to reject the individual 'discarded_event'
+                            // here. Rejecting the entire item also seems wrong.
+                            continue;
+                        }
+                        *output_events
+                            .entry((
+                                outcome_type,
+                                discarded_event.reason,
+                                discarded_event.category,
+                            ))
+                            .or_insert(0) += discarded_event.quantity;
                     }
-                    *output_events
-                        .entry((
-                            outcome_type,
-                            discarded_event.reason,
-                            discarded_event.category,
-                        ))
-                        .or_insert(0) += discarded_event.quantity;
+                    if let Some(ts) = report_timestamp {
+                        timestamp.get_or_insert(ts);
+                    }
                 }
-                if let Some(ts) = report_timestamp {
-                    timestamp.get_or_insert(ts);
+                Err(err) => {
+                    relay_log::trace!(
+                        error = &err as &dyn std::error::Error,
+                        "invalid client report received"
+                    );
+                    records.reject_err(Error::InvalidClientReport, item);
                 }
-            }
-            Err(err) => {
-                relay_log::trace!(error = &err as &dyn Error, "invalid client report received")
             }
         }
-    }
 
-    if output_events.is_empty() {
-        return;
-    }
+        ExpandedClientReports {
+            headers: client_reports.headers,
+            timestamp,
+            output_events: output_events
+                .into_iter()
+                .map(
+                    |((outcome_type, reason, category), quantity)| OutcomeEvent {
+                        outcome_type,
+                        reason,
+                        category,
+                        quantity,
+                    },
+                )
+                .collect(),
+        }
+    })
+}
 
+/// Validates and extracts client reports to their [`ValidatedClientReports`] representation.
+pub fn validate(
+    client_reports: Managed<ExpandedClientReports>,
+    config: &Config,
+    project_info: &ProjectInfo,
+) -> Result<Managed<ValidatedClientReports>, Rejected<Error>> {
+    let received = client_reports.received_at();
+    let clock_drift_processor =
+        ClockDriftProcessor::new(client_reports.headers.sent_at(), received)
+            .at_least(MINIMUM_CLOCK_DRIFT);
+
+    let mut timestamp = client_reports.timestamp;
     let timestamp =
         timestamp.get_or_insert_with(|| UnixTimestamp::from_secs(received.timestamp() as u64));
 
@@ -134,7 +136,7 @@ pub fn process_client_reports(
             "skipping client outcomes older than {} days",
             max_age.num_days()
         );
-        return;
+        return Err(client_reports.reject_err(Error::TimestampTooOld));
     }
 
     let max_future = SignedDuration::seconds(config.max_secs_in_future());
@@ -148,28 +150,53 @@ pub fn process_client_reports(
             "skipping client outcomes more than {}s in the future",
             max_future.num_seconds()
         );
-        return;
+        return Err(client_reports.reject_err(Error::TimestampInFuture));
     }
 
-    for ((outcome_type, reason, category), quantity) in output_events.into_iter() {
-        let Ok(outcome) = outcome_from_parts(outcome_type, &reason) else {
-            relay_log::trace!(?outcome_type, reason, "invalid outcome combination");
-            continue;
-        };
+    let scoping = client_reports.scoping();
+    Ok(client_reports.map(|client_reports, records| {
+        let mut outcomes = Vec::new();
+        for event_outcome in client_reports.output_events {
+            let OutcomeEvent {
+                outcome_type,
+                ref reason,
+                category,
+                quantity,
+            } = event_outcome;
 
-        outcome_aggregator.send(TrackOutcome {
-            // If we get to this point, the unwrap should not be used anymore, since we know by
-            // now that the timestamp can be parsed, but just incase we fallback to UTC current
-            // `DateTime`.
-            timestamp: timestamp.as_datetime().unwrap_or_else(Utc::now),
-            scoping: client_reports.scoping(),
-            outcome,
-            event_id: None,
-            remote_addr: None, // omitting the client address allows for better aggregation
-            category,
-            quantity,
-        });
-    }
+            let Ok(outcome) = outcome_from_parts(outcome_type, reason) else {
+                relay_log::trace!(?outcome_type, reason, "invalid outcome combination");
+                records.reject_err(Error::InvalidOutcome, event_outcome);
+                continue;
+            };
+            outcomes.push(TrackOutcome {
+                //   If we get to this point, the unwrap should not be used anymore, since we know by
+                // now that the timestamp can be parsed, but just incase we fallback to UTC current
+                // `DateTime`.
+                timestamp: timestamp.as_datetime().unwrap_or_else(Utc::now),
+                scoping,
+                outcome,
+                event_id: None,
+                remote_addr: None, // omitting the client address allows for better aggregation
+                category,
+                quantity,
+            });
+        }
+
+        ValidatedClientReports { outcomes }
+    }))
+}
+
+/// Emits the validated client report outcomes to the outcome aggregator.
+pub fn emit(
+    client_reports: Managed<ValidatedClientReports>,
+    outcome_aggregator: &Addr<TrackOutcome>,
+) {
+    client_reports.accept(|client_reports| {
+        for outcome in client_reports.outcomes {
+            outcome_aggregator.send(outcome)
+        }
+    })
 }
 
 /// Parse an outcome from an outcome ID and a reason string.
