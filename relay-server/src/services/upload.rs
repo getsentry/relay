45db05f87d46be2079f17a99a76f59a6f1a6be50
Joris Bayer <joris.bayer@sentry.io>
2025-11-27 14:42:29 +0100

    wip

--- relay-server/src/services/upload.rs
+++ relay-server/src/services/upload.rs
@@ -11,7 +11,8 @@ use relay_system::{Addr, FromMessage, Interface, NoResponse, Receiver, Service};
 
 use crate::constants::DEFAULT_ATTACHMENT_RETENTION;
 use crate::envelope::{Item, ItemType};
-use crate::managed::{OutcomeError, TypedEnvelope};
+use crate::managed::{Managed, ManagedResult, OutcomeError, Rejected, TypedEnvelope};
+use crate::processing::spans::ValidatedSpanAttachment;
 use crate::services::outcome::DiscardReason;
 use crate::services::processor::Processed;
 use crate::services::store::{Store, StoreEnvelope};
@@ -22,6 +23,16 @@ use super::outcome::Outcome;
 /// Messages that the upload service can handle.
 pub enum Upload {
     Envelope(StoreEnvelope),
+    Attachment(Managed<ValidatedSpanAttachment>),
+}
+
+impl Upload {
+    fn ty(&self) -> &str {
+        match self {
+            Upload::Envelope(store_envelope) => "envelope",
+            Upload::Attachment(managed) => "attachment_v2",
+        }
+    }
 }
 
 impl Interface for Upload {}
@@ -34,18 +45,28 @@ impl FromMessage<StoreEnvelope> for Upload {
     }
 }
 
+impl FromMessage<Managed<ValidatedSpanAttachment>> for Upload {
+    type Response = NoResponse;
+
+    fn from_message(message: Managed<ValidatedSpanAttachment>, _sender: ()) -> Self {
+        Self::Attachment(message)
+    }
+}
+
 /// Errors that can occur when trying to upload an attachment.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("timeout")]
     Timeout,
-    UploadFailed,
+    #[error("upload failed: {0}")]
+    UploadFailed(#[from] objectstore_client::Error),
 }
 
 impl Error {
     fn as_str(&self) -> &'static str {
         match self {
             Error::Timeout => "timeout",
-            Error::UploadFailed => "upload_failed",
+            Error::UploadFailed(_) => "upload_failed",
         }
     }
 }
@@ -83,7 +104,9 @@ impl UploadService {
         };
 
         let objectstore_client = Client::builder(objectstore_url).build()?;
-        let attachments_usecase = Usecase::new("attachments")
+        let attachments_v1 = Usecase::new("attachments")
+            .with_expiration_policy(ExpirationPolicy::TimeToLive(DEFAULT_ATTACHMENT_RETENTION));
+        let attachments_v2 = Usecase::new("trace_attachments")
             .with_expiration_policy(ExpirationPolicy::TimeToLive(DEFAULT_ATTACHMENT_RETENTION));
 
         let inner = UploadServiceInner {
@@ -92,7 +115,8 @@ impl UploadService {
             store,
 
             objectstore_client,
-            attachments_usecase,
+            attachments_v1,
+            attachments_v2,
         };
 
         Ok(Some(Self {
@@ -103,25 +127,24 @@ impl UploadService {
     }
 
     fn handle_message(&self, message: Upload) {
-        let Upload::Envelope(StoreEnvelope { envelope }) = message;
         if self.pending_requests.len() >= self.max_concurrent_requests {
             // Load shed to prevent backlogging in the service queue and affecting other parts of Relay.
             // We might want to have a less aggressive mechanism in the future.
-
             relay_statsd::metric!(
                 counter(RelayCounters::AttachmentUpload) += 1,
                 result = "load-shed",
-                type = "envelope",
+                type = message.ty(),
             );
-
-            // for now, this will just forward to the store endpoint without uploading attachments.
-            self.inner.store.send(StoreEnvelope { envelope });
+            if let Upload::Envelope(envelope) = message {
+                // for now, this will just forward to the store endpoint without uploading attachment
+                self.inner.store.send(envelope);
+            }
             return;
         }
 
         let inner = self.inner.clone();
-        let future = async move { inner.handle_envelope(envelope).await };
-        self.pending_requests.push(future.boxed());
+        self.pending_requests
+            .push(async move { inner.handle_message(message).await }.boxed());
     }
 }
 
@@ -155,10 +178,20 @@ struct UploadServiceInner {
     store: Addr<Store>,
 
     objectstore_client: Client,
-    attachments_usecase: Usecase,
+    attachments_v1: Usecase,
+    attachments_v2: Usecase,
 }
 
 impl UploadServiceInner {
+    async fn handle_message(&self, message: Upload) {
+        match message {
+            Upload::Envelope(StoreEnvelope { envelope }) => {
+                self.handle_envelope(envelope).await;
+            }
+            Upload::Attachment(managed) => self.handle_managed(managed).await,
+        }
+    }
+
     /// Uploads all attachments belonging to the given envelope.
     ///
     /// This mutates the attachment items in-place, setting their `stored_key` field to the key
@@ -166,7 +199,7 @@ impl UploadServiceInner {
     async fn handle_envelope(&self, mut envelope: TypedEnvelope<Processed>) -> () {
         let scoping = envelope.scoping();
         let session = self
-            .attachments_usecase
+            .attachments_v1
             .for_project(scoping.organization_id.value(), scoping.project_id.value())
             .session(&self.objectstore_client);
 
@@ -185,7 +218,7 @@ impl UploadServiceInner {
             }
             Ok(session) => {
                 for attachment in attachments {
-                    let result = self.handle_attachment(&session, attachment).await;
+                    let result = self.handle_envelope_attachment(&session, attachment).await;
                     relay_statsd::metric!(
                         counter(RelayCounters::AttachmentUpload) += 1,
                         result = match result {
@@ -202,7 +235,61 @@ impl UploadServiceInner {
         self.store.send(StoreEnvelope { envelope });
     }
 
-    async fn handle_attachment(
+    async fn handle_managed(&self, managed: Managed<ValidatedSpanAttachment>) {
+        let result = self.do_handle_managed(managed).await;
+
+        relay_statsd::metric!(
+            counter(RelayCounters::AttachmentUpload) += 1,
+            result = match result {
+                Ok(()) => "success",
+                Err(e) => e.into_inner().as_str(),
+            },
+            type = "attachment_v2",
+        );
+    }
+
+    async fn do_handle_managed(
+        &self,
+        managed: Managed<ValidatedSpanAttachment>,
+    ) -> Result<(), Rejected<Error>> {
+        let scoping = managed.scoping();
+        let session = self
+            .attachments_v1
+            .for_project(scoping.organization_id.value(), scoping.project_id.value())
+            .session(&self.objectstore_client)
+            .map_err(Error::UploadFailed)
+            .reject(&managed)?;
+
+        let ValidatedSpanAttachment { meta, body } = &*managed;
+
+        if !body.is_empty() {
+            relay_log::trace!("Starting attachment upload");
+            let future = async {
+                let result = session
+                    .put(body.clone())
+                    .send()
+                    .await
+                    .map_err(Error::UploadFailed)
+                    .reject(&managed)?;
+                Ok(result.key)
+            };
+
+            // FIXME: Pick your own key.
+            let stored_key = tokio::time::timeout(self.timeout, future)
+                .await
+                .map_err(|_elapsed| Error::Timeout)
+                .reject(&managed)??;
+
+            // attachment.set_stored_key(stored_key);
+            relay_log::trace!("Finished attachment upload");
+        }
+
+        // FIXME: Send metadata and payload size to Store service to write to EAP.
+
+        Ok(())
+    }
+
+    async fn handle_envelope_attachment(
         &self,
         session: &Session,
         attachment: &mut Item,
@@ -220,7 +307,7 @@ impl UploadServiceInner {
         let stored_key = tokio::time::timeout(self.timeout, future)
             .await
             .map_err(|_elapsed| Error::Timeout)?
-            .map_err(|_error: objectstore_client::Error| Error::UploadFailed)?;
+            .map_err(Error::UploadFailed)?;
 
         attachment.set_stored_key(stored_key);
         relay_log::trace!("Finished attachment upload");
