3ac397c2c29d4e026627798f207dd4120ca2f9bb
Joris Bayer <joris.bayer@sentry.io>
2025-12-02 12:36:05 +0100

    fix(quotas): Report invalid quotas (#5428)

--- relay-dynamic-config/src/project.rs
+++ relay-dynamic-config/src/project.rs
@@ -101,7 +101,7 @@ pub struct ProjectConfig {
 impl ProjectConfig {
     /// Validates fields in this project config and removes values that are partially invalid.
     pub fn sanitize(&mut self) {
-        self.quotas.retain(Quota::is_valid);
+        self.remove_invalid_quotas();
 
         metrics::convert_conditional_tagging(self);
         defaults::add_span_metrics(self);
@@ -128,6 +128,18 @@ impl ProjectConfig {
                         );
                     }
                 }
+            }
+        }
+    }
+
+    fn remove_invalid_quotas(&mut self) {
+        let invalid_quotas: Vec<_> = self.quotas.extract_if(.., |q| !q.is_valid()).collect();
+        if !invalid_quotas.is_empty() {
+            {
+                relay_log::error!(
+                    invalid_quotas = ?invalid_quotas,
+                    "Found an invalid quota definition",
+                );
             }
         }
     }
