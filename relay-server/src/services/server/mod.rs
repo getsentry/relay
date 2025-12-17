6e39fc23aef7878450b087b4dedf6367b37ab0c0
David Herberth <david.herberth@sentry.io>
2025-12-17 09:11:45 +0100

    ref(server): Remove body timing layer (#5493)

--- relay-server/src/services/server/mod.rs
+++ relay-server/src/services/server/mod.rs
@@ -17,9 +17,7 @@ use tower_http::compression::{CompressionLayer, DefaultPredicate, Predicate};
 use tower_http::set_header::SetResponseHeaderLayer;
 
 use crate::constants;
-use crate::middlewares::{
-    self, BodyTimingLayer, CatchPanicLayer, NormalizePath, RequestDecompressionLayer,
-};
+use crate::middlewares::{self, CatchPanicLayer, NormalizePath, RequestDecompressionLayer};
 use crate::service::ServiceState;
 use crate::statsd::{RelayCounters, RelayGauges};
 
@@ -64,7 +62,6 @@ fn make_app(service: ServiceState, f: impl FnOnce(&Config) -> axum::Router<Servi
     //  - Requests go from top to bottom
     //  - Responses go from bottom to top
     let middleware = ServiceBuilder::new()
-        .layer(BodyTimingLayer)
         .layer(axum::middleware::from_fn(middlewares::metrics))
         .layer(CatchPanicLayer::custom(middlewares::handle_panic))
         .layer(SetResponseHeaderLayer::overriding(
