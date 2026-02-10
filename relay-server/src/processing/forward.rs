e0b1d5a873043a1eca525d678fd2c1bc5190e29a
tobias-wilfert <tobias.wilfert@sentry.io>
2026-02-10 14:55:50 +0100

    appease clippy

--- relay-server/src/processing/forward.rs
+++ relay-server/src/processing/forward.rs
@@ -71,10 +71,16 @@ pub trait Forward {
 #[derive(Copy, Clone, Debug)]
 pub struct ForwardContext<'a> {
     /// The Relay configuration.
-    #[expect(unused, reason = "not yet used")]
+    #[cfg_attr(
+        not(feature = "processing"),
+        expect(unused, reason = "only used in processing")
+    )]
     pub config: &'a Config,
     /// A view of the currently active global configuration.
-    #[expect(unused, reason = "not yet used")]
+    #[cfg_attr(
+        not(feature = "processing"),
+        expect(unused, reason = "only used in processing")
+    )]
     pub global_config: &'a GlobalConfig,
     /// Project configuration associated with the unit of work.
     pub project_info: &'a ProjectInfo,
