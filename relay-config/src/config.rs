eec779bd6ae7f5bc44178176028e1940cfcb2873
Kev <6111995+k-fish@users.noreply.github.com>
2026-04-01 06:19:30 -0400

    feat(tracemetrics): Allow similar size to logs (#5779)
    Co-authored-by: Joris Bayer <joris.bayer@sentry.io>

--- relay-config/src/config.rs
+++ relay-config/src/config.rs
@@ -716,7 +716,7 @@ impl Default for Limits {
             max_api_file_upload_size: ByteSize::mebibytes(40),
             max_api_chunk_upload_size: ByteSize::mebibytes(100),
             max_profile_size: ByteSize::mebibytes(50),
-            max_trace_metric_size: ByteSize::kibibytes(2),
+            max_trace_metric_size: ByteSize::mebibytes(1),
             max_log_size: ByteSize::mebibytes(1),
             max_span_size: ByteSize::mebibytes(1),
             max_container_size: ByteSize::mebibytes(12),
