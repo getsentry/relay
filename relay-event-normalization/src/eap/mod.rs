937f5023de0ea381f8b96d985efde74f15fbe123
Sebastian Zivota <loewenheim@users.noreply.github.com>
2026-05-13 10:38:36 +0200

    fix(spanv2): Backfill `sentry.transaction` (#5987)

--- relay-event-normalization/src/eap/mod.rs
+++ relay-event-normalization/src/eap/mod.rs
@@ -665,20 +665,22 @@ pub fn write_legacy_attributes(attributes: &mut Annotated<Attributes>) {
     };
 
     // Map of new sentry conventions attributes to legacy SpanV1 attributes
+    #[allow(
+        deprecated,
+        reason = "Writing possibly deprecated legacy attributes is the point of this function."
+    )]
     let current_to_legacy_attributes = [
         // DB attributes
         (DB__QUERY__TEXT, SENTRY__DESCRIPTION),
         (SENTRY__NORMALIZED_DB_QUERY, SENTRY__NORMALIZED_DESCRIPTION),
         (DB__OPERATION__NAME, SENTRY__ACTION),
-        #[allow(
-            deprecated,
-            reason = "Writing possibly deprecated legacy attributes is the point of this function."
-        )]
         (DB__SYSTEM__NAME, DB__SYSTEM),
         // HTTP attributes
         (SERVER__ADDRESS, SENTRY__DOMAIN),
         (HTTP__REQUEST__METHOD, SENTRY__ACTION),
         (HTTP__RESPONSE__STATUS_CODE, SENTRY__STATUS_CODE),
+        // Transaction
+        (SENTRY__SEGMENT__NAME, SENTRY__TRANSACTION),
     ];
 
     for (current_attribute, legacy_attribute) in current_to_legacy_attributes {
