05e5b4e9829d715dc5ff13af74aa392070694a35
David Herberth <david.herberth@sentry.io>
2026-05-22 09:23:52 +0200

    test(asserts): Minimize diffs with assertion helpers

--- tests/integration/asserts/__init__.py
+++ tests/integration/asserts/__init__.py
@@ -1,8 +1,10 @@
 from typing import Any, Callable
 from .time import time_after, time_within, time_within_delta, time_is
 from .protocol import only_items
+from .utils import PrintLastCompare
 
 __all__ = [
+    "any",
     "matches",
     "only_items",
     "time_after",
@@ -12,7 +14,7 @@ __all__ = [
 ]
 
 
-class _Matches(object):
+class _Matches(PrintLastCompare):
     def __init__(self, compare_with: Callable[[Any], bool]):
         self._compare_with = compare_with
 
@@ -37,3 +39,15 @@ def matches(f: Callable[[Any], bool]):
         }
     """
     return _Matches(f)
+
+
+class _Any(PrintLastCompare):
+    def __eq__(self, other, /):
+        return True
+
+    def __repr__(self):
+        return "<ANY>"
+
+
+def any():
+    return _Any()
---
+++ tests/integration/asserts/utils.py
@@ -0,0 +1,63 @@
+import functools
+
+
+class PrintLastCompare(object):
+    """
+    Meta class which remembers the last comparison result to minimize differences in pytest's diff output.
+    """
+
+    def __init_subclass__(cls, **kwargs) -> None:
+        super().__init_subclass__(**kwargs)
+
+        def _wrap_eq(fn):
+            @functools.wraps(fn)
+            def wrapped(self, value, /):
+                self._last_compare_value = value
+                self._last_compare_is_eq = fn(self, value)
+                return self._last_compare_is_eq
+
+            return wrapped
+
+        if hasattr(cls, "__eq__"):
+            setattr(cls, "__eq__", _wrap_eq(cls.__eq__))
+
+        def _wrap_ne(fn):
+            @functools.wraps(fn)
+            def wrapped(self, value, /):
+                self._last_compare_value = value
+                self._last_compare_is_eq = not fn(self, value)
+                return not self._last_compare_is_eq
+
+            return wrapped
+
+        if hasattr(cls, "__ne__"):
+            setattr(cls, "__ne__", _wrap_ne(cls.__ne__))
+
+        # This depends on pytest internals, should this ever break, a simple alternative is to instead override
+        # repr of `cls` to return `repr(self._last_compare_value)`. This will not work for anything where pytest
+        # has custom formatters, but for all primitives this will work.
+        def _register_any_printer(cls):
+            from _pytest._io.pprint import PrettyPrinter
+
+            def _dispatch_last_compare(
+                printer, obj, stream, indent, allowance, context, level
+            ):
+                if getattr(obj, "_last_compare_is_eq", False):
+                    p = obj._last_compare_value
+                else:
+                    # Since cls.__repr__ points to this function and we want to delegate to the original
+                    # repr implementation, we'll have to use a wrapper such as `_NoopRepr` to break the recursion.
+                    p = _NoopRepr(obj)
+                printer._format(p, stream, indent, allowance, context, level)
+
+            PrettyPrinter._dispatch[cls.__repr__] = _dispatch_last_compare
+
+        _register_any_printer(cls)
+
+
+class _NoopRepr(object):
+    def __init__(self, delegate):
+        self.delegate = delegate
+
+    def __repr__(self) -> str:
+        return repr(self.delegate)
