3e2bd9a5a179926f0a0930693ed286fa70f8a538
Sebastian Zivota <loewenheim@mailbox.org>
2026-06-23 14:39:23 +0200

    ref(spans): Modify segment attributes during conversion
    Refactor of #6042. Instead of modifying `is_segment` and related
    fields/attributes as a span V2 normalization step, we do it when we
    convert from V1 to V2 in the new standalone span pipeline. To make this
    possible, the core of the function that already does this for the
    legacy pipeline is extracted to `relay-spans`.
    
    This PR actually does slightly more than move logic from #6042 around:
    it also ports some other small normalizations from the legacy pipeline
    to the V2 one. I had these noted down to investigate anyway, so this
    takes care of them in one fell swoop.

--- relay-server/src/processing/legacy_spans/normalize.rs
+++ relay-server/src/processing/legacy_spans/normalize.rs
@@ -62,33 +62,7 @@ pub struct NormalizeSpanConfig<'a> {
 
 fn set_segment_attributes(span: &mut Annotated<Span>) {
     let Some(span) = span.value_mut() else { return };
-
-    // Identify INP spans or other WebVital spans and make sure they are not wrapped in a segment.
-    if let Some(span_op) = span.op.value()
-        && (span_op.starts_with("ui.interaction.") || span_op.starts_with("ui.webvital."))
-    {
-        span.is_segment = None.into();
-        span.parent_span_id = None.into();
-        span.segment_id = None.into();
-        return;
-    }
-
-    let Some(span_id) = span.span_id.value() else {
-        return;
-    };
-
-    if let Some(segment_id) = span.segment_id.value() {
-        // The span is a segment if and only if the segment_id matches the span_id.
-        span.is_segment = (segment_id == span_id).into();
-    } else if span.parent_span_id.is_empty() {
-        // If the span has no parent, it is automatically a segment:
-        span.is_segment = true.into();
-    }
-
-    // If the span is a segment, always set the segment_id to the current span_id:
-    if span.is_segment.value() == Some(&true) {
-        span.segment_id = span.span_id.clone();
-    }
+    relay_spans::set_segment_attributes(span);
 }
 
 /// Normalizes a standalone span.
--- relay-spans/src/lib.rs
+++ relay-spans/src/lib.rs
@@ -10,6 +10,7 @@ pub use crate::description::derive_description_for_v2_span;
 pub use crate::name::name_for_attributes;
 pub use crate::op::derive_op_for_v2_span;
 pub use crate::otel_to_sentry_v2::otel_to_sentry_span as otel_to_sentry_span_v2;
+pub use crate::segment::set_segment_attributes;
 pub use crate::v1_to_v2::span_v1_to_span_v2;
 
 pub use opentelemetry_proto::tonic::trace::v1 as otel_trace;
@@ -18,4 +19,5 @@ mod description;
 mod name;
 mod op;
 mod otel_to_sentry_v2;
+mod segment;
 mod v1_to_v2;
