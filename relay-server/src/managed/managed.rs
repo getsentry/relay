f5612b708c53d6869c309a868be871a8c129de7f
Pierce Roberts <pierceroberts95@gmail.com>
2026-06-24 03:12:41 -0600

    run make style-rust lint-rust.

--- relay-server/src/managed/managed.rs
+++ relay-server/src/managed/managed.rs
@@ -5,8 +5,8 @@ use std::fmt;
 use std::iter::FusedIterator;
 use std::mem::ManuallyDrop;
 use std::net::IpAddr;
-use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 use chrono::{DateTime, Utc};
 use itertools::Either;
@@ -15,12 +15,12 @@ use relay_quotas::{DataCategory, Scoping};
 use relay_system::Addr;
 use smallvec::SmallVec;
 
+use crate::Envelope;
 use crate::endpoints::common::BadStoreRequest;
 use crate::extractors::RequestMeta;
 use crate::managed::{Counted, ManagedEnvelope, Quantities};
 use crate::services::outcome::{DiscardReason, Outcome, TrackOutcome};
 use crate::services::processor::ProcessingError;
-use crate::Envelope;
 
 #[cfg(debug_assertions)]
 mod debug;
