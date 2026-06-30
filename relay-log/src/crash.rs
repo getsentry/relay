b85b75d79218d688a6f35b09fe1e4b4d2448da1a
David Herberth <david.herberth@sentry.io>
2026-07-01 01:24:26 +0200

    ref(crash): Replace relay-crash with minidumper

---
+++ relay-log/src/crash.rs
@@ -0,0 +1,96 @@
+const CRASH_REPORTER_PROCESS_ENV_VAR: &str = "_RELAY_CRASH_REPORTER_PROCESS";
+
+/// Returns `true` if the current process is the crash reporting process.
+pub fn is_crash_reporter_process() -> bool {
+    cfg!(feature = "crash-handler") && std::env::var_os(CRASH_REPORTER_PROCESS_ENV_VAR).is_some()
+}
+
+/// Initializes a secondary watcher process for reporting fatal crashes.
+///
+/// This creates a secondary process, spawned from the same binary, communicating
+/// with the original process via IPC. If the primary process crashes,
+/// the secondary process will capture a minidump and report it to Sentry, delaying
+/// the shutdown of the crashed process until the minidump has been uploaded.
+#[cfg(feature = "crash-handler")]
+pub fn init(config: &crate::SentryConfig, client: crate::sentry::Client) {
+    use crate::sentry::{self, protocol};
+
+    let Some(db) = config._crash_db.clone() else {
+        return;
+    };
+
+    let child = minidumper_child::MinidumperChild::new()
+        .with_server_env_var(CRASH_REPORTER_PROCESS_ENV_VAR.to_owned())
+        .with_crashes_dir(db.clone())
+        .on_process(|command| {
+            #[cfg(unix)]
+            {
+                use std::os::unix::process::CommandExt;
+                command.arg0("relay-crash");
+            }
+            // Skip arg0, the process name.
+            command.args(std::env::args().skip(1));
+        })
+        .on_minidump(move |buffer, path| {
+            crate::info!("Minidump captured ({})", path.display());
+
+            crate::with_scope(
+                |scope| {
+                    // Remove event.process because this event came from the
+                    // main app process
+                    scope.remove_extra("event.process");
+
+                    let filename = path
+                        .file_name()
+                        .map(|s| s.to_string_lossy().into_owned())
+                        .unwrap_or_else(|| "minidump.dmp".to_owned());
+
+                    scope.add_attachment(protocol::Attachment {
+                        buffer,
+                        filename,
+                        ty: Some(protocol::AttachmentType::Minidump),
+                        ..Default::default()
+                    });
+                },
+                || {
+                    sentry::capture_event(protocol::Event {
+                        level: protocol::Level::Fatal,
+                        ..Default::default()
+                    })
+                },
+            );
+
+            // We need to flush because the server will exit after this closure returns
+            client.flush(Some(std::time::Duration::from_secs(15)));
+        });
+
+    // Make sure the implementations agree on the detection.
+    debug_assert_eq!(
+        is_crash_reporter_process(),
+        child.is_crash_reporter_process()
+    );
+
+    if child.is_crash_reporter_process() {
+        // Set the `event.process` so that it's obvious when Rust events come from
+        // the crash reporter process rather than the main app process
+        sentry::configure_scope(|scope| {
+            scope.set_extra("event.process", "crash-reporter".to_owned().into());
+        });
+    } else {
+        crate::info!("Initializing crash handler in {}", db.display());
+    }
+
+    match child.spawn() {
+        Ok(handle) => {
+            crate::info!("Crash handler initialized");
+            // Dropping the handle would detach the crash handler not catching any crashes.
+            std::mem::forget(handle);
+        }
+        Err(err) => {
+            crate::warn!(
+                error = &err as &dyn std::error::Error,
+                "Failed to initialize crash reporting process"
+            );
+        }
+    }
+}
