48f61a9e94db9055c861b5e520e5116b820cf0ea
Joris Bayer <joris.bayer@sentry.io>
2026-07-10 11:26:34 +0200

    fix(upload): Retry multipart uploads

--- relay-server/src/services/objectstore.rs
+++ relay-server/src/services/objectstore.rs
@@ -948,8 +948,23 @@ impl ObjectstoreServiceInner {
                         let chunk = chunk?;
                         let part_number = u32::try_from(i + 1)
                             .map_err(|_| objectstore_client::Error::InvalidPartNumber(u32::MAX))?;
-                        let part = multipart_upload.put(chunk, part_number, None).await?;
-                        parts.push(part);
+
+                        // NOTE: This is a retry loop within a retry loop (see caller of this function).
+                        // if we keep the Rechunked approach we might as well remove the outer loop for streaming uploads.
+                        let mut attempts = 0;
+                        let part = loop {
+                            let result = multipart_upload.put(chunk.clone(), part_number, None).await;
+                            attempts += 1;
+                            if attempts < self.max_attempts.get()
+                                && matches!(&result, Err(e) if is_retryable(e))
+                            {
+                                tokio::time::sleep(self.retry_interval).await;
+                            } else {
+                                break result;
+                            }
+                        };
+
+                        parts.push(part?);
                     }
                     multipart_upload.complete(parts).await?
                 });
