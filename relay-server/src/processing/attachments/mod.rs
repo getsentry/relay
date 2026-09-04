1cfb894def5d381c370929344c745644500abe57
tobias-wilfert <tobias.wilfert@sentry.io>
2026-09-04 09:49:12 +0200

    add verbose log

--- relay-server/src/processing/attachments/mod.rs
+++ relay-server/src/processing/attachments/mod.rs
@@ -105,6 +105,10 @@ impl processing::Processor for AttachmentProcessor {
         );
 
         if !has_event_id {
+            relay_log::info!(
+                sdk = attachments.headers.meta().client().unwrap_or("unknown"),
+                "attachment without EventId"
+            );
             return Err(attachments.reject_err(Error::NoEventId));
         }
 
