c9447e6a532e5db8eaf70d7a8151e3898b0092e2
Joris Bayer <joris.bayer@sentry.io>
2026-09-08 14:35:53 +0200

    feat(server): Tag request metric by content encoding

--- relay-server/src/middlewares/metrics.rs
+++ relay-server/src/middlewares/metrics.rs
@@ -2,6 +2,7 @@ use axum::RequestExt;
 use axum::extract::{MatchedPath, Request};
 use axum::middleware::Next;
 use axum::response::Response;
+use http::header;
 use std::time::Instant;
 
 use crate::extractors::ReceivedAt;
@@ -19,11 +20,17 @@ pub async fn metrics(mut request: Request, next: Next) -> Response {
     let matched_path = request.extract_parts::<MatchedPath>().await;
     let route = matched_path.as_ref().map_or("unknown", |m| m.as_str());
     let method = request.method().clone();
+    let content_encoding = request
+        .headers()
+        .get(header::CONTENT_ENCODING)
+        .and_then(|v| v.to_str().ok())
+        .unwrap_or("");
 
     relay_statsd::metric!(
         counter(RelayCounters::Requests) += 1,
         route = route,
         method = method.as_str(),
+        content_encoding = content_encoding,
     );
 
     let response = next.run(request).await;
