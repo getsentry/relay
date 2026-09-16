ca1c5ea41c61bd93bca2c1c2b38ed5b0d862a15b
Joris Bayer <joris.bayer@sentry.io>
2026-09-16 12:58:01 +0200

    Revert "test: use artificial timer"
    This reverts commit 0a5bd04fb16ac27320e3457cd906ce4e8fea4269.

--- relay-server/src/services/buffer/envelope_stack/sqlite.rs
+++ relay-server/src/services/buffer/envelope_stack/sqlite.rs
@@ -1,10 +1,9 @@
 use std::fmt::Debug;
 use std::num::NonZeroUsize;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use chrono::{DateTime, Utc};
 use relay_base_schema::project::ProjectKey;
-use tokio::time::Instant;
 
 use crate::envelope::Envelope;
 use crate::services::buffer::envelope_stack::EnvelopeStack;
@@ -412,10 +411,8 @@ mod tests {
         assert_eq!(stack.batch.len(), 2);
         assert_eq!(envelope_store.total_count().await.unwrap(), 0);
 
-        tokio::time::pause();
-        tokio::time::advance(timeout + timeout).await;
-
         // Third push (after timeout): spool
+        stack.last_flush = Instant::now() - timeout - Duration::from_secs(1);
         stack.push(envelopes[2].clone()).await.unwrap();
         assert_eq!(stack.batch.len(), 1);
         assert_eq!(envelope_store.total_count().await.unwrap(), 2);
