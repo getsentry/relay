b06b1ba4bcf7f973871d90ee9f8e4ed4050aaac3
Joris Bayer <joris.bayer@sentry.io>
2026-09-17 10:56:11 +0200

    mutex

--- relay-server/src/services/store.rs
+++ relay-server/src/services/store.rs
@@ -17,6 +17,7 @@ use relay_conventions::attributes::SENTRY__SEGMENT__ID;
 use sentry::protocol::{Attachment, SpanId};
 use sentry_protos::snuba::v1::{TraceItem, TraceItemType};
 use serde::Serialize;
+use tokio::sync::Mutex;
 use uuid::Uuid;
 
 use relay_base_schema::data_category::DataCategory;
@@ -30,7 +31,7 @@ use relay_metrics::{
     Bucket, BucketView, BucketViewValue, BucketsView, ByNamespace, GaugeValue, MetricName,
     MetricNamespace, SetView,
 };
-use relay_protocol::{Annotated, FiniteF64, IntoValue, SerializableAnnotated};
+use relay_protocol::{Annotated, FiniteF64, SerializableAnnotated};
 use relay_quotas::Scoping;
 use relay_statsd::metric;
 use relay_system::{FromMessage, Interface, NoResponse, Service};
@@ -439,7 +440,7 @@ pub struct StoreService {
     global_config: GlobalConfigHandle,
     metric_outcomes: MetricOutcomes,
     producer: Producer,
-    last_span_report: Instant,
+    last_span_report: Mutex<Instant>, // used to debounce expensive instrumentation.
 }
 
 impl StoreService {
@@ -456,7 +457,7 @@ impl StoreService {
             global_config,
             metric_outcomes,
             producer,
-            last_span_report: Instant::now(),
+            last_span_report: Instant::now().into(),
         })
     }
 
@@ -707,16 +708,14 @@ impl StoreService {
                 .value()
                 .and_then(|a| a.get_value(SENTRY__SEGMENT__ID))
                 .and_then(|v| v.as_str())
+                && let Err(e) = segment_id.parse::<SpanId>()
             {
-                // This will trigger an error log in handle_message.
-                let _: SpanId = segment_id.parse().inspect_err(|_| {
-                    relay_log::configure_scope(|scope| {
-                        scope.set_tag("sentry_project_id", scoping.project_id);
+                relay_log::configure_scope(|scope| {
+                    scope.set_tag("sentry_project_id", scoping.project_id);
+                    if let Ok(mut last_report) = self.last_span_report.try_lock() {
                         let now = Instant::now();
-                        if (now.saturating_duration_since(self.last_span_report))
-                            > Duration::from_secs(1)
-                        {
-                            self.last_span_report = now;
+                        if (now.saturating_duration_since(*last_report)) > Duration::from_secs(1) {
+                            *last_report = now;
                             if let Ok(json) = Annotated::new(span.item).to_json() {
                                 scope.add_attachment(Attachment {
                                     buffer: json.into_bytes(),
@@ -726,10 +725,10 @@ impl StoreService {
                                 });
                             }
                         }
-                    });
-                })?;
+                    }
+                });
+                return Err(e.into());
             }
-
             let item = Annotated::new(span.item);
             let message = KafkaMessage::SpanV2 {
                 routing_key: span.routing_key,
